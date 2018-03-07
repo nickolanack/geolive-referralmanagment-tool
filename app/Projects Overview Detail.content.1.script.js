a7054caa9e4fdb8804f71b5fceb1bf7d8cfde471
nickolanack <nickblackwell82@gmail.com>
2018-03-07 09:21:46 -0800

    -- Auto Commit - Changes made through Frontend Interface

--- app/Projects Overview Detail.content.1.script.js
+++ app/Projects Overview Detail.content.1.script.js
@@ -11,32 +11,9 @@ return function(viewer, element, parentModule){
     (new ListSortModule(function(){
         return viewer.getChildView('content', 2);
     }, {
-        sorters:[
-            {
-            label:"priority",
-            sortFn:function(a, b){
-                    return (a.getPriorityNumber()>b.getPriorityNumber()?1:-1);
-            }
-        },{
-            label:"name",
-            sortFn:function(a, b){
-                    return (a.getName()>b.getName()?1:-1);
-            }
-        },
-        {
-            label:"client",
-            sortFn:function(a, b){
-                    return (a.getCompanyName()>b.getCompanyName()?1:-1);
-            }
-        },
-        {
-            label:"deadline",
-            sortFn:function(a, b){
-                    return (a.getSubmitDate()>b.getSubmitDate()?1:-1);
-            }
-        }],
-        applySort:"priority",
-        applySortInvert:true
+        sorters:ReferralManagementDashboard.projectSorters(),
+        currentSort:"priority",
+        currentSortInvert:true
     })).load(null, div, null);
     
 
@@ -45,21 +22,9 @@ return function(viewer, element, parentModule){
     (new ListFilterModule(function(){
         return viewer.getChildView('content', 2)
     }, {
-        filters:[{
-            label:"complete",
-            filterFn:function(a){
-                    return a.isComplete();
-            }
-        },
-        {
-            label:"high priority",
-            name:"high",
-            filterFn:function(a){
-                    return a.isHighPriority();
-            }
-        }],
-        applyFilter:"complete",
-        applyFilterInvert:true
+        filters:ReferralManagementDashboard.projectFilters(),
+        currentFilter:"complete",
+        currentFilterInvert:true
     })).load(null, div, null);
     
 
