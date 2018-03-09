ee0e69b57182ca158c31b8a913556ecb1fbda086
nickolanack <nickblackwell82@gmail.com>
2018-03-09 11:16:25 -0800

    -- Auto Commit - Changes made through Frontend Interface

--- app/People Users Detail.content.3.getItemListScript.js
+++
@@ -1,7 +0,0 @@
-return ProjectTeam.CurrentTeam().getAllUsers().filter(function(u){
-    var roles=u.getRoles();
-    if(roles.length==0||roles[0]=="community-member"){
-        return true;
-    }
-    return false;
-});
--- app/People Users Detail.content.5.getItemListScript.js
+++
@@ -1,1 +0,0 @@
-return ProjectTeam.CurrentTeam().getDevices();
