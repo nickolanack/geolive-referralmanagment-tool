35384ae0e871b9d1a44d02535319540fde95dec8
nickolanack <nickblackwell82@gmail.com>
2018-03-12 13:35:28 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.9.script.js
+++ app/singleProjectOverviewDetail.content.9.script.js
@@ -5,28 +5,25 @@
 
 
 var activityLabel=new ElementModule("div");
+var chart=new BarChartModule({data:function(callback){
+        ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+            
+            var data=ReferralManagementDashboard.projectActivityChartData(item)
+            ReferralManagementDashboard.addChartNavigation(chart, data, team);
+            callback(data);
+            
+            var actions=0;
+            data.forEach(function(d){
+                actions+=d.value;
+            });
+            activityLabel.getElement().innerHTML=actions+' action'+(actions==1?' has':'s have')+' been recorded recently.';
+             
+        });
+        
+    }})
 
 return new ModuleArray([
     new ElementModule("label",{html:"Project Activity"}),
     activityLabel,
-    
-    
-    
-    
-    new BarChartModule({data:function(callback){
-        var data=ReferralManagementDashboard.projectActivityChartData(item)
-        callback(data);
-        
-        var actions=0;
-        data.forEach(function(d){
-            actions+=d.value;
-        });
-        activityLabel.getElement().innerHTML=actions+' action'+(actions==1?' has':'s have')+' been recorded recently.';
-        
-    }})
-
-    
-    
-    
-   
+    chart
 ],{"class":"inline-list-item timesheets-list-item-icon"});
