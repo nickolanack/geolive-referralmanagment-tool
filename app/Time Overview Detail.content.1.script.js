b7fc905da5b8c9cd81e25284f6eec2a7a2919a5c
nickolanack <nickblackwell82@gmail.com>
2018-03-12 13:23:45 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/Time Overview Detail.content.1.script.js
+++ app/Time Overview Detail.content.1.script.js
@@ -8,37 +8,10 @@
 
 var chart= new BarChartModule({data:function(callback){
     
-    //TODO: get team data
-    var data;
+
     ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-        data=ReferralManagementDashboard.projectActivityChartData(team, application);
-        
-    
-        
-    
-   
-        chart.addEvent('load', function(){
-           var nav= chart.getElement().appendChild(new Element('span', {"class":"nav"}));
-           nav.appendChild(new Element('button',{"class":"prev-btn", events:{
-               click:function(){
-                   console.log(data[0]);
-                   data=ReferralManagementDashboard.projectActivityChartData(team, application, {
-                       endAt:data[0].day
-                   });
-                   chart.redraw(data);
-               }
-           }}));
-           nav.appendChild(new Element('button',{"class":"next-btn", events:{
-               click:function(){
-                   console.log(data[data.length-1]);
-                   data=ReferralManagementDashboard.projectActivityChartData(team, application, {
-                       startAt:data[data.length-1].day
-                   });
-                   chart.redraw(data);
-               }
-           }}));
-        });
-        
+        var data=ReferralManagementDashboard.projectActivityChartData(team, application);
+        ReferralManagementDashboard.addChartNavigation(chart, data, team, application);
         callback(data);
         
         
