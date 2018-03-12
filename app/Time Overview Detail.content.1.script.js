31a3f57171c439cf49ee673ae695bab88e080c54
nickolanack <nickblackwell82@gmail.com>
2018-03-12 12:46:09 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/Time Overview Detail.content.1.script.js
+++ app/Time Overview Detail.content.1.script.js
@@ -14,29 +14,31 @@ var chart= new BarChartModule({data:function(callback){
         data=ReferralManagementDashboard.projectActivityChartData(team, application);
         callback(data);
     
-    });
+  
     
    
-    chart.runOnceOnLoad(function(){
-       var nav= chart.getElement().appendChild(new Element('span', {"class":"nav"}));
-       nav.appendChild(new Element('button',{"class":"prev-btn", events:{
-           click:function(){
-               console.log(data[0]);
-               data=ReferralManagementDashboard.projectActivityChartData(team, application, {
-                   endAt:parseInt(data[data.length-1].day)
-               });
-               chart.redraw(data);
-           }
-       }}));
-       nav.appendChild(new Element('button',{"class":"next-btn", events:{
-           click:function(){
-               console.log(data[data.length-1]);
-               data=ReferralManagementDashboard.projectActivityChartData(team, application, {
-                   startAt:parseInt(data[data.length-1].day)
-               });
-               chart.redraw(data);
-           }
-       }}));
+        chart.runOnceOnLoad(function(){
+           var nav= chart.getElement().appendChild(new Element('span', {"class":"nav"}));
+           nav.appendChild(new Element('button',{"class":"prev-btn", events:{
+               click:function(){
+                   console.log(data[0]);
+                   data=ReferralManagementDashboard.projectActivityChartData(team, application, {
+                       endAt:parseInt(data[data.length-1].day)
+                   });
+                   chart.redraw(data);
+               }
+           }}));
+           nav.appendChild(new Element('button',{"class":"next-btn", events:{
+               click:function(){
+                   console.log(data[data.length-1]);
+                   data=ReferralManagementDashboard.projectActivityChartData(team, application, {
+                       startAt:parseInt(data[data.length-1].day)
+                   });
+                   chart.redraw(data);
+               }
+           }}));
+        });
+    
     });
     
 }});
