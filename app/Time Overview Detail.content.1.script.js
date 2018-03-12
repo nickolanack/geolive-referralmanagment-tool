35d43bd16b8ecd179af069e02e68fd297248822c
nickolanack <nickblackwell82@gmail.com>
2018-03-12 12:40:38 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/Time Overview Detail.content.1.script.js
+++ app/Time Overview Detail.content.1.script.js
@@ -9,18 +9,26 @@
 var chart= new BarChartModule({data:function(callback){
     
     //TODO: get team data
-    
+    var data;
     ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-        
-        callback(ReferralManagementDashboard.projectActivityChartData(team, application));
+        data=ReferralManagementDashboard.projectActivityChartData(team, application);
+        callback(data);
     
     });
     
    
     chart.runOnceOnLoad(function(){
        var nav= chart.getElement().appendChild(new Element('span', {"class":"nav"}));
-       nav.appendChild(new Element('button',{"class":"prev-btn"}));
-       nav.appendChild(new Element('button',{"class":"next-btn"}));
+       nav.appendChild(new Element('button',{"class":"prev-btn", events:{
+           click:function(){
+               console.log(data[0])
+           }
+       }}));
+       nav.appendChild(new Element('button',{"class":"next-btn", events:{
+           click:function(){
+               console.log(data[data.length-1]);
+           }
+       }}));
     });
     
 }});
