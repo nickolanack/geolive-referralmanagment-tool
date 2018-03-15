1628722ee3b229306b37427f51827132653d445a
nickolanack <nickblackwell82@gmail.com>
2018-03-15 11:12:09 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/TUS Crowdsource app config.formatOutputScript.js
+++ app/TUS Crowdsource app config.formatOutputScript.js
@@ -3,6 +3,7 @@
 $parameters['client']=GetClient()->getUserMetadata();
 $parameters['client']['role-icon']=GetPlugin('ReferralManagement')->getUserRoleIcon();
 $parameters['client']['user-icon']=GetPlugin('ReferralManagement')->getUserRoleLabel();
-$parameters['client']['community']=GetPlugin('ReferralManagement')->canCreateCommunityContent();
+$parameters['client']['create']=GetPlugin('ReferralManagement')->canCreateCommunityContent();
+$parameters['client']['community']=GetPlugin('ReferralManagement')->getCommunity();
 
 return $parameters;
