e71df5975b70cdc1d2ee24a4b5fe95d0ba817ea7
nickolanack <nickblackwell82@gmail.com>
2018-03-19 10:54:13 -0700

    -- Auto Commit - Changes made through Frontend Interface

---
+++ app/widget.AjaxMethod.236.customScript.js
@@ -0,0 +1,54 @@
+$client=GetClient()->getUserId();
+if($client<=0){
+    return false;
+}
+
+$profileImageHtml=$json->profile;
+
+if(empty($profileImageHtml)){
+    return false;
+}
+
+GetPlugin('Attributes');
+(new attributes\Record('deviceUserAttributes'))
+    ->setValues($client, 'user', array(
+        'profileImage'=>$profileImageHtml,
+        'profileName'=>$json->name
+    ));
+    
+   
+
+
+$parameters=array();
+$parameters['client']=($client=GetClient()->getUserMetadata());
+$parameters['account-authorized']=false;
+$parameters['account-profile-image']=GetWidget('mobile-app-config')->getParameter('profile-image');
+if($client['id']>0){
+    GetPlugin('Attributes');
+    $attributes=(new attributes\Record('deviceUserAttributes'))->getValues($client['id'],'user');
+    $parameters['client']['attributes']= $attributes;
+    
+    if(!empty($attributes['authEmail'])&&$attributes['authEmail']===$attributes['authorizedEmail']){
+        $parameters['account-authorized']=true;
+    }
+    
+    
+    if(!empty($attributes['profileImage'])){
+        $parameters['account-profile-image']=array($attributes['profileImage']);
+    }
+    
+    $parameters['account-name']=GetClient()->getRealName();
+    if(!empty($attributes['profileName'])){
+        $parameters['account-name']=$attributes['profileName'];
+    }
+    
+    
+}
+
+
+
+
+ return array(
+            "text"=>"Your profile has been updated",
+            "parameters"=>$parameters
+        );
