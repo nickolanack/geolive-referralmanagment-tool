7b80ef82382dab95f195399699b2e0f32a52ccfc
nickolanack <nickblackwell82@gmail.com>
2018-03-19 11:21:00 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/saveProfile.customScript.js
+++ app/saveProfile.customScript.js
@@ -19,22 +19,9 @@ GetPlugin('Attributes');
    
 
 
-$parameters=array();
-
-
-$parameters['client']=GetClient()->getUserMetadata();
-
-$ref=GetPlugin('ReferralManagement');
-
-$parameters['client']['role-icon']=$ref->getUserRoleIcon();
-$parameters['client']['user-icon']=$ref->getUserRoleLabel();
-$parameters['client']['can-create']=$ref->canCreateCommunityContent();
-$parameters['client']['community']=$ref->getCommunity();
-$parameters['client']['avatar']=$ref->getUsersAvatar();
-
 
 
  return array(
             "text"=>"Your profile has been updated",
-            "parameters"=>$parameters
+            "parameters"=>GetPlugin('ReferralManagement')->getUsersMetadata()
         );
