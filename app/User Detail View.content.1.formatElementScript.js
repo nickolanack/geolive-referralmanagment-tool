cb0ba0a077b7792bf91a2c236c6743ff9bf2d991
nickolanack <nickblackwell82@gmail.com>
2018-03-20 01:16:57 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/User Detail View.content.1.formatElementScript.js
+++ app/User Detail View.content.1.formatElementScript.js
@@ -1,2 +1,3 @@
 /* turn the entire element (el) into a form button*/
 new UIModalFormButton(el.parentNode, application, AppClient, {"formName":"userProfileForm", "formOptions":{template:"form"}});
+el.addClass('editable');
