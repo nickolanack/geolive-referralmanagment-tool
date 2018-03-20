faeb37b1a588d059b584699455f2011f1e09246d
nickolanack <nickblackwell82@gmail.com>
2018-03-20 10:00:02 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/loginForm.1.content.4.script.js
+++ app/loginForm.1.content.4.script.js
@@ -1,17 +1,7 @@
-var login= new Element('label', {
-    html:'Register to create an account', 'class':'login-button-text', 
-    style:"text-align:left; color: mediumseagreen",
-    events:{
-        click:function(){
-            //goto next step
-            wizard.displayNext();
-        }
-}});
-//login.appendChild(new Element('br'));
-login.appendChild(new Element('button',{
-    html:'Register',
-    style:"background-color:mediumseagreen;",
+var map==new Element('button',{
+    html:'View Map',
+    style:"",
     "class":"primary-btn"
     
-}));
-return login;
+});
+return map;
--- app/loginForm.1.content.5.script.js
+++ app/loginForm.1.content.5.script.js
@@ -1,7 +1,17 @@
-var map==new Element('button',{
-    html:'View Map',
-    style:"",
+var login= new Element('label', {
+    html:'Register to create an account', 'class':'login-button-text', 
+    style:"text-align:left; color: mediumseagreen",
+    events:{
+        click:function(){
+            //goto next step
+            wizard.displayNext();
+        }
+}});
+//login.appendChild(new Element('br'));
+login.appendChild(new Element('button',{
+    html:'Register',
+    style:"background-color:mediumseagreen;",
     "class":"primary-btn"
     
-});
-return map;
+}));
+return login;
