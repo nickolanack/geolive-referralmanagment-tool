677ab285d00e7cc95e6b41411c7f4e36e44da366
nickolanack <nickblackwell82@gmail.com>
2018-03-20 10:52:05 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/layerItemListViewDetail.content.1.script.js
+++ app/layerItemListViewDetail.content.1.script.js
@@ -1,6 +1,7 @@
 <?php 
     //$base=GetPlugin('Maps')->urlForView('mapitem.staticmap');
+    //return new ElementModule("img",{ src:"<?php echo $base;?>&mapitem="+item.id,"class":"static-map"});
 ?>
 
-//return new ElementModule("img",{ src:"<?php echo $base;?>&mapitem="+item.id,"class":"static-map"});
+
 return new ElementModule("img",{ src:item.style,"class":"static-map"});
