c9b960617074500380d10f08288336cc5315a325
nickolanack <nickblackwell82@gmail.com>
2018-03-20 09:59:45 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/loginForm.1.content.5.script.js
+++ app/loginForm.1.content.5.script.js
@@ -1,17 +1,7 @@
-var login= new Element('label', {
-    html:'View Public Map', 'class':'login-button-text', 
-    style:"text-align:left; color: mediumseagreen",
-    events:{
-        click:function(){
-            //goto next step
-           
-        }
-}});
-//login.appendChild(new Element('br'));
-login.appendChild(new Element('button',{
+var map==new Element('button',{
     html:'View Map',
     style:"",
     "class":"primary-btn"
     
-}));
-return login;
+});
+return map;
