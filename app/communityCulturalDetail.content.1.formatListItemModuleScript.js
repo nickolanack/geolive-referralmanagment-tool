44a1a0d502775d24698c74b3c96a481afe67cc2f
nickolanack <nickblackwell82@gmail.com>
2018-03-20 11:19:26 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/communityCulturalDetail.content.1.formatListItemModuleScript.js
+++ app/communityCulturalDetail.content.1.formatListItemModuleScript.js
@@ -1,3 +1,9 @@
 childView.getElement().addEvent('click', function(){
     console.log(child);
+    
+    if(window.GeoliveMapInstances&&window.GeoliveMapInstances.length){
+        
+        GeoliveSearch.SearchAndOpenMapItem(GeoliveMapInstances[0], child.id, child.lid);
+
+    }
 })
