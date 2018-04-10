6576bf44076ee91d5e902fd5f7416b8d4243e977
nickolanack <nickblackwell82@gmail.com>
2018-04-10 15:13:20 -0700

    -- Auto Commit - Changes made through Frontend Interface

---
+++ app/singleProjectFilesDetail.content.1.formatModuleScript.js
@@ -0,0 +1,9 @@
+
+
+new AjaxFileUploader(module.getElement(),{
+        types:["document"],
+        selectFile:function(fileinfo, type){
+            console.log(fileinfo);
+            console.log(type);
+        }
+    })
