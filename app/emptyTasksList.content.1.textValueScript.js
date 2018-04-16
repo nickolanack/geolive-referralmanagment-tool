bfd02335e2cc7b9a8b9121fb788448f0f66f2f24
nickolanack <nickblackwell82@gmail.com>
2018-04-16 14:57:45 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/emptyTasksList.content.1.textValueScript.js
+++ app/emptyTasksList.content.1.textValueScript.js
@@ -1,1 +1,1 @@
-callback("<p>Manage your proposals through tasks. blurb... </p>");
+callback("<p>Manage your proposals through tasks. this will be a quickstart help guide with videos section... </p>");
