a4b7f506eb840764aa1ddf847ec27fad6e80b8b5
nickolanack <nickblackwell82@gmail.com>
2018-04-16 13:30:04 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/emptyTasksList.content.1.textValueScript.js
@@ -0,0 +1,10 @@
+ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+    var proposals=team.getProposals();
+    var tasks=[];
+    proposals.forEach(function(prop){
+        tasks=tasks.concat(prop.getTasks());
+    })
+    callback(tasks.length==0?
+        "You currently have no tasks, to get started, create a project. Watch this video https://www.youtube.com/watch?v=1IkY0_qONRk":
+        "");
+ })
