3e9ef6e0fb3cdcfa2c09c58c1add07d8fb3d39db
nickolanack <nickblackwell82@gmail.com>
2018-04-17 08:22:13 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/Tasks Overview List Detail.content.3.namedViewScript.js
@@ -0,0 +1,13 @@
+
+ ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+    var proposals=team.getProposals();
+    var tasks=[];
+    proposals.forEach(function(prop){
+        tasks=tasks.concat(prop.getTasks());
+    })
+    if(tasks.length==0){
+        callback(namedView);
+    }
+    callback('emptyListView');
+ })
+
