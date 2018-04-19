6d1eb9a76d11d9444c0ff5b988d20861957105af
nickolanack <nickblackwell82@gmail.com>
2018-04-19 10:21:55 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Display Proposal Spatial Files.customScript.js
+++ app/Display Proposal Spatial Files.customScript.js
@@ -4,20 +4,16 @@
 
 ?>
 
-SpatialDocumentPreview.setClearTile(tile, control);
+SpatialDocumentPreview.setTile(tile, control);
 SpatialDocumentPreview.setMap(map);
 
-tile.disable();
 
-tile.addEvent('click',function(){
-    
-    
-    
-});
 var getFiles=window.GetSpatialFiles||window.parent.GetSpatialFiles
 if(getFiles){
     var files = getFiles();
     if(files.length){
         SpatialDocumentPreview.show(files);
+    }else{
+        tile.disable();
     }
 }
