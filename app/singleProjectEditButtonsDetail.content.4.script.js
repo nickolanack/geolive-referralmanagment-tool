120a80e73303d097751e584b1caf8cbd973907eb
nickolanack <nickblackwell82@gmail.com>
2018-04-20 14:33:17 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectEditButtonsDetail.content.4.script.js
+++ app/singleProjectEditButtonsDetail.content.4.script.js
@@ -4,7 +4,7 @@ if(item.isArchived()){
     return null;
 }
 
-return new Element('button',{"html":"Share", "class":"primary-btn", "style":"background-color:coral;" "events":{"click":function(){
+return new Element('button',{"html":"Share", "class":"primary-btn", "style":"background-color:coral;", "events":{"click":function(){
     
     
    
