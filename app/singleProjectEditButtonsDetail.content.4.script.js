8a23325cdd9500c26be320e5cd9b566c9517c041
nickolanack <nickblackwell82@gmail.com>
2018-04-20 14:32:40 -0700

    -- Auto Commit - Changes made through Frontend Interface

---
+++ app/singleProjectEditButtonsDetail.content.4.script.js
@@ -0,0 +1,12 @@
+/*Share Button*/
+
+if(item.isArchived()){
+    return null;
+}
+
+return new Element('button',{"html":"Share", "class":"primary-btn", "style":"background-color:coral;" "events":{"click":function(){
+    
+    
+   
+
+}}})
