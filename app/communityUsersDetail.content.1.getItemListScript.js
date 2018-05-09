0b600a0b7ff004db55d9c990ed388fdb950a1694
nickolanack <nickblackwell82@gmail.com>
2018-05-09 15:42:46 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/communityUsersDetail.content.1.getItemListScript.js
+++ app/communityUsersDetail.content.1.getItemListScript.js
@@ -1,6 +1,6 @@
 return ProjectTeam.CurrentTeam().getAllUsers().filter(function(u){
     var roles=u.getRoles();
-    if(roles.length==0||roles[0]=="community-member"){
+    if(roles.length==0||roles[0]=="community-member"||roles[0]=="none"){
         return true;
     }
     return false;
