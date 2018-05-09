ee2488ccd3001f3e777224412b0a52b646e25865
nickolanack <nickblackwell82@gmail.com>
2018-05-09 16:07:42 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/communityUsersDetail.content.1.textValueScript.js
+++ app/communityUsersDetail.content.1.textValueScript.js
@@ -1,1 +1,5 @@
-callback("<p>When users log in for the first time there roll must be approved by a lands department member or manager. This page will show all the users who have not yet been approved</p>");
+callback(
+    "<p>When users log in for the first time there roll must be approved by a lands department member or manager. "+
+    "This page shows all the community members as well as users who have not yet been assigned to any roll. "+
+    "Lands department members can approve users as community members, while Lands department managers can also "+
+    "add users to the lands department team"+"</p>");
