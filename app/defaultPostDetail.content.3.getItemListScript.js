168c670e7f9aca9a6b7d536c90f49f8a90ecd3c8
nickolanack <nickblackwell82@gmail.com>
2018-05-10 15:50:59 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/defaultPostDetail.content.3.getItemListScript.js
+++ app/defaultPostDetail.content.3.getItemListScript.js
@@ -1,34 +1,41 @@
 
-
-if(item.getMetadata().items){
-   return item.getMetadata().items.map(function(i){
-       
-       var type=i.type;
-       
-       if(type=='ReferralManagement.team'){
-           return ProjectTeam.CurrentTeam();
-       }
-       if(type=='Tasks.task'){
-           return ProjectTeam.CurrentTeam().getTask(i.id);
-       }
-       if(type=='ReferralManagement.proposal'){
-           return ProjectTeam.CurrentTeam().getProposal(i.id);
-       }
-       if(type=='User'){
-           try{
-               return ProjectTeam.CurrentTeam().getUserOrDevice(i.id);
-           }catch(e){
-                
-                return new MockDataTypeItem({
-                    type:"user"
-                })
-               
+ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+    
+    if(item.getMetadata().items){
+      callback(item.getMetadata().items.map(function(i){
+           
+           var type=i.type;
+           
+           if(type=='ReferralManagement.team'){
+               return team;
            }
-       }
-       
-       
-       return i;
-   })
-}
+           if(type=='Tasks.task'){
+               return team.getTask(i.id);
+           }
+           if(type=='ReferralManagement.proposal'){
+               return team.getProposal(i.id);
+           }
+           if(type=='User'){
+               try{
+                   return team.getUserOrDevice(i.id);
+               }catch(e){
+                    
+                    return new MockDataTypeItem({
+                        type:"user"
+                    })
+                   
+               }
+           }
+           
+           
+           return i;
+       }))
+    }
 
-return [];
+callback([]);
+    
+    
+    
+    
+    
+})
