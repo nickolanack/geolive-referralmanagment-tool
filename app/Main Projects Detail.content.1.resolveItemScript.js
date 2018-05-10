fd875c4c6faa7ba8f67af20a835b3930678bb091
nickolanack <nickblackwell82@gmail.com>
2018-05-10 16:14:43 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Main Projects Detail.content.1.resolveItemScript.js
+++ app/Main Projects Detail.content.1.resolveItemScript.js
@@ -3,12 +3,15 @@ ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
     var current=application.getNamedValue("currentProject");    
     if(current){
         callback(current);
+        return;
     }
     
     var projects=team.getProjects();
     if(projects.length){
         callback(projects[0]);
+        return;
     }
+    
     callback([]);
 })
 
