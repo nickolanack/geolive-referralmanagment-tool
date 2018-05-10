e40c06f0dc6dd39c99b03f875d09976b993e37cb
nickolanack <nickblackwell82@gmail.com>
2018-05-10 14:12:04 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Left Panel.content.1.formatModuleScript.js
+++ app/Left Panel.content.1.formatModuleScript.js
@@ -1,3 +1,6 @@
-module.addWeakEvent(item, 'update', function(){
-    module.redraw();
+
+ProjectTeam.CurrentTeam().getUser(AppClient.getId(), function(user){
+    module.addWeakEvent(user, 'update', function(){
+        module.redraw();
+    })
 })
