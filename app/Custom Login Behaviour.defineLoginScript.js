84c7eb84af1eb6d883bfb7bf5351d2954617c41f
nickolanack <nickblackwell82@gmail.com>
2018-05-14 14:01:01 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Custom Login Behaviour.defineLoginScript.js
+++ app/Custom Login Behaviour.defineLoginScript.js
@@ -4,4 +4,5 @@ return function(){
 $$('.public-map').setStyle('opacity',0.1);
 $$('.public-map').setStyle('pointer-events','none');
 $$('.login-form').setStyle('display',null);
+$$('.public-menu').setStyle('display',null);
 }
