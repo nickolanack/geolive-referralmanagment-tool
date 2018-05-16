d631d022b76b652837f92e577fa8bf26de4431ef
nickolanack <nickblackwell82@gmail.com>
2018-05-16 08:48:29 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/singleProjectOverviewDetail.content.3.script.js
+++ app/singleProjectOverviewDetail.content.3.script.js
@@ -4,6 +4,6 @@ var d=item.getDaysUntilDeadline();
 
 return new ModuleArray([
     new ElementModule("label",{html:"Deadline"}),
-    new ElementModule("div",{html:item.hasDeadline()?(d+" day"+(d==1?"":"s")+" left until deadline, "+item.getDeadlineDate()):"There is no deadline set for this project", "class":"percent-complete-value"}),
+    new ElementModule("div",{html:item.hasDeadline()?(d+" day"+(d==1?"":"s")+" left until deadline, "+moment(item.getDeadlineDate()).format("MMM Do")):"There is no deadline set for this project", "class":"percent-complete-value"}),
     new ProgressBarModule({value:function(){ return item.getPercentTimeComplete(); },"class":"percent-complete reverse-tint"})
 ],{"class":"progress"});
