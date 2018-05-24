8c4bef98ba72d6b11d87873324f0751eb6eaaf3c
nickolanack <nickblackwell82@gmail.com>
2018-05-24 09:33:55 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/shareLink.1.content.1.script.js
+++ app/shareLink.1.content.1.script.js
@@ -5,4 +5,4 @@
 
 
         var token="abcdefg";
-        return new ElementModule('div' {html:application.getNamedValue('projectMenuController').getUrl()+'/pt'+token});
+        return new ElementModule('div', {html:application.getNamedValue('projectMenuController').getUrl()+'/pt'+token});
