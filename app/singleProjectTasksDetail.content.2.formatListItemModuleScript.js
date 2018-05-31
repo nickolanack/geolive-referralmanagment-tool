ce0b50e807826dd26af91b4ffd6c04781289dc55
nickolanack <nickblackwell82@gmail.com>
2018-05-31 16:54:49 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/singleProjectTasksDetail.content.2.formatListItemModuleScript.js
+++ app/singleProjectTasksDetail.content.2.formatListItemModuleScript.js
@@ -25,6 +25,21 @@ childView.addEvent('load:once',function(){
         
     })
     
+    
+    childView.addWeakEvent(child, "saving", function(){
+        childView.startSpinner();
+    });
+    childView.addWeakEvent(child, "change", function(){
+        if(listModule.applyFilterToItem(child)){
+             childView.redraw();
+             return;
+        }
+        
+        //no longer passes filter
+        childView.remove();
+       
+    });
+    
 })
 
 childView.addWeakEvent(child, 'remove',function(){
