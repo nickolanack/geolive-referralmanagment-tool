2b9804b44c93c099cd1ad40871e04eba98f8449f
nickolanack <nickblackwell82@gmail.com>
2018-06-01 09:41:59 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Tasks Overview List Detail.content.2.formatListItemModuleScript.js
+++ app/Tasks Overview List Detail.content.2.formatListItemModuleScript.js
@@ -10,29 +10,7 @@ if(child.getItem().getType()==="user"){
 }
 
 childView.addEvent('load:once',function(){
-    childView.getElement().addEvent("click",function(){
-        
-        
-        var formName="taskForm";
-
-			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
-			if ((typeof wizardTemplate) != 'function') {
-
-				if(window.console&&console.warn){
-					console.warn('Expected named wizardTemplate: '+formName+', to exist');
-				}
-
-			}
-			var modalFormViewController =  new PushBoxModuleViewer(application, {});
-			var wizard = wizardTemplate(child, {});
-            wizard.buildAndShow(modalFormViewController, {template:"form"}); 
-        
-        
-        
-            // wizard.addEvent("complete",function(){
-            //     childView.redraw();
-            // })
-    })
+    
     childView.addWeakEvent(child, "saving", function(){
         childView.startSpinner();
     })
