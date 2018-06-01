2004e6ad3964d998c15b3fe562e858b78bb3104c
nickolanack <nickblackwell82@gmail.com>
2018-06-01 09:40:55 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Single Task List Item Detail.content.1.formatElementScript.js
+++ app/Single Task List Item Detail.content.1.formatElementScript.js
@@ -4,3 +4,23 @@
    }
    
  el.addClass('editable');
+ 
+ 
+el.addEvent("click",function(){
+        
+        
+        var formName="taskForm";
+
+			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
+			if ((typeof wizardTemplate) != 'function') {
+
+				if(window.console&&console.warn){
+					console.warn('Expected named wizardTemplate: '+formName+', to exist');
+				}
+
+			}
+			var modalFormViewController =  new PushBoxModuleViewer(application, {});
+			var wizard = wizardTemplate(item, {});
+            wizard.buildAndShow(modalFormViewController, {template:"form"}); 
+        
+    })
