8187115020ac01aad4239382f530f8a00eb42ca6
nickolanack <nickblackwell82@gmail.com>
2018-06-01 08:20:50 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/synopsisProgressDetail.content.2.script.js
+++ app/synopsisProgressDetail.content.2.script.js
@@ -13,7 +13,10 @@ var compute=function(team){
         anchor:UIPopover.AnchorAuto()
     });
     
-    var tasks=team.getTasks().filter(function(t){return t.isStarred()&&(!t.isComplete());  });
+    var tasks=team.getTasks().filter(function(t){
+        return t.isStarred();//&&(!t.isComplete());  
+        
+    });
     var l=tasks.length;
     module.getElement().innerHTML='You have <span class="counter">'+l+'</span> starred task'+(l==1?"":"s")+'.';
     module.getElement().addEvents(ReferralManagementDashboard.taskHighlightMouseEvents(tasks))
