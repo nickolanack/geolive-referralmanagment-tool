4a25906e7b9de224aeed721ab66b527a9a623d83
nickolanack <nickblackwell82@gmail.com>
2018-06-01 09:43:11 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/projectPriorityTasksDetail.content.1.formatListItemModuleScript.js
+++ app/projectPriorityTasksDetail.content.1.formatListItemModuleScript.js
@@ -1,29 +1,5 @@
 childView.addEvent('load:once',function(){
-    childView.getElement().addEvent("click",function(){
-        
-        
-        var formName="taskForm";
-
-			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
-			if ((typeof wizardTemplate) != 'function') {
-
-				if(window.console&&console.warn){
-					console.warn('Expected named wizardTemplate: '+formName+', to exist');
-				}
-
-			}
-			var modalFormViewController =  new PushBoxModuleViewer(application, {});
-			var wizard = wizardTemplate(child, {});
-            wizard.buildAndShow(modalFormViewController, {template:"form"}); 
-        
-        
-        
-            wizard.addEvent("complete",function(){
-                childView.redraw();
-            })
-        
-        
-    })
+    
     
 })
 
