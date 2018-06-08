e48fb4841ccf482170e1d78b1e5973515075ad28
nickolanack <nickblackwell82@gmail.com>
2018-06-08 12:07:49 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/singleTaskWithFilesDetail.content.1.filterModulesScript.js
+++ app/singleTaskWithFilesDetail.content.1.filterModulesScript.js
@@ -2,11 +2,17 @@
 list.content.push(new ElementModule('button',{
     "class":"remove-btn",
     events:{click:function(){
-        if(confirm("Are you sure")){
-            alert("do");
-            return;
-        }
-        alert("don't");
+        if(confirm("Are you sure you want to remove this file")){
+            (new AjaxControlQuery(CoreAjaxUrlRoot, 'file_metadata', {
+				'file': listItem,
+				'show': ['iconsetDetails']
+			})).addEvent('onSuccess', function(response) {
+			    
+			    
+			   
+			}).execute();
+           
+        };
     }}
 }))
 list.content.push((new ModalFormButtonModule(application, new MockDataTypeItem({file:listItem}),{
