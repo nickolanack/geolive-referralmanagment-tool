6ff03a303ffea9e4dd227d190b14a74599ec88f1
nickolanack <nickblackwell82@gmail.com>
2018-06-08 11:30:24 -0700

    -- Auto Commit - Changes made through Frontend Interface

--- app/singleTaskWithFilesDetail.content.1.filterModulesScript.js
+++ app/singleTaskWithFilesDetail.content.1.filterModulesScript.js
@@ -9,5 +9,16 @@ list.content.push(new ElementModule('button',{
         alert("don't");
     }}
 }))
+list.content.push((new ModalFormButtonModule(application, task,{
+        label:"Edit",
+        formName:"fileForm",
+        formOptions:{
+            template:"form"
+        },
+        hideText:true,
+        "class":"inline-btn add primary-btn"
+    })).addEvent("show",function(){
+        
+    }))
 
 return list
