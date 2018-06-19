c1f8667bdb388429f857184f42c5f329205848a3
nickolanack <nickblackwell82@gmail.com>
2018-06-19 14:36:49 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Single Task List Item Detail.content.8.script.js
+++ app/Single Task List Item Detail.content.8.script.js
@@ -1,5 +1,56 @@
-return function(viewer, element, parentModule){
-    viewer.addEvent('load:once',function(){
-        viewer.getUIView().getElement().addClass("task-item-"+item.getId());
-    })
+var tags=item.getTags();
+if(tags.length){
+    
+    var classMap=function(type){
+        if(type=="ReferralManagement.proposal"){
+            return "menu-main-projects";
+        }
+        
+        if(type=="ReferralManagement.client"){
+            return "menu-people-clients";
+        }
+        
+        return "";
+    }
+    
+    var ul=new ElementModule('ul', {"class":"item-tags"});
+    tags.forEach(function(t){
+        ul.appendChild(new Element('li',{html:t.getName(), 
+            "class":classMap(t.getType()),
+            events:{click:function(e){
+                e.stop();
+                
+                
+                
+                
+                
+                
+                //var application=childView.getApplication()
+        var controller=application.getNamedValue('navigationController');
+        var view=controller.getCurrentView();
+        console.log(view);
+        
+            
+        
+       
+                
+                
+                
+                
+                
+                if(t.getType()==='ReferralManagement.proposal'){
+                    application.setNamedValue("currentProject", t);
+                    controller.navigateTo("Projects", "Main");
+                }
+                if(t.getType()==='ReferralManagement.client'){
+                    application.setNamedValue("currentClient", t);
+                    controller.navigateTo("Clients", "People");
+                }
+            }}
+            
+        }));
+    });
+    return ul;
 }
+
+return null;
