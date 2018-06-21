9174385e983edd083163c2071fa5846bec1f3f4e
nickolanack <nickblackwell82@gmail.com>
2018-06-21 13:32:59 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/landsDepartmentMemberForm.1.content.1.formatElementScript.js
+++ app/landsDepartmentMemberForm.1.content.1.formatElementScript.js
@@ -1,7 +1,7 @@
 checkbox.addEvent('change', function(){
     if(checkbox.checked){
-        $$("ui-view.dashboard-main")[0].addClass("dark");
+        $$(".ui-view.dashboard-main")[0].addClass("dark");
         return;
     }  
-     $$("ui-view.dashboard-main")[0].removeClass("dark");
+     $$(".ui-view.dashboard-main")[0].removeClass("dark");
 })
