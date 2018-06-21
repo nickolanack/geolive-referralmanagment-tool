ae1324e18b18d97d85073269d10d051ef653480e
nickolanack <nickblackwell82@gmail.com>
2018-06-21 13:32:08 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/landsDepartmentMemberForm.1.content.1.formatElementScript.js
+++ app/landsDepartmentMemberForm.1.content.1.formatElementScript.js
@@ -1,4 +1,4 @@
-checkbox.addEvent('change'.function(){
+checkbox.addEvent('change', function(){
     if(checkbox.checked){
         $$("ui-view.dashboard-main")[0].addClass("dark");
         return;
