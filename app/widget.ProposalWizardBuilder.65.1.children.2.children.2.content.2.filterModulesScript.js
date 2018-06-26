151fda6a67e2e26af79d9384b9281c77db05a659
nickolanack <nickblackwell82@gmail.com>
2018-06-26 12:24:08 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/widget.ProposalWizardBuilder.65.1.children.2.children.2.content.2.filterModulesScript.js
@@ -0,0 +1,16 @@
+if(item.getId()<=0||item.getAvailableUsers().length<=0){
+    return list;
+}
+
+var insert=(new ModalFormButtonModule(application, item,{
+        label:"Assign To",
+        formName:"userSelectionForm",
+        formOptions:{
+            template:"form"
+        },
+        //hideText:true,
+        "class":"primary-btn"
+    }));
+list.content.push(insert)
+
+return list
