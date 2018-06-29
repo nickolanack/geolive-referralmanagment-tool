80dacfee889a16eff42820a187ade98219379001
nickolanack <nickblackwell82@gmail.com>
2018-06-29 10:58:40 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

---
+++ app/Single Task List Item Detail.content.1.formatModuleScript.js
@@ -0,0 +1,11 @@
+
+
+    (new UIModalFormButton(module.getElement(),application, item,{
+    
+        formName:"userSelectionForm",
+        formOptions:{
+            template:"form"
+        }
+       
+    }));
+
