140262965743af9914589a34ec54f6e57bdc699f
nickolanack <nickblackwell82@gmail.com>
2018-06-30 19:57:01 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Tasks Overview List Detail.content.2.getItemListScript.js
+++ app/Tasks Overview List Detail.content.2.getItemListScript.js
@@ -1,11 +1,6 @@
  
   ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-    var proposals=team.getProposals();
-    var tasks=[];
-    proposals.forEach(function(prop){
-        tasks=tasks.concat(prop.getTasks());
-    })
-    callback(tasks)
+    callback(team.getIncompleteTasks());
  })
  
  
