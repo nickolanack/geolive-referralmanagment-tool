93908ca4f300313c5e27fb6ee272b947a969891c
nickolanack <nickblackwell82@gmail.com>
2018-06-30 20:06:45 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/synopsisProgressDetail.content.4.script.js
+++ app/synopsisProgressDetail.content.4.script.js
@@ -35,7 +35,7 @@ var compute=function(team){
             var filter=application.getNamedValue("taskListFilter");
             if(filter){
             
-                filter.applyFilter("complete", true)
+                filter.applyFilter(null, false)
             
             }
         });
