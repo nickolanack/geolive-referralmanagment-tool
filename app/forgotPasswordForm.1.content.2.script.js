67d877adb46cc978df79dfaf5a3855f68e3d8203
nickolanack <nickblackwell82@gmail.com>
2018-07-01 00:09:36 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/forgotPasswordForm.1.content.2.script.js
+++ app/forgotPasswordForm.1.content.2.script.js
@@ -1,17 +1,21 @@
 
 var link=new ElementModule('label',{
+    "class":"label-block,"
         html:"Send me a magic login link"
     });
         
 link.getElement().appendChild(new Element('button',{
+    "class":"btn WizardButton,"
     html:'Send Link'
 }))
         
 var setPwd=new ElementModule('label',{
+        "class":"label-block,"
         html:"Send me a magic login link"
     });    
         
 setPwd.getElement().appendChild(new Element('button',{
+        "class":"btn WizardButton,"
         html:'Change Password'
     }))
         
