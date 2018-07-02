7911f85cc4358b240ce1cb44101b11bf8dc2eee8
nickolanack <nickblackwell82@gmail.com>
2018-07-02 16:46:47 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/singleUserListItemDetail.content.1.formatElementScript.js
+++ app/singleUserListItemDetail.content.1.formatElementScript.js
@@ -1,9 +1,10 @@
 
 
-if(AppClient.getUserType()=="admin"||item.getId()==AppClient.getId())
-new UIModalFormButton(el, application, item, {"formName":"userProfileForm", "formOptions":{template:"form"}});
-el.addClass('editable');
-
-if(el.previousSibling){
-    new UIModalFormButton(el.previousSibling, application, item, {"formName":"userProfileForm", "formOptions":{template:"form"}});
+if(AppClient.getUserType()=="admin"||item.getId()==AppClient.getId()){
+    new UIModalFormButton(el, application, item, {"formName":"userProfileForm", "formOptions":{template:"form"}});
+    el.addClass('editable');
+    
+    if(el.previousSibling){
+        new UIModalFormButton(el.previousSibling, application, item, {"formName":"userProfileForm", "formOptions":{template:"form"}});
+    }
 }
