0ba43a83ac9393a87fc69bfe05e99ba2bb4e36b5
nickolanack <nickblackwell82@gmail.com>
2018-07-05 19:06:53 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/loginForm.1.content.3.forgotPasswordCustomScript.js
+++ app/loginForm.1.content.3.forgotPasswordCustomScript.js
@@ -1,6 +1,36 @@
+
+var LoginItem=new Class({
+    Extends:DataTypeItem,
+    Implements:Events,
+    setSendMagicLink:function(){
+        me._task="send_magic_link";
+    },
+    setSendReset:function(){
+         me._task="send_magic_link";
+        
+    },
+    setEmail:function(e){
+        me._email=e;
+    },
+    save:function(cb){
+        
+        var me=this;
+        
+        (new AjaxControlQuery(CoreAjaxUrlRoot, me._task, {
+		  'plugin': "Users",
+		  'email':me._email
+		})).addEvent('success',function(){
+		    cb(true);
+		}).execute(); 
+		
+		
+
+    }
+})
+
 application.getDisplayController().displayPopoverForm(
 				'forgotPasswordForm', 
-				item, 
+				new LoginItem(), 
 				application,
 				{
 				    template:"form"
