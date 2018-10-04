66976874d5031675a13e01177efc5687b05bc22f
nickolanack <nickblackwell82@gmail.com>
2018-10-04 11:25:56 -0700

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/defaultPostDetail.content.1.script.js
+++
@@ -1,8 +0,0 @@
-
-var defaultIcon=<?php 
-    echo json_encode(UrlFrom(GetWidget('dashboardConfig')->getParameter('defaultUserImage')[0])); 
-?>;
-
-
-return ReferralManagementDashboard.createUserIcon(item, defaultIcon);
-
