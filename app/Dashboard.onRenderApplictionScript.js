ae979b54ffac78b506858eb7c57202cf10ddb075
nickolanack <nickblackwell82@gmail.com>
2018-12-31 13:20:41 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/Dashboard.onRenderApplictionScript.js
+++ app/Dashboard.onRenderApplictionScript.js
@@ -26,7 +26,7 @@ $firelight->includeScripts();
 GetWidget('proposalForm')->display($targetInstance); //proposal wizard from map
 
 
-if(GetClient()->isGuest()){
+//if(GetClient()->isGuest()){
    GetWidget('guestDashboard')->display($targetInstance); 
    GetWidget('loginForm')->display($targetInstance); //login wizard from map
    GetWidget('forgotPasswordForm')->display($targetInstance); //login wizard from map
@@ -39,8 +39,8 @@ if(GetClient()->isGuest()){
    
    
    
-   return;
-}
+   //return;
+//}
 
 IncludeJSBlock('
     
@@ -63,20 +63,20 @@ IncludeJSBlock('
 ');
 
 
-if((!GetClient()->isAdmin())&&count(array_intersect($firelight->teamMemberRoles(), ($roles=$firelight->getUserRoles())))==0){
+//if((!GetClient()->isAdmin())&&count(array_intersect($firelight->teamMemberRoles(), ($roles=$firelight->getUserRoles())))==0){
     
     
     GetWidget('defaultPostDetail')->display($targetInstance);
     
-    if(count(array_intersect($firelight->communityMemberRoles(),$roles))>0){
+    //if(count(array_intersect($firelight->communityMemberRoles(),$roles))>0){
         GetWidget('communityMemberDashboard')->display($targetInstance); 
-        return;
-    }
+        //return;
+    //}
     
     
     GetWidget('nonMemberDashboard')->display($targetInstance); 
-    return;
-}
+    //return;
+//}
 
 
 GetWidget('mainActivityDetail')->display($targetInstance);
