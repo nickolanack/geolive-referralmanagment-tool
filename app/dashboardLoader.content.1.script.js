ad8e46d5d83690bb8f1b9a5373be380ab99ab98c
nickolanack <nickblackwell82@gmail.com>
2019-01-03 16:29:23 -0800

    nickola -- Auto Commit - Changes made through Administrator Interface

--- app/dashboardLoader.content.1.script.js
+++ app/dashboardLoader.content.1.script.js
@@ -1,3 +1,3 @@
 return function(){
-    //application.getChildView('content',0).redraw({"namedView":"dashboard"})
+    application.getChildView('content',0).redraw({"namedView":"dashboardContent"})
 }
