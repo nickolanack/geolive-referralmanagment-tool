9fbd76a36a0ddc61f2a860181a7ceb8710de7e8b
nickolanack <nickblackwell82@gmail.com>
2019-01-29 18:28:12 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/communityUsersDetail.content.1.textValueScript.js
+++ app/communityUsersDetail.content.1.textValueScript.js
@@ -1,2 +1,5 @@
 callback(
-    "<p>These are other lands department members etc.</p>");
+    "<p>When users log in for the first time they must be approved by a lands department member or manager. "+
+    "This page shows all the community members as well as users who have not yet been assigned to any roll. "+
+    "Lands department members can approve users as community members, while Lands department managers can also "+
+    "add users to the lands department team"+"</p>");
