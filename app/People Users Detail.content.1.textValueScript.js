52cb16bbe10439c0f4414cd0a423407437f61201
nickolanack <nickblackwell82@gmail.com>
2019-01-29 18:53:26 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/People Users Detail.content.1.textValueScript.js
+++ app/People Users Detail.content.1.textValueScript.js
@@ -1,4 +1,5 @@
 callback(
-    "<p>Teams are created around a communities Lands Department."+
+    "<p>Teams are created around a communities Lands Department. "+
     "Lands Department managers can approve new Lands Department members, "+
-    "(as well approving regular community users and mobile app users). `Wabun` Lands department members are visible to all communities"+"</p>"+ReferralManagementDashboard.getUsersTeamMembersDescription());
+    "(as well approving regular community users and mobile app users). "+
+    "`Wabun` Lands department members are visible to all communities"+"</p>"+ReferralManagementDashboard.getUsersTeamMembersDescription());
