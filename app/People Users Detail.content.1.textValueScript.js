24cac7ac9223d9efa06de029e60942f49f7c4b93
nickolanack <nickblackwell82@gmail.com>
2019-01-29 18:51:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/People Users Detail.content.1.textValueScript.js
+++ app/People Users Detail.content.1.textValueScript.js
@@ -1,5 +1,4 @@
 callback(
-    "<p>When users log in for the first time they must be approved by a lands department member or manager. "+
-    "This page shows all the community members as well as users who have not yet been assigned to any roll. "+
-    "Lands department members can approve users as community members, while Lands department managers can also "+
-    "add users to the lands department team"+"</p>");
+    "<p>Teams are created around a communities Lands Department."+
+    "Lands Department managers can approve new Lands Department members, "+
+    "(as well approving regular community users and mobile app users). `Wabun` Lands department members are visible to all communities"+"</p>"+ReferralManagementDashboard.getUsersTeamMembersDescription());
