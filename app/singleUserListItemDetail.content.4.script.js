514de3518a35787beefc616fda391c706b48b4e3
nickolanack <nickblackwell82@gmail.com>
2019-01-29 00:55:37 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleUserListItemDetail.content.4.script.js
+++ app/singleUserListItemDetail.content.4.script.js
@@ -1,1 +1,20 @@
-return ReferralManagementDashboard.createRoleEditModules(item);
+if(!(item.isProjectMember&&item.isProjectMember())){
+    return null;
+}
+
+return new OptionListModule({
+    functions:[
+        {
+            "label":"Project Settings",
+            "fn":function(e){
+                e.stop();
+                application.getDisplayController().displayPopoverForm(
+    				"UsersProjectSettings", 
+    				item, 
+    				application,
+    				{template:"form"}
+			    );
+            }
+        }
+    ]
+})
