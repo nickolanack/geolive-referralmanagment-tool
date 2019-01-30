238729ebfa0bd88557668feb9ac1fbf340d70486
nickolanack <nickblackwell82@gmail.com>
2019-01-30 05:17:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Needs User Role.customScript.js
+++
@@ -1,8 +0,0 @@
-var wizardTemplate = (map.getDisplayController().getWizardTemplate('UserTemplate'));
-                            if ((typeof wizardTemplate) != 'function') {
-                                throw 'Expecting Proposal Wizard';
-                            }
-
-                            var wizard = wizardTemplate(map, {});
-
-                            wizard.buildDefaultAndShow();
