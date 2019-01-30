57beec1b2d82ca1b7bcf4023222432538985fa16
Nicholas  Blackwell <nblackwe@Nicholass-Mac-mini.local>
2019-01-29 22:36:45 -0800

    replace id with userId in ReferralManagement/lib/User.php

--- Plugins/ReferralManagement/lib/User.php
+++ Plugins/ReferralManagement/lib/User.php
@@ -6,23 +6,23 @@ class User {
 
 	protected $currentUserAttributes = null;
 
-	public function getMetadata($id = -1) {
+	public function getMetadata($userId = -1) {
 
 		$metadata = null;
 
-		if (is_array($id)) {
-			$metadata = $id;
+		if (is_array($userId)) {
+			$metadata = $userId;
 			if (!key_exists('id', $metadata)) {
 				throw new \Exception('Expected user metadata with id: ' . json_encode($metadata));
 			}
-			$id = $metadata['id'];
+			$userId = $metadata['id'];
 		}
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 		if (!$metadata) {
-			$metadata = GetClient()->userMetadataFor($id);
+			$metadata = GetClient()->userMetadataFor($userId);
 		}
 
 		$metadata['device'] = false;
@@ -32,8 +32,8 @@ class User {
 
 		GetPlugin('Attributes');
 		$this->_withUserAttributes(
-			(new \attributes\Record('userAttributes'))->getValues($id, 'user'),
-			function ($attributes) use (&$metadata, $id) {
+			(new \attributes\Record('userAttributes'))->getValues($userId, 'user'),
+			function ($attributes) use (&$metadata, $userId) {
 
 				// $ref=GetPlugin('ReferralManagement');
 				//
@@ -48,21 +48,21 @@ class User {
 
 				$metadata['communityId'] = array_search($metadata['community'], $this->listCommunities());
 
-				$metadata['role-icon'] = $this->getUserRoleIcon($id);
-				$metadata['user-icon'] = $this->getUserRoleLabel($id);
-				$metadata['can-create'] = $this->canCreateCommunityContent($id);
-				$metadata['communities'] = $this->getCommunities($id);
+				$metadata['role-icon'] = $this->getUserRoleIcon($userId);
+				$metadata['user-icon'] = $this->getUserRoleLabel($userId);
+				$metadata['can-create'] = $this->canCreateCommunityContent($userId);
+				$metadata['communities'] = $this->getCommunities($userId);
 				// $metadata['community'] = $metadata['communities'][0];
 				// $metadata['communityId'] = 0;
-				$metadata['teams'] = $this->getTeams($id);
-				$metadata['avatar'] = $this->getUsersAvatar($id);
-				$metadata['name'] = $this->getUsersName($id, $metadata['name']);
-				$metadata['lastName'] = $this->getUsersLastName($id, '');
-				$metadata['number'] = $this->getUsersNumber($id);
-				$metadata['email'] = $this->getEmail($id, $metadata['email']);
-				$metadata['can-assignroles'] = $this->getRolesUserCanEdit($id);
+				$metadata['teams'] = $this->getTeams($userId);
+				$metadata['avatar'] = $this->getUsersAvatar($userId);
+				$metadata['name'] = $this->getUsersName($userId, $metadata['name']);
+				$metadata['lastName'] = $this->getUsersLastName($userId, '');
+				$metadata['number'] = $this->getUsersNumber($userId);
+				$metadata['email'] = $this->getEmail($userId, $metadata['email']);
+				$metadata['can-assignroles'] = $this->getRolesUserCanEdit($userId);
 
-				$metadata['roles'] = $this->getUserRoles($id);
+				$metadata['roles'] = $this->getUserRoles($userId);
 
 			});
 
@@ -70,21 +70,21 @@ class User {
 
 	}
 
-	protected function getRolesUserCanEdit($id = -1) {
-		return (new \ReferralManagement\UserRoles())->getRolesUserCanEdit($id);
+	protected function getRolesUserCanEdit($userId = -1) {
+		return (new \ReferralManagement\UserRoles())->getRolesUserCanEdit($userId);
 	}
 
-	protected function getUserRoles($id = -1) {
-		return (new \ReferralManagement\UserRoles())->getUsersRoles($id);
+	protected function getUserRoles($userId = -1) {
+		return (new \ReferralManagement\UserRoles())->getUsersRoles($userId);
 	}
 
-	public function getUsersAvatar($id = -1, $default = null) {
+	public function getUsersAvatar($userId = -1, $default = null) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 		if ($attribs["profileIcon"]) {
 			return HtmlDocument()->parseImageUrls($attribs["profileIcon"])[0];
 		}
@@ -96,16 +96,16 @@ class User {
 
 	}
 
-	public function getTeams($id = -1) {
-		return $this->getCommunities($id);
+	public function getTeams($userId = -1) {
+		return $this->getCommunities($userId);
 	}
 
-	public function getCommunities($id = -1) {
+	public function getCommunities($userId = -1) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 		$communities = array();
 
 		if (in_array($attribs['community'], $this->listCommunities())) {
@@ -115,12 +115,12 @@ class User {
 		return $communities;
 	}
 
-	protected function canCreateCommunityContent($id = -1) {
-		return $this->getUserRoleLabel($id) !== 'none';
+	protected function canCreateCommunityContent($userId = -1) {
+		return $this->getUserRoleLabel($userId) !== 'none';
 	}
 
-	protected function getUserRoleLabel($id = -1) {
-		return (new \ReferralManagement\UserRoles())->getUsersRoleLabel($id);
+	protected function getUserRoleLabel($userId = -1) {
+		return (new \ReferralManagement\UserRoles())->getUsersRoleLabel($userId);
 	}
 
 	public function listTeams() {
@@ -139,17 +139,17 @@ class User {
 		$this->currentUserAttributes = null;
 	}
 
-	protected function getUserRoleIcon($id = -1) {
-		return (new \ReferralManagement\UserRoles())->getUserRoleIcon($id);
+	protected function getUserRoleIcon($userId = -1) {
+		return (new \ReferralManagement\UserRoles())->getUserRoleIcon($userId);
 	}
 
-	protected function getUsersName($id = -1, $default = null) {
+	protected function getUsersName($userId = -1, $default = null) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 
 		if ($attribs["firstName"]) {
 			return $attribs["firstName"];
@@ -163,13 +163,13 @@ class User {
 
 	}
 
-	protected function getUsersLastName($id = -1, $default = null) {
+	protected function getUsersLastName($userId = -1, $default = null) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 
 		if ($attribs["lastName"]) {
 			return $attribs["lastName"];
@@ -183,13 +183,13 @@ class User {
 
 	}
 
-	protected function getUsersNumber($id = -1, $default = null) {
+	protected function getUsersNumber($userId = -1, $default = null) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 		if ($attribs["phone"]) {
 			return $attribs["phone"];
 		}
@@ -202,13 +202,13 @@ class User {
 
 	}
 
-	public function getEmail($id = -1, $default = null) {
+	public function getEmail($userId = -1, $default = null) {
 
-		if ($id < 1) {
-			$id = GetClient()->getUserId();
+		if ($userId < 1) {
+			$userId = GetClient()->getUserId();
 		}
 
-		$attribs = $this->_getUserAttributes($id);
+		$attribs = $this->_getUserAttributes($userId);
 		if ($attribs["email"]) {
 			return $attribs["email"];
 		}
@@ -217,19 +217,19 @@ class User {
 			return $default;
 		}
 
-		return GetClient()->userMetadataFor($id)['email'];
+		return GetClient()->userMetadataFor($userId)['email'];
 	}
 
 	public function getAttributes($userId) {
 		return $this->_getUserAttributes($userId);
 	}
 
-	protected function _getUserAttributes($id) {
+	protected function _getUserAttributes($userId) {
 
 		if (is_null($this->currentUserAttributes)) {
 
 			GetPlugin('Attributes');
-			return (new \attributes\Record('userAttributes'))->getValues($id, 'user');
+			return (new \attributes\Record('userAttributes'))->getValues($userId, 'user');
 
 		}
 
