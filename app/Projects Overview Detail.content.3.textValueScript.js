30cacab8dd7b3b6618869867488f0b0d90e355ad
nickolanack <nickblackwell82@gmail.com>
2019-01-31 17:09:08 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects Overview Detail.content.3.textValueScript.js
+++ app/Projects Overview Detail.content.3.textValueScript.js
@@ -2,6 +2,6 @@ ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
     var proposals=team.getProposals();
 
     callback(proposals.length==0?
-        "You currently have no projects, to get started, create a project.":
+        "There are no active projects. To get started, create a new project":
         "");
  })
