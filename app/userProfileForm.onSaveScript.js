7135a4395771887473bc90ce007b178b1dd1a115
nickolanack <nickblackwell82@gmail.com>
2019-02-08 19:12:39 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userProfileForm.onSaveScript.js
+++ app/userProfileForm.onSaveScript.js
@@ -1,4 +1,4 @@
-(new UIModalDialog(application, "Your profile has been saved. A lands department member must approve your account.", {
+(new UIModalDialog(application, "Your profile has been saved. An administrator must approve your account.", {
 				"formName": "dialogForm",
 				"formOptions": {
 					"template": "form",
