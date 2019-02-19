4420d7c3e9db8aa126b05a44eac467c12195cc32
nickolanack <nickblackwell82@gmail.com>
2019-02-19 17:32:25 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/User Detail View.content.4.fieldValueScript.js
+++ app/User Detail View.content.4.fieldValueScript.js
@@ -1,5 +1,8 @@
 ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
    var value=team.getUser(AppClient.getId()).getCommunity();
+   if(value=="none"){
+       value="no community selected";
+   }
    callback(value);
 });
 
