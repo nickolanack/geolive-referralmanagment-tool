941fe141df8daf13e9b2b7eeccc05830e7b56f0e
nickolanack <nickblackwell82@gmail.com>
2019-02-20 05:02:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Left Panel.content.3.script.js
+++ app/Left Panel.content.3.script.js
@@ -1,1 +1,1 @@
-return new ElementModule('div',{"class":"application-logo",html:"Gather Referrals Management Tool "+(new Date()).getFullYear()+""})
+return new ElementModule('div',{"class":"application-logo gather-logo",html:"Gather Referrals Management Tool "+(new Date()).getFullYear()+""})
