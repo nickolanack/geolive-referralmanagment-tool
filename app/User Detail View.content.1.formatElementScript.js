2fed9a0b2903ee4f265d91f0e4afab6d2d2098ac
nickolanack <nickblackwell82@gmail.com>
2019-02-20 04:44:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/User Detail View.content.1.formatElementScript.js
+++ app/User Detail View.content.1.formatElementScript.js
@@ -1,3 +1,5 @@
 /* turn the entire element (el) into a form button*/
 new UIModalFormButton(el.parentNode, application, item, {"formName":"userProfileForm", "formOptions":{template:"form"}});
 el.addClass('editable');
+
+el.appendChild(new Element('button', {"html":"logout", "events":{"click":function(){AppClient.logout();}}}));
