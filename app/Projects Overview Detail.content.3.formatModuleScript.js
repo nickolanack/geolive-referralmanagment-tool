cd2152b307b2c27c0b74e9742199b534b05c6018
nickolanack <nickblackwell82@gmail.com>
2019-02-26 19:17:20 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Projects Overview Detail.content.3.formatModuleScript.js
@@ -0,0 +1,9 @@
+
+ProjectTeam.CurrentTeam.runOnceOnLoad(function(team){
+    
+    module.addWeakEvent(team, 'addProject', function(){
+        var el=module.getElement();
+        el.parentNode.removeChild(el);
+    });
+    
+});
