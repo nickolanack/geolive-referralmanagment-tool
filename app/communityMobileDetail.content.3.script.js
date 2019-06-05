f5afa1ebc4e810d909cc82030a4768fb6c271d2a
nickolanack <nickblackwell82@gmail.com>
2019-06-05 14:06:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/communityMobileDetail.content.3.script.js
@@ -0,0 +1,9 @@
+return (new ModalFormButtonModule(application, AppClient,{
+        label:"Invite App User",
+        formName:"userInviteForm",
+        formOptions:{
+            template:"form"
+        },
+        //hideText:true,
+        "class":"primary-btn"
+    }));
