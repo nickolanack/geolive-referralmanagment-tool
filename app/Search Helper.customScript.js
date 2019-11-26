7956bfb010606d9efc7bda03e7323af547bc5321
nickolanack <nickblackwell82@gmail.com>
2019-11-26 20:37:28 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Search Helper.customScript.js
@@ -0,0 +1,37 @@
+application.getNamedValue("Search",function(search){
+    
+    console.log("got search");
+    search.search.addEvent('resultSelect',function(result){
+        
+       console.log(result); 
+       
+       try{
+           
+           
+           var layer=application.getLayerManager().getLayer(result.lid);
+           if(layer&&layer.isTiled()){
+               var coordinates=result.coordinates;
+               if(typeof coordinates=='string'){
+                   coordinates=JSON.parse(coordinates);
+               }
+                
+                if(coordinates.coordinates){
+                    coordinates=coordinates.coordinates;
+                }
+                if(coordinates.length&&typeof coordinates[0]!="number"){
+                    coordinates=coordinates[0];
+                }
+               
+               application.setCenter(coordinates);
+           }
+           
+           
+           
+       }catch(e){
+           console.error(e);
+       }
+        
+    });
+    
+    
+});
