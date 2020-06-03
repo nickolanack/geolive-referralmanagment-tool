85608a2feac0630fd0cad4adb3029ad6757f9fa0
nickolanack <nickblackwell82@gmail.com>
2020-06-03 19:11:19 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Main Dashboard Detail.content.5.resolveItemScript.js
@@ -0,0 +1,1 @@
+return new MockDataTypeItem({"label":"Test", "filter":function(){}});
---
+++ app/placeholderDetail.content.1.getItemListScript.js
@@ -0,0 +1,10 @@
+ ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+     var proposals=team.getProposals();
+     if(!application.getNamedValue("currentProject")){
+        application.setNamedValue("currentProject", proposals[0]);
+    }
+    callback(proposals)
+ })
+
+       return null;
+       
