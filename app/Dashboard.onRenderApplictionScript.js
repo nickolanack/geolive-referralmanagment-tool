db7f88eae83cb616ac209109c1c43912d9b7bbff
nickolanack <nickblackwell82@gmail.com>
2020-06-04 22:46:40 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Dashboard.onRenderApplictionScript.js
+++ app/Dashboard.onRenderApplictionScript.js
@@ -3,7 +3,7 @@ HtmlDocument()->META(HtmlDocument()->website(), 'base');
 IncludeJSBlock('
 
 
-TemplateModule.SetTemplate(\'form\',\'<div><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
+TemplateModule.SetTemplate(\'form\',\'<div class="dark"><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
 TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
 
 
