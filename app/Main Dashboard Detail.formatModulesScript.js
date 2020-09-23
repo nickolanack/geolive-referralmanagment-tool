2d5fe2d5f827235de25e79b9db6db41b6c192f9c
nickolanack <nickblackwell82@gmail.com>
2020-09-23 15:57:22 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Main Dashboard Detail.formatModulesScript.js
+++ app/Main Dashboard Detail.formatModulesScript.js
@@ -1,12 +1,15 @@
 
 //return function(callback){
 
-    if(DashboardConfig.getValue('showRecentProjectsDetail')){
-        list.content=list.content.slice(5);
+
+    list.content=DashboardPageLayout.layoutPage('mainDashboardDetail', list.content);
+
+    // if(DashboardConfig.getValue('showRecentProjectsDetail')){
+    //     list.content=list.content.slice(5);
         
-    }else{
-        list.content=list.content.slice(0,5);
-    }
+    // }else{
+    //     list.content=list.content.slice(0,5);
+    // }
     
     //callback(list);
     
