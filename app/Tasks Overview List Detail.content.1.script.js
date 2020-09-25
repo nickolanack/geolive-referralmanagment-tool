06416e7b8fbcc45b30d9fdf5ae53a2808b4eb83d
nickolanack <nickblackwell82@gmail.com>
2020-09-25 18:57:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Tasks Overview List Detail.content.1.script.js
@@ -0,0 +1,47 @@
+return function(viewer, element, parentModule){
+
+
+     
+
+   var filter=(new ListSortModule(function(){
+        return viewer.getChildView('content', 3);
+    }, {
+        sorters:ReferralManagementDashboard.taskSorters().filter(function(f){
+            return f.label!=='complete';
+        }),
+        currentSort:"priority",
+        currentSortInvert:true
+    }));
+    
+    
+
+    var sort=(new ListFilterModule(function(){
+        return viewer.getChildView('content', 3);
+    }, {
+        filters:ReferralManagementDashboard.taskFilters().filter(function(f){
+            return f.label!=='complete';
+        }),
+        //currentFilter:"complete",
+        //currentFilterInvert:true,
+        //showClear:false,
+        //showReset:true
+    }));
+    
+
+    // var reset=new ElementModule('button',{
+    //      "class":"btn-reset-list",
+    //      events:{click:function(){
+             
+    //          filter.reset();
+    //          sort.reset();
+             
+    //      }}
+    //  })
+    
+    
+
+    application.setNamedValue('taskListFilter', sort);
+    
+    return new ModuleArray([filter, sort],{"class":"filter-btns left-justified"});
+    
+}
