075f3aa1c84aed11fb21d98237c30d4a37327d1d
nickolanack <nickblackwell82@gmail.com>
2020-10-27 18:04:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects List Detail.content.2.script.js
+++ app/Projects List Detail.content.2.script.js
@@ -1,38 +1,3 @@
 /*Project Filters*/
 
-return function(viewer, element, parentModule){
-    
-    var div=element.appendChild(new Element('div',{
-        
-        "class":"project-list-filters section-indent",
-        
-        styles:{
-            "display": "inline-table",
-            "width": "100%"
-        }
-    }));
-    
-
-    (new ListSortModule(function(){
-        return viewer.getChildView('content', 2);
-    }, {
-        sorters:ReferralManagementDashboard.projectSorters(),
-        currentSort:"priority",
-        currentSortInvert:true
-    })).load(null, div, null);
-    
-
-    
-  
-    (new ListFilterModule(function(){
-        return viewer.getChildView('content', 2)
-    }, {
-        filters:ReferralManagementDashboard.projectFilters(),
-        currentFilter:"complete",
-        currentFilterInvert:true
-    })).load(null, div, null);
-    
-
-  
-
-}
+return Proposal.MenuButtons();
