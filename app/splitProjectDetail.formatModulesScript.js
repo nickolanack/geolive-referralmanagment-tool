43f9e1112d13c258a37f1705b16738b999454917
nickolanack <nickblackwell82@gmail.com>
2020-10-28 01:17:06 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/splitProjectDetail.formatModulesScript.js
+++ app/splitProjectDetail.formatModulesScript.js
@@ -1,5 +1,2 @@
-if(!DashboardConfig.getValue('showSplitProjectDetail')){
-        list.content=list.content.slice(0,1);
-        list.content[0].options.className=list.content[0].options.className.split(' ').slice(0,-1).join(' ');
-        
-    }
+
+return DashboardPageLayout.layoutPage('splitProjectDetail', list);
