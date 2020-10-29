2bdce5d66120805179927edab131b373b38b4190
nickolanack <nickblackwell82@gmail.com>
2020-10-29 22:05:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects Overview Detail.content.1.script.js
+++ app/Projects Overview Detail.content.1.script.js
@@ -1,37 +1,4 @@
 /*Projects Header*/
 
 
-var div = new Element('div',{"class":""});
-div.appendChild(new Element("button",{"data-lbl":"New project", "class":"inline-btn add primary-btn", "events":{"click":function(){
-    
-   
-            var formName="ProposalTemplate";
-
-			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
-			if ((typeof wizardTemplate) != 'function') {
-
-				if(window.console&&console.warn){
-					console.warn('Expected named wizardTemplate: '+formName+', to exist');
-				}
-
-			}
-			var modalFormViewController =  new PushBoxModuleViewer(application, {});
-			var newItem=new Proposal();
-			var wizard = wizardTemplate(newItem, {});
-            wizard.buildAndShow(modalFormViewController, {template:"form"}); 
-			
-			
-			newItem.addEvent("save:once",function(){
-			    ProjectTeam.CurrentTeam().addProject(newItem);
-			})
-    
-    
-
-    
-    
-    
-    
-    
-    
-}}}));
-return div;
+return ProjectList.HeaderMenuContent(item);
