46fa0d2e34f7b555faab0fa7099ad230b2600bcd
nickolanack <nickblackwell82@gmail.com>
2020-10-29 21:17:08 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects List Detail.content.2.script.js
+++ app/Projects List Detail.content.2.script.js
@@ -1,3 +1,38 @@
-/*Project Filters*/
+/*Projects Header*/
 
-return Proposal.ProjectSortMenuButtons();
+
+var div = new Element('div',{"class":"project-list-btns"});
+
+div.appendChild(new Element("button",{"data-lbl":"New project", "class":"inline-btn add primary-btn", "events":{"click":function(){
+    
+   
+            var formName="ProposalTemplate";
+
+			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
+			if ((typeof wizardTemplate) != 'function') {
+
+				if(window.console&&console.warn){
+					console.warn('Expected named wizardTemplate: '+formName+', to exist');
+				}
+
+			}
+			var modalFormViewController =  new PushBoxModuleViewer(application, {});
+			var newItem=new Proposal();
+			var wizard = wizardTemplate(newItem, {});
+            wizard.buildAndShow(modalFormViewController, {template:"form"}); 
+			
+			
+			newItem.addEvent("save:once",function(){
+			    ProjectTeam.CurrentTeam().addProject(newItem);
+			})
+    
+    
+
+    
+    
+    
+    
+    
+    
+}}}));
+return div;
---
+++ app/Projects List Detail.content.3.script.js
@@ -0,0 +1,3 @@
+/*Project Filters*/
+
+return Proposal.ProjectSortMenuButtons();
