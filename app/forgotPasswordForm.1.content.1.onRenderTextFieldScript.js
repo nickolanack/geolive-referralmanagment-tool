9e08801b4b2342ac993fa10ed32f3c227f4f5a27
nickolanack <nickblackwell82@gmail.com>
2020-11-02 20:33:22 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/forgotPasswordForm.1.content.1.onRenderTextFieldScript.js
+++ app/forgotPasswordForm.1.content.1.onRenderTextFieldScript.js
@@ -1,7 +1,7 @@
 inputElement.addEvent('keydown', function(k) {
 			var key = k.key;
 			if (key == "enter") {
-				item.setSendReset();
+				item.setSendMagicLink();
                 wizard.complete();
 
 				return false; //returning false stops the character from printing.
