7521b11f40808c83ed16db66041dd1f3217146a8
nickolanack <nickblackwell82@gmail.com>
2020-11-03 04:43:45 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectEditButtonsDetail.content.5.script.js
+++ app/singleProjectEditButtonsDetail.content.5.script.js
@@ -1,20 +1,3 @@
 
 
-return [
-    new Element('button',{
-        "html":"Pending", 
-        "style":"", 
-        "class":"primary-btn selectable "+(item.isPending()?"selected":""), 
-        "events":{"click":function(){
-             item.setPending();
-        }}
-    }),
-    new Element('button',{
-        "html":"Implemented", 
-        "style":"", 
-        "class":"primary-btn selectable "+(item.isImplemented()?"selected":""), 
-        "events":{"click":function(){
-            item.setImplemented();  
-        }}
-    })
-    ];
+return Proposal.PendingButtons(item);
