e1cdd6b23143cae307373c16a74fc0e09ccb8e17
nickolanack <nickblackwell82@gmail.com>
2020-11-11 19:47:21 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleFileListItemDetail.content.1.fieldValueScript.js
+++ app/singleFileListItemDetail.content.1.fieldValueScript.js
@@ -1,15 +1,1 @@
-var file=item.file||item.id||item;
-
-if(file.html){
-    file=Proposal.ParseHtmlUrls(file.html)[0];
-}
-
-(new AjaxControlQuery(CoreAjaxUrlRoot, 'file_metadata', {
-				'file': file,
-				'show': ['iconsetDetails']
-			})).addEvent('onSuccess', function(response) {
-			    callback(response.metadata.name);
-			}).execute();
-
-
-return file.split("/").pop();
+return ProjectFiles.getFileName(item);
