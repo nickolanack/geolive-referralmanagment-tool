d019caf2fb6603cb1e922a555ff5be9ab4c561e8
nickolanack <nickblackwell82@gmail.com>
2020-11-17 05:20:25 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Tasks Overview List Detail.content.3.formatListItemModuleScript.js
+++ app/Tasks Overview List Detail.content.3.formatListItemModuleScript.js
@@ -9,24 +9,4 @@ if(child.getItem().getType()==="user"){
     
 }
 
-childView.addEvent('load:once',function(){
-    
-    childView.addWeakEvent(child, "saving", function(){
-        childView.startSpinner();
-    })
-    childView.addWeakEvent(child, "change", function(){
-        if(listModule.applyFilterToItem(child)&&(!child.isComplete())){
-             childView.redraw();
-             return;
-        }
-        
-        //no longer passes filter
-        childView.remove();
-    })
-    
-})
-
-
-childView.addWeakEvent(child, 'remove', function(){
-    childView.remove();
-});
+TaskItem.AddListItemEvents(listModule, childView, child);
