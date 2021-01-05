9d8ae730c9e5b078aebda724db2aeed4f8ba5d0f
nickolanack <nickblackwell82@gmail.com>
2021-01-05 17:07:23 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetItemView.content.3.formatElementScript.js
+++ app/datasetItemView.content.3.formatElementScript.js
@@ -8,9 +8,9 @@ if(projects.length>0){
 el.setAttribute('data-item-list',projects.map(function(p){
     
     
-    if (ProjectTeam.CurrentTeam().hasUser(p.getUserId())){
-        UserIcon.createUserAvatarModule(ProjectTeam.CurrentTeam().getUser(p.getUserId()));
-    }
+   // if (ProjectTeam.CurrentTeam().hasUser(p.getUserId())){
+       // UserIcon.createUserAvatarModule(ProjectTeam.CurrentTeam().getUser(p.getUserId()));
+   // }
     return p.getId();
     
     
