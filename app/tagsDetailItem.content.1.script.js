42257bb390b7606d670d638ec4a9e562e1635269
nickolanack <nickblackwell82@gmail.com>
2021-02-04 23:03:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/tagsDetailItem.content.1.script.js
+++ app/tagsDetailItem.content.1.script.js
@@ -1,1 +1,8 @@
-return '<div class="section-title"><span>'+item.getCategory().capitalize()+' Tag:</span></div>';
+
+
+var type='Tag';
+if(item.isRootTage()){
+    type="Category";
+}
+
+return '<div class="section-title"><span>'+item.getCategory().capitalize()+' '+type+':</span></div>';
