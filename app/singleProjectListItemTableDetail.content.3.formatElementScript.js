ce269c4f73653553a11210f74a12bd4bd64fcf4a
nickolanack <nickblackwell82@gmail.com>
2021-02-05 22:03:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.3.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.3.formatElementScript.js
@@ -1,2 +1,3 @@
 el.addClass("inline tag-width");
 RecentItems.colorizeEl(valueEl, item.getProjectType());
+el.setAttribute("data-col","type");
--- app/singleProjectListItemTableDetail.content.4.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.4.formatElementScript.js
@@ -4,3 +4,5 @@ el.addClass("inline");
 if(item.getDocuments().concat(item.getAttachments()).concat(item.getSpatialDocuments()).length>0){
     el.addClass('withItems');
 }
+
+el.setAttribute("data-col","attachments");
