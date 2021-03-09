a0149c26aba04239de43b23a65a626e489198d1a
nickolanack <nickblackwell82@gmail.com>
2021-03-09 23:31:27 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/documentProjectForm.1.children.2.content.1.getItemListScript.js
+++ app/documentProjectForm.1.children.2.content.1.getItemListScript.js
@@ -1,6 +1,7 @@
 
 ProjectTeam.CurrentTeam().runOnceOnLoad(function(team) {
-    callback(item).getProjects().map(function(p){
-        return team.getProject(p);
-    })
+    callback(item.getProjects().map(function(p){
+            return team.getProject(p);
+        })
+    );
 });    
