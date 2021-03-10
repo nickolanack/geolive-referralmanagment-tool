723bb12899b408724415faa7dd896f740c1a2f4c
nickolanack <nickblackwell82@gmail.com>
2021-03-10 09:46:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.2.script.js
+++
@@ -1,6 +0,0 @@
-return null;
-return new ModuleArray([
-    new ElementModule("label",{html:"Budget"}),
-    new ElementModule("div",{html:item.getPercentBudgetComplete()+"%", "class":"percent-complete-value"}),
-    new ProgressBarModule({value:function(){ return item.getPercentBudgetComplete(); },"class":"percent-complete"})
-],{"class":"progress"});
