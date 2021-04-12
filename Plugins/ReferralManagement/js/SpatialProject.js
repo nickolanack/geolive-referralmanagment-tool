a6f8f5589b3767d0fb230478d1081821e99a3c7e
Nick Blackwell <nickblackwell82@gmail.com>
2021-04-12 09:32:50 -0700

    fix syntax

--- Plugins/ReferralManagement/js/SpatialProject.js
+++ Plugins/ReferralManagement/js/SpatialProject.js
@@ -24,7 +24,7 @@ var SpatialProject=(function(){
 
 					var layer = ProjectLayer.MakeProjectLayer(map, {
 						url: url,
-						name:project.getName();
+						name:project.getName(),
 						group:project.getBaseMapLayerType()
 					});
 
