8120026289752b60e794119bf96fb4a4839f7875
nickolanack <nickblackwell82@gmail.com>
2021-04-12 14:35:01 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Display Proposal Spatial Files.customScript.js
+++ app/Display Proposal Spatial Files.customScript.js
@@ -1,19 +1,2 @@
-<?php 
- IncludeJS(GetPlugin('ReferralManagement')->getPath().'/js/SpatialDocumentPreview.js');
- IncludeJS(GetPlugin('ReferralManagement')->getPath().'/js/ProposalLayer.js');
-
-?>
-
-SpatialDocumentPreview.setTile(tile, control);
-SpatialDocumentPreview.setMap(map);
-
-
-var getFiles=window.GetSpatialFiles||window.parent.GetSpatialFiles
-if(getFiles){
-    var files = getFiles();
-    if(files.length){
-        SpatialDocumentPreview.show(files);
-    }else{
-        tile.disable();
-    }
-}
+SpatialDocumentPreview.InitMapLayers(map);
+SpatialDocumentPreview.InitMapTile(tile, control, map);
