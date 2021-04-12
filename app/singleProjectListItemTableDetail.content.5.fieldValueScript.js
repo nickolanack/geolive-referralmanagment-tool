8151877e7a8aa03ab8d56412d6ea378a18ba6083
nickolanack <nickblackwell82@gmail.com>
2021-04-12 01:52:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.5.fieldValueScript.js
+++ app/singleProjectListItemTableDetail.content.5.fieldValueScript.js
@@ -1,8 +1,10 @@
 
 
 
-
-return item.getDocuments().concat(item.getAttachments()).length+" docs; "+ item.getSpatialDocuments().length+" spatial"
+if(item.isDataset()){
+    return item.getSpatialDocuments().length+" spatial";
+}
+return item.getDocuments().concat(item.getAttachments()).length+" docs; "+ item.getSpatialDocuments().length+" spatial";
 
 
 
