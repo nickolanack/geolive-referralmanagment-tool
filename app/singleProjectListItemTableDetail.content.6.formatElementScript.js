3afe3c234e4740433263e3777066aac5800d60c1
nickolanack <nickblackwell82@gmail.com>
2021-04-13 04:03:00 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.6.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.6.formatElementScript.js
@@ -1,8 +1,12 @@
 el.addClass("inline");
 
 
-if(item.getDocuments().concat(item.getAttachments()).concat(item.getSpatialDocuments()).length>0){
+if(item.getDocumentsRecursive().concat(item.getAttachmentsRecursive()).concat(item.getSpatialDocumentsRecursive()).length>0){
     el.addClass('withItems');
+}
+
+if(item.getDocumentsChildren().concat(item.getAttachmentsChildren()).concat(item.getSpatialDocumentsChildren()).length>0){
+    el.addClass('withChildItems');
 }
 
 el.setAttribute("data-col","attachments");
