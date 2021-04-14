317d9d0c77febbd273b2cfe9a547a6fe7ec74584
Nick Blackwell <nickblackwell82@gmail.com>
2021-04-14 00:17:00 -0700

    add project members

--- Plugins/ReferralManagement/js/NamedCategory.js
+++ Plugins/ReferralManagement/js/NamedCategory.js
@@ -15,13 +15,13 @@ var NamedCategory = (function() {
 
 	NamedCategory = new Class({
 		Extends: MockDataTypeItem,
-		Implements:[Events],
+		Implements: [Events],
 		initialize: function(options) {
 			this.parent(options);
 
-			options.shortName=options.shortName||options.name;
+			options.shortName = options.shortName || options.name;
 
-			
+
 
 			this.setName = function(n) {
 				options.name = n
@@ -39,9 +39,9 @@ var NamedCategory = (function() {
 
 		},
 
-		
 
-		isRootTag:function(){
+
+		isRootTag: function() {
 			return this.getCategory().toLowerCase() == this.getName().toLowerCase();
 		},
 		getDescriptionPlain: function() {
@@ -71,7 +71,7 @@ var NamedCategory = (function() {
 			var args = {
 
 				name: this.getName(),
-				shortName:this.getShortName(),
+				shortName: this.getShortName(),
 				description: this.getDescription(),
 				category: this.getCategory(),
 				color: this.getColor()
@@ -101,11 +101,11 @@ var NamedCategory = (function() {
 	});
 
 
-	NamedCategory.CreateNewCategory=function(category){
+	NamedCategory.CreateNewCategory = function(category) {
 
 		var newTag = new NamedCategory({
 			name: "",
-			shortName:"",
+			shortName: "",
 			description: "",
 			type: "Project.tag",
 			id: -1,
@@ -118,46 +118,51 @@ var NamedCategory = (function() {
 	};
 
 
-	NamedCategory.CreateCategoryButtons=function(application, item){
+	NamedCategory.CreateCategoryButtons = function(application, item) {
 
 
 		return [new ModalFormButtonModule(application, item, {
-         
-            label: "Edit",
-            formOptions: {template:"form"},
-            formName: "tagForm",
-            "class": "primary-btn"
 
-    
-		}),new ModalFormButtonModule(application, NamedCategory.CreateNewCategory(item.getCategory()), {
-		         
-		            label: "Add "+item.getCategory().capitalize()+" Tag",
-		            formOptions: {template:"form"},
-		            formName: "tagForm",
-		            "class": "primary-btn"
+			label: "Edit",
+			formOptions: {
+				template: "form"
+			},
+			formName: "tagForm",
+			"class": "primary-btn"
 
-		    
-		}),new ModalFormButtonModule(application, new MockDataTypeItem({
-							"name":"Are you sure you want to delete this item"}), {
-		         
-		            label: "Delete",
-							"formName": "dialogForm",
-							"formOptions": {
-								"template": "form",
-								"className": "alert-view"
-							},
-							"class":"primary-btn error"
 
-		    
+		}), new ModalFormButtonModule(application, NamedCategory.CreateNewCategory(item.getCategory()), {
+
+			label: "Add " + item.getCategory().capitalize() + " Tag",
+			formOptions: {
+				template: "form"
+			},
+			formName: "tagForm",
+			"class": "primary-btn"
+
+
+		}), new ModalFormButtonModule(application, new MockDataTypeItem({
+			"name": "Are you sure you want to delete this item"
+		}), {
+
+			label: "Delete",
+			"formName": "dialogForm",
+			"formOptions": {
+				"template": "form",
+				"className": "alert-view"
+			},
+			"class": "primary-btn error"
+
+
 		})];
 
 
 	};
 
 
-	NamedCategory.GetShortName=function(category){
+	NamedCategory.GetShortName = function(category) {
 
-		if((!category)||category==""){
+		if ((!category) || category == "") {
 			return "";
 		}
 
@@ -165,70 +170,80 @@ var NamedCategory = (function() {
 	}
 
 
-	NamedCategory.AddItemIcons=function(item, el){
+	NamedCategory.AddItemIcons = function(item, el) {
 
 		el.addClass('item-icon left-icons');
 
-		var projects=ProjectTagList.getProjectsWithTag(item.getName());
+		var projects = ProjectTagList.getProjectsWithTag(item.getName());
 		el.setAttribute('data-count-projects', projects.length);
-		if(projects.length>0){
-		    el.addClass('hasItems');
+		if (projects.length > 0) {
+			el.addClass('hasItems');
 		}
-		var counter=0;
-		var max=5;
+		var counter = 0;
+		var max = 5;
 
-		var cache=[];
-		
-		el.setAttribute('data-item-list',projects.map(function(p){
-		    
-		    console.error('avatar');
-		    var userid=p.getProjectSubmitterId();
-		    if (ProjectTeam.CurrentTeam().hasUser(userid)&&cache.indexOf(userid)==-1){
-		    	cache.push(userid);
-		        var iconModule=UserIcon.createUserAvatarModule(ProjectTeam.CurrentTeam().getUser(userid));
+		var cache = [];
+
+		el.setAttribute('data-item-list', projects.map(function(p) {
+
+			console.error('avatar');
+			var uid = p.getProjectSubmitterId();
+			var users = p.getUsers()
+
+			([uid]).concat(users).forEach(function(userid) {
 
 
-		        if(iconModule){
-		        	
-		            //el.appendChild(icon);
-		            iconModule.load(null, el, null);
-		            counter++;
-		            iconModule.getElement().addClass('index-'+counter);
-		            if(counter>max){
-						iconModule.getElement().addClass('index-more-than-'+max);
-						iconModule.getElement().addClass('index-more-than-max');
+
+				if (ProjectTeam.CurrentTeam().hasUser(userid) && cache.indexOf(userid) == -1) {
+					cache.push(userid);
+					var iconModule = UserIcon.createUserAvatarModule(ProjectTeam.CurrentTeam().getUser(userid));
+
+
+					if (iconModule) {
+
+						//el.appendChild(icon);
+						iconModule.load(null, el, null);
+						counter++;
+						iconModule.getElement().addClass('index-' + counter);
+						if (counter > max) {
+							iconModule.getElement().addClass('index-more-than-' + max);
+							iconModule.getElement().addClass('index-more-than-max');
+						}
+
 					}
 
-		        }
-		            
-		    }
-		    return p.getId();
-		    
-		    
-		    
+				}
+
+			});
+
+
+			return p.getId();
+
+
+
 		}).join('-'));
 
-		el.setAttribute('data-count-users', counter);           
-		el.addClass('items-'+counter);
-		el.addClass('items-limit-'+Math.min(counter, max));
-		if(counter>max){
-			el.addClass('items-more-than-'+max);
+		el.setAttribute('data-count-users', counter);
+		el.addClass('items-' + counter);
+		el.addClass('items-limit-' + Math.min(counter, max));
+		if (counter > max) {
+			el.addClass('items-more-than-' + max);
 			el.addClass('items-more-than-max');
-			el.setAttribute('data-count-users-overflow', counter-max);
+			el.setAttribute('data-count-users-overflow', counter - max);
 			el.setAttribute('data-count-users', counter);
 		}
 
 
 	};
 
-	NamedCategory.CreateCategoryLabel=function(application, item){
+	NamedCategory.CreateCategoryLabel = function(application, item) {
 
-		var type='Tag';
-		if(item.isRootTag()){
-		    type="Category";
+		var type = 'Tag';
+		if (item.isRootTag()) {
+			type = "Category";
 		}
 
-		return '<div class="section-title"><span>'+item.getCategory().capitalize()+' '+type+':</span></div>';
+		return '<div class="section-title"><span>' + item.getCategory().capitalize() + ' ' + type + ':</span></div>';
 	};
 
 
@@ -239,5 +254,3 @@ var NamedCategory = (function() {
 var ProjectTag = new Class({
 	Extends: NamedCategory
 });
-
-
