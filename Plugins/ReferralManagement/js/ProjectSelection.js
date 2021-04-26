d4f64077482f83078e1b38dabd617ac784bb82a1
Nick Blackwell <nickblackwell82@gmail.com>
2021-04-26 09:36:04 -0700

    add message and update list

--- Plugins/ReferralManagement/js/ProjectSelection.js
+++ Plugins/ReferralManagement/js/ProjectSelection.js
@@ -1,32 +1,40 @@
-var ProjectSelection=(function(){
+var ProjectSelection = (function() {
 
-	var ProjectSelectionClass=new Class({
 
-		handleSelection:function(item, checkbox){
+	var selection = [];
+
+
+	var ProjectSelectionClass = new Class({
+
+		handleSelection: function(item, checkbox) {
 
 			console.log('selection');
 
-			// if(checkbox.checked){
+			if (checkbox.checked) {
 
-			// 	checkbox.checked=false;
-			// 	return;
-			// }
+				NotificationBubble.Make("", "Added `" + project.getName() + "` to selection");
+				if (selection.indexOf(project.getId()) == -1) {
+					selection.push(project.getId());
+				}
 
-			// checkbox.checked=true;
+				return;
+			}
 
+			NotificationBubble.Make("", "Removed `" + project.getName() + "` to selection");
+			var index = selection.indexOf(project.getId());
+			if (index => = 0) {
+				selection.splice(index, 1);
+			}
 
 		},
-		initSelection:function(item, checkbox){
+		initSelection: function(item, checkbox) {
 
 
 
 		}
 
 	});
-	var ProjectSelection=new ProjectSelectionClass();
-
-
-
+	var ProjectSelection = new ProjectSelectionClass();
 
 
 
