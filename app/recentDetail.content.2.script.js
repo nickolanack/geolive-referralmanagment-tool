566bfd48daa8d23f5476613f03b92bd0aee2039c
nickolanack <nickblackwell82@gmail.com>
2021-04-26 16:50:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/recentDetail.content.2.script.js
+++ app/recentDetail.content.2.script.js
@@ -2,6 +2,16 @@ var btn= new Element('button', {html:"View selection", "class":"primary-btn nav-
 
 //add weak event
 
+new WeakEvent(btn, ProjectSelection, 'change', function(selection){
+    
+    if(selection.length>0){
+        btn.addClass('with-selection');
+        return;
+    }
+    btn.removeClass('with-selection');
+
+    
+})
 
 
 return btn;
