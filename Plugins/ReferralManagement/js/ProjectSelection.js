4ee6365f505044602901bf609885636ec454cc06
Nick Blackwell <nickblackwell82@gmail.com>
2021-04-26 16:23:19 -0700

    add make preview button to class

--- Plugins/ReferralManagement/js/ProjectSelection.js
+++ Plugins/ReferralManagement/js/ProjectSelection.js
@@ -58,6 +58,42 @@ var ProjectSelection = (function() {
 	var ProjectSelection = new ProjectSelectionClass();
 
 
+	ProjectSelection.MakePreviewBtn=function(){
+
+		var btn= new Element('button', {html:"View selection", "class":"primary-btn nav-new-btn view-selection inline"});
+
+		//add weak event
+
+		new WeakEvent(btn, ProjectSelection, 'change', function(selection){
+		    
+		    if(selection.length>0){
+		        btn.addClass('with-selection');
+		        return;
+		    }
+		    btn.removeClass('with-selection');
+
+		    
+		})
+
+
+		btn.addEvent('click', function(){
+		    
+		    
+		    var application =ReferralManagementDashboard.getApplication();
+					
+						var controller = application.getNamedValue('navigationController');
+						controller.navigateTo("Map", "Main");
+		    
+		    
+		})
+
+
+		return btn;
+
+
+	};
+
+
 
 	return ProjectSelection;
 
