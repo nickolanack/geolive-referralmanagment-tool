10743b47896814a9893f10e2b97c2f4d03fd8be9
nickolanack <nickblackwell82@gmail.com>
2021-04-27 02:16:10 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/mainSearchDetail.content.4.script.js
@@ -0,0 +1,9 @@
+var application =ReferralManagementDashboard.getApplication();
+
+		return new ElementModule('span',{
+		    "class":"notifications selection",
+		    events:{click:function(){
+		        
+		        
+		    }}
+		})
