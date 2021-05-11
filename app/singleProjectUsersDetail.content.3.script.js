a0b12091fd8bd8591f3ed73ccf66dccc105e0c90
nickolanack <nickblackwell82@gmail.com>
2021-05-11 16:37:35 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/singleProjectUsersDetail.content.3.script.js
@@ -0,0 +1,9 @@
+return (new ModalFormButtonModule(application, item,{
+        label:"Add Community",
+        formName:"--userSelectionForm",
+        formOptions:{
+            template:"form"
+        },
+        //hideText:true,
+        "class":"primary-btn"
+    }))
