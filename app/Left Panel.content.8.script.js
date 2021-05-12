0a69149c9a05a863f1ca933c6a38ef824eff31a1
nickolanack <nickblackwell82@gmail.com>
2021-05-12 17:57:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Left Panel.content.8.script.js
@@ -0,0 +1,9 @@
+return (new ModalFormButtonModule(application, AppClient,{
+        label:"Theme",
+        formName:"themeForm",
+        formOptions:{
+            template:"form"
+        },
+        //hideText:true,
+        "class":"primary-btn"
+    }));
