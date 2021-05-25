88270457429d70846244d99416f3fe76e9babd2b
nickolanack <nickblackwell82@gmail.com>
2021-05-25 16:18:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.5.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.5.formatElementScript.js
@@ -1,3 +1,33 @@
 el.addClass("inline tag-width");
 RecentItems.colorizeEl(valueEl, item.getProjectType());
 el.setAttribute("data-col","type");
+
+
+el.addEvent('click', function(e){
+    e.stop();//Propagation()
+    controller.navigateTo("Datasets", "Main", {
+						
+						filters:ProjectTagList.getProjectTagsData('_root').map(function(item){ return item.getName(); }),
+						//filter:child.getName(),
+
+						item:new ProjectList({
+							"icon":child.getIcon(),
+							"color":child.getColor(),
+			                "label":child.getName()+" Datasets & Collections",
+			                "showCreateBtn":false,
+			                "createBtns":[{
+			                		"label":"Add Dataset",
+                    				"formName":"documentForm"
+                    			},
+                    			{
+			                		"label":"Add Collection",
+                    				"formName":"documentProjectForm",
+                    				"className":"add collection"
+                    			}
+			                ],
+			                "filter":null,
+			                "lockFilter":[/*"!collection", */ item.getProjectType());]
+			            })
+					});
+    
+});
