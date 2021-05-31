9457ece468ef922d1fe19c55a97ec3334a3f9852
nickolanack <nickblackwell82@gmail.com>
2021-05-31 16:10:27 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/userProfileForm.1.content.9.script.js
@@ -0,0 +1,8 @@
+return new ModalFormButtonModule(application, item, {
+         
+            label: "Change Password",
+            formName: "changePassword",
+            "class": "primary-btn update-pwd"
+
+    
+});
