f787464f95d62d8441771dd5c567937b0b55efc1
nickolanack <nickblackwell82@gmail.com>
2021-05-31 03:36:34 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userProfileForm.1.content.1.script.js
+++ app/userProfileForm.1.content.1.script.js
@@ -1,1 +1,10 @@
-return new ElementModule('h2',{html:"Edit your profile"});
+
+
+var userId = parseInt((item.getUserId || item.getId).bind(item)());
+						
+
+var name="your";
+if (AppClient.getId() !== userId) {
+    name="users";
+}
+return new ElementModule('h2',{html:"Edit "+name+" profile"});
