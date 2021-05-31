087fdbd5fe7adfc0c83442c0563b37a9b023a9c7
Nick Blackwell <nickblackwell82@gmail.com>
2021-05-30 20:16:08 -0700

    only show settings if main menu includes settins

--- Plugins/ReferralManagement/js/MainNavigationMenu.js
+++ Plugins/ReferralManagement/js/MainNavigationMenu.js
@@ -607,23 +607,7 @@ var SettingsNavigationMenu=new Class({
 	Extends: MainNavigationMenuBase,
 	initialize: function(application) {
 
-		
-		this.parent({
-			"Configuration": [{
-				html: "Settings",
-				alias: {"section":"Configuration", "button":"Settings", "mirrorActive":true, "menu":function(){
-					return application.getNamedValue('navigationController');
-				}},
-			},{
-				html: "Settings",
-				name:"FixedSettings",
-				alias: {"section":"Configuration", "button":"Settings", "mirrorActive":true, "menu":function(){
-					return application.getNamedValue('navigationController');
-				}},
-			}]
-
-		}, application);
-
+		MainNavigationMenuBase.prototype.initialize.call(this, null, application);
 		this.options.activateFirstMenuItem=false;
 		this.options.manipulateHistory=false;
 	},
@@ -633,40 +617,71 @@ var SettingsNavigationMenu=new Class({
 		var parent=this.parent;
 		var application = this.application;
 		application.getNamedValue('navigationController', function(mainMenu){
+
 			mainMenu.runOnceOnLoad(function(){
-				MainNavigationMenuBase.prototype.process.call(me);
 
-				var updateSticky=function(){
-					var li0=me._buttons['Configuration']['Settings'];
-					var li1=me._buttons['Configuration']['FixedSettings'];
+				me.menu = {}; 
 
-					var p0=li0.getPosition();
-					var p1=li1.getPosition();
-					if(p0.y>p1.y){
-						if(li0.hasClass('bottom')){
+
+				if(mainMenu.hasView({view:"Settings", section:"Configuration"})){
+					me.menu = {
+						"Configuration": [{
+							html: "Settings",
+							alias: {"section":"Configuration", "button":"Settings", "mirrorActive":true, "menu":function(){
+								return application.getNamedValue('navigationController');
+							}},
+						},{
+							html: "Settings",
+							name:"FixedSettings",
+							alias: {"section":"Configuration", "button":"Settings", "mirrorActive":true, "menu":function(){
+								return application.getNamedValue('navigationController');
+							}},
+						}]
+
+					};
+
+
+
+					
+
+					var updateSticky=function(){
+						var li0=me._buttons['Configuration']['Settings'];
+						var li1=me._buttons['Configuration']['FixedSettings'];
+
+						var p0=li0.getPosition();
+						var p1=li1.getPosition();
+						if(p0.y>p1.y){
+							if(li0.hasClass('bottom')){
+								return;
+							}
+							li0.addClass('bottom');
+							li1.removeClass('bottom');
 							return;
 						}
-						li0.addClass('bottom');
-						li1.removeClass('bottom');
-						return;
-					}
 
 
-					if(li1.hasClass('bottom')){
-						return;
-					}
-					li0.removeClass('bottom');
-					li1.addClass('bottom');
+						if(li1.hasClass('bottom')){
+							return;
+						}
+						li0.removeClass('bottom');
+						li1.addClass('bottom');
 
-				};
-				me.runOnceOnLoad(function(){
-					updateSticky();
-					setTimeout(updateSticky, 1000);
-				});
-
-				window.addEvent('resize', updateSticky);
+					};
+					me.runOnceOnLoad(function(){
+						updateSticky();
+						setTimeout(updateSticky, 1000);
+					});
 
 
+
+
+					//me.menu=DashboardPageLayout.layoutMenu('mainMenu', me.menu);
+
+					window.addEvent('resize', updateSticky);
+
+				}
+
+				MainNavigationMenuBase.prototype.process.call(me);
 			});
 		});
 			
