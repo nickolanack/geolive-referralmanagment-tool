6b78072b0ebc05dd3d8e14a4a537c32977498fc3
nickolanack <nickblackwell82@gmail.com>
2021-06-08 18:21:00 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.6.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.6.formatElementScript.js
@@ -16,3 +16,21 @@ el.addEvent('click', function(e){
    UIInteraction.navigateToProjectSection(item ,"Map");
     
 });
+
+
+if(el.hasClass('withItems')){
+    el.appendChild(new Element('button',{
+    "html":"", 
+    "style":"", 
+    "class":"download-link", 
+    "events":{"click":function(){
+    
+        var downloadQuery=new AjaxControlQuery(CoreAjaxUrlRoot, 'download_files', {
+		                "plugin": "ReferralManagement",
+		                "proposal":item.getId()
+		                });
+    				//downloadQuery.execute(); //for testing.
+    				window.open(downloadQuery.getUrl(true),'Download'); 
+
+    }}}))
+}
