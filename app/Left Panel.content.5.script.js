31f5fa3673658c5b33d9484c55841c96c9191e81
nickolanack <nickblackwell82@gmail.com>
2021-06-17 22:47:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Left Panel.content.5.script.js
+++ app/Left Panel.content.5.script.js
@@ -1,3 +1,3 @@
-return new ElementModule('div',{
-    "class":"application-logo gather-logo gather-icon",
-    html:"<?php echo GetWidget('dashboardConfig')->getParameter('gatherLabel'); ?> "+(new Date()).getFullYear()+""})
+return new ModuleArray([new SettingsNavigationMenu(application)], {
+    "class":"navigation-menu settings-menu"
+});
