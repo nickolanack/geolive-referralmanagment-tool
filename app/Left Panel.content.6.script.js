e8a035d4ba6a0e191410397542d58e1cc9a38183
nickolanack <nickblackwell82@gmail.com>
2021-06-17 22:20:24 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Left Panel.content.6.script.js
+++ app/Left Panel.content.6.script.js
@@ -2,5 +2,8 @@ return new Element('div',{'class':"survey-link",html:
 
 
 
-`<a class="survey-form" href="`+DashboardConfig.getValue('surveyUrl')+`">Survey</a>
-<a class="slack-link" href="`+DashboardConfig.getValue('slackUrl')+`">Slack Chat</a>`});
+`
+<ul class="">
+<li><a class="survey-form" href="`+DashboardConfig.getValue('surveyUrl')+`">Survey</a></li>
+<li><a class="slack-link" href="`+DashboardConfig.getValue('slackUrl')+`">Slack Chat</a></li>
+</ul.`});
