1d2a4b2ec8a2e7ceab80c1b86d0f5cbc904f8786
Nick Blackwell <nickblackwell82@gmail.com>
2021-06-29 12:17:05 -0700

    add light dark detection

--- Plugins/ReferralManagement/js/RecentItems.js
+++ Plugins/ReferralManagement/js/RecentItems.js
@@ -123,6 +123,22 @@ var RecentItems = (function() {
 					el.setStyles({
 					"background-color":tag.getColor()
 					});
+
+					var c=tag.getColor();
+					if(c[0]=="#"){
+						var c = c.substring(1);      // strip #
+						var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+						var r = (rgb >> 16) & 0xff;  // extract red
+						var g = (rgb >>  8) & 0xff;  // extract green
+						var b = (rgb >>  0) & 0xff;  // extract blue
+
+						var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+						if (luma < 40) {
+						    el.addClass('is-dark');
+						}
+					}
+
 				}
 			});
 
