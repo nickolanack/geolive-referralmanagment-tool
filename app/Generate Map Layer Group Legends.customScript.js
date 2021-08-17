2f574657e5d751042fdf35a192fdb1073af8825d
nickolanack <nickblackwell82@gmail.com>
2021-08-17 17:52:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Generate Map Layer Group Legends.customScript.js
+++ app/Generate Map Layer Group Legends.customScript.js
@@ -23,6 +23,7 @@
                 return LayerGroupLegend.EditLayerScript(
                     map,
                     "'.$groupName.'", 
+                    layerObject,
                     defaultBehaviorFn
                 );    
             
