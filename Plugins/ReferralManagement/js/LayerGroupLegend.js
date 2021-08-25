420cbc16b598038611ee943b0d940f289f67d44c
Nick Blackwell <nickblackwell82@gmail.com>
2021-08-25 08:57:42 -0700

    add line drawing options

--- Plugins/ReferralManagement/js/LayerGroupLegend.js
+++ Plugins/ReferralManagement/js/LayerGroupLegend.js
@@ -218,6 +218,11 @@ var LayerGroupLegend = (function() {
                     mutable:true,
                     showIcons:true,
                     showLabels:false,
+                    lineColor:"#000000",
+                    fillColor:"#000000",
+                    lineOpacity:1,
+                    fillOpacity:0.5,
+                    lineWidth:1
                     description:"",
                 };
 
