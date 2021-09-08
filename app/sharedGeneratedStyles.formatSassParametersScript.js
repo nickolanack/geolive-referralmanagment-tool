3c22f80ddfe79a19355d848ae36059042f8b1e64
nickolanack <nickblackwell82@gmail.com>
2021-09-08 06:29:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sharedGeneratedStyles.formatSassParametersScript.js
+++ app/sharedGeneratedStyles.formatSassParametersScript.js
@@ -8,5 +8,39 @@ $parameters['showFileThumbnails']=$config->getParameter("showFileThumbnails");
 $parameters['showUsersRoles']=$config->getParameter("showUsersRoles");
 $parameters['showSplitProjectDetail']=$config->getParameter("showSplitProjectDetail");
 
+    
+    $menuIconDefault=$config->getParameter('defaultMenuIcon')[0];
+
+
+
+    $priorityIcon=$config->getParameter('priorityIcon', array());
+    if(empty($priorityIcon)){
+        $priorityIcon=$menuIconDefault;
+    }else{
+        $priorityIcon=$priorityIcon[0];
+    }
+
+    $overdueIcon=$config->getParameter('overdueIcon', array());
+    if(empty($overdueIcon)){
+        $overdueIcon=$menuIconDefault;
+    }else{
+        $overdueIcon=$overdueIcon[0];
+    }
+
+
+    $remainingIcon=$config->getParameter('remainingIcon', array());
+    if(empty($remainingIcon)){
+        $remainingIcon=$menuIconDefault;
+    }else{
+        $remainingIcon=$remainingIcon[0];
+    }
+
+    $parameters['priorityIcon']=$priorityIcon;
+    $parameters['overdueIcon']=$overdueIcon;
+    $parameters['remainingIcon']=$remainingIcon;
+
+
+
+
 
 return $parameters;
