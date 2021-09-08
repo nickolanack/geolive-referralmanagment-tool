101478a9b3733aad9575b3bf14887313c5ae94ea
nickolanack <nickblackwell82@gmail.com>
2021-09-08 06:20:50 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sharedGeneratedStyles.formatSassParametersScript.js
+++ app/sharedGeneratedStyles.formatSassParametersScript.js
@@ -6,5 +6,7 @@ $config=GetWidget('dashboardConfig');
 $parameters['useFontAwesome']=$config->getParameter('useFontAwesome');
 $parameters['showFileThumbnails']=$config->getParameter("showFileThumbnails");
 $parameters['showUsersRoles']=$config->getParameter("showUsersRoles");
+$parameters['showSplitProjectDetail']=$config->getParameter("showSplitProjectDetail");
+
 
 return $parameters;
