2885947e99d0b122769814b672c1933cb64d9945
nickolanack <nickblackwell82@gmail.com>
2021-09-08 20:44:46 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sharedGeneratedStyles.formatSassParametersScript.js
+++ app/sharedGeneratedStyles.formatSassParametersScript.js
@@ -24,7 +24,7 @@ $parameters['showSplitProjectDetail']=$config->getParameter("showSplitProjectDet
             }else{
                 $mobileIcons[$mobile]=false;
             }
-        }
+        
     }
     
     $parameters['mobileIcons']=(object) $mobileIcons;
