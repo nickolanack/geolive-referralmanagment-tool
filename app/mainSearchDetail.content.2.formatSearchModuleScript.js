645c6101a990fbbcf9f7e6cfa8232f0dc23e093f
nickolanack <nickblackwell82@gmail.com>
2021-09-09 17:44:44 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/mainSearchDetail.content.2.formatSearchModuleScript.js
+++ app/mainSearchDetail.content.2.formatSearchModuleScript.js
@@ -1,16 +1,1 @@
-searchModule.addEvent('focus',function(){
-    setTimeout(function(){  
-    searchModule.results.setStyle('width', "370px");
-    },100);
-    
-})
-
-searchModule.addEvent('blur',function(){
-        
-    setTimeout(function(){
-    var p =  searchModule.search.searchInput.getCoordinates( searchModule.search.container);
-     searchModule.results.setStyle('width', (p.width) + "px");
-    },100);
-
-    
-})
+ProjectSearch.formatSearchModule(searchModule);
