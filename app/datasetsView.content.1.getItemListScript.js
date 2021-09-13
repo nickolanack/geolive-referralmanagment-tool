9a1d4b446d3194c1ff2f12b78d3a10c678a6621d
nickolanack <nickblackwell82@gmail.com>
2021-09-13 23:32:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetsView.content.1.getItemListScript.js
+++ app/datasetsView.content.1.getItemListScript.js
@@ -1,3 +1,12 @@
+
+if(item instanceof ProjectList&&item.getLockFilter){
+   var filter=item.getLockFilter();
+   if(filter&&filter.length==1&&ProjectTagList.getProjectTagsData(filter[0]).length>0){
+       return ProjectTagList.getProjectTagsData(filter[0]);
+   }
+}
+
+
 return ProjectTagList.getProjectTagsData('_root');
 
 
