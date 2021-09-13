436d3a17302efe075a0f5fd91b703d39b2389eaf
nickolanack <nickblackwell82@gmail.com>
2021-09-13 23:23:53 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects List Detail.content.1.formatModuleScript.js
+++ app/Projects List Detail.content.1.formatModuleScript.js
@@ -1,5 +1,7 @@
 
-if(item instanceof ProjectList){
-    
-    module.draw();
+if(item instanceof ProjectList&&item.getLockFilter){
+   var filter=item.getLockFilter();
+   if(filter&&filter.length==1&&ProjectTagList.getProjectTagsData(filter[0]).length>0){
+        module.draw();
+   }
 }
