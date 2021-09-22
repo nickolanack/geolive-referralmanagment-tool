ec0dfb37344c75f333d9a26487d98e39264070e5
nickolanack <nickblackwell82@gmail.com>
2021-09-22 20:32:11 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetsView.content.1.formatListItemModuleScript.js
+++ app/datasetsView.content.1.formatListItemModuleScript.js
@@ -1,11 +1,3 @@
 
 
 UIInteraction.addDatasetTypeEvents(child, childView, application);
-
-NamedCategory.AddClass(child, childView.getElement());
-
-if(item instanceof ProjectList){
-    if(item.isFilteringOnTag(child)){
-        childView.getElement().addClass('current-filter');
-    }
-}
