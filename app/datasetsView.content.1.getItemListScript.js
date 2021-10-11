1b6f77897c44bff7e6891bd361daa468002669f2
nickolanack <nickblackwell82@gmail.com>
2021-10-11 17:30:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetsView.content.1.getItemListScript.js
+++ app/datasetsView.content.1.getItemListScript.js
@@ -1,3 +1,12 @@
+
+if(item instanceof ProjectList){
+   var tags=item.getProjectListFilterChildTags();
+   if(tags){
+       return tags;
+   }
+}
+
+
 return ProjectTagList.getProjectTagsData('_root');
 
 
