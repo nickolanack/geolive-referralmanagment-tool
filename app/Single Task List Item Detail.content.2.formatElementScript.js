fb4c19939426aa1a234bf2e3255223c6ebd4b3e5
nickolanack <nickblackwell82@gmail.com>
2021-10-18 15:04:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Single Task List Item Detail.content.2.formatElementScript.js
+++ app/Single Task List Item Detail.content.2.formatElementScript.js
@@ -25,18 +25,7 @@
         
         var formName="taskForm";
 
-// 			var wizardTemplate = application.getDisplayController().getWizardTemplate(formName);
-// 			if ((typeof wizardTemplate) != 'function') {
 
-// 				if(window.console&&console.warn){
-// 					console.warn('Expected named wizardTemplate: '+formName+', to exist');
-// 				}
-
-// 			}
-// 			var modalFormViewController =  new PushBoxModuleViewer(application, {});
-// 			var wizard = wizardTemplate(item, {});
-//             wizard.buildAndShow(modalFormViewController, {template:"form"}); 
-            
             
             application.getDisplayController().displayPopoverForm(
 				formName, 
