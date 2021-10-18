907d179e59f38013aec2a8209f32553183bf462d
nickolanack <nickblackwell82@gmail.com>
2021-10-18 14:52:32 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects Overview Detail.content.3.getItemListScript.js
+++ app/Projects Overview Detail.content.3.getItemListScript.js
@@ -1,15 +1,1 @@
  ProjectList.GetUIListItems(item, callback);
- return;
- 
- 
- 
- ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-     var proposals=team.getProposals();
-     if(!application.getNamedValue("currentProject")){
-        application.setNamedValue("currentProject", proposals[0]);
-    }
-    callback(proposals)
- })
-
-       return null;
-       
