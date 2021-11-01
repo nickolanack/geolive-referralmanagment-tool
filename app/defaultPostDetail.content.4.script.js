fc06aaf44977c02bb961d04e7836c38958d1109f
nickolanack <nickblackwell82@gmail.com>
2021-11-01 16:00:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/defaultPostDetail.content.4.script.js
+++ app/defaultPostDetail.content.4.script.js
@@ -1,10 +1,10 @@
 return new ModuleArray([
         new ElementModule('button', {
             html:"Delete",
-            "class":"inline-btn"
+            "class":"inline-btn remove"
         }),
          new ElementModule('button', {
             html:"Edit",
-            "class":"inline-btn"
+            "class":"inline-btn edit"
         })
     ], {identifier:"post-buttons"});
