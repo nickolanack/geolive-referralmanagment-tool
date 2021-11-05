33aa8b1f24ef458986ce80cf9f21ea4548c3c682
Nick Blackwell <nickblackwell82@gmail.com>
2021-11-05 15:18:55 -0700

    add user notifications tray

--- Plugins/ReferralManagement/js/UserNotifications.js
+++ Plugins/ReferralManagement/js/UserNotifications.js
@@ -9,15 +9,44 @@ var UserNotifications=(function(){
 
 		var application =ReferralManagementDashboard.getApplication();
 
-		return new ElementModule('span',{
-		    "class":"notifications",
-		    events:{click:function(){
-		        
-		        var controller = application.getNamedValue('navigationController');
-						controller.navigateTo("Notifications", "Main");
-		        
-		    }}
-		})
+		return [
+			new ElementModule('span',{
+			    "class":"notifications",
+			    events:{click:function(){
+			        
+			        var controller = application.getNamedValue('navigationController');
+							controller.navigateTo("Notifications", "Main");
+			        
+			    }}
+			}),
+			new ElementModule('span',{
+			    "class":"dark-toggle",
+			    events:{click:function(){
+			       
+			       var el=$$('.ui-view.dashboard-main')[0];
+			       if(el.hasClass('dark')){
+			       		el.removeClass('dark');
+			       		return;
+			       }
+			       el.addClass('dark');
+			       
+			    }}
+			}),
+			new ElementModule('span',{
+			    "class":"admin-toggle",
+			    events:{click:function(){
+			        
+
+			        var el=$$('.ui-view.dashboard-main')[0];
+			       if(el.hasClass('hide-admin')){
+			       		el.removeClass('hide-admin');
+			       		return;
+			       }
+			       el.addClass('hide-admin');
+			        
+			    }}
+			})
+		]
 
 	}
 
