5945e302c6ed523a36b13d77440d8afcb2d666e0
nickolanack <nickblackwell82@gmail.com>
2021-11-10 17:13:41 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Kml renderer.resolveSpatialFilePathScript.js
@@ -0,0 +1,26 @@
+
+if(strpos($file,'{datawarehouse}')===0){
+    //throw new \Exception(print_r($args, true));
+    
+    
+    $id=explode('{project:', $file);
+    $id=array_pop($id);
+    $id=explode('}',$id);
+    $id=array_shift($id);
+    
+    // if(intval($id)<=0){
+    //     $id=30;
+    // }
+    
+    
+    $data=GetPlugin('ReferralManagement')->getProjectData($id);
+    
+    if(file_exists('/srv/gather_gis/Data_Warehouse/'.$data['metadata']->file)){
+        return '/srv/gather_gis/Data_Warehouse/'.$data['metadata']->file;
+    }
+    
+    return $data['metadata']->file;
+    
+}
+
+return $file;
