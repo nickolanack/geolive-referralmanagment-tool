a3890df2a69b4ea9ab5c08b3f521e3848ce1c2f0
nickolanack <nickblackwell82@gmail.com>
2021-11-10 06:53:14 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Kml renderer.resolveSpatialFilePathScript.js
+++ app/Kml renderer.resolveSpatialFilePathScript.js
@@ -1,6 +1,15 @@
 
 if(strpos($file,'{datawarehouse}')===0){
     throw new \Exception(print_r($args, true));
+    
+    
+    $id=explode('{project:', $file);
+    $id=array_pop($id);
+    $id=explode('}',$id);
+    $id=array_shift($id);
+    $data=GetPlugin('ReferralManagement')->getProjectData($id);
+    return $data['metadata']->file;
+    
 }
 
 return $file;
