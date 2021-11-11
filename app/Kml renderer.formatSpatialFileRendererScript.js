e55da9a57913009985ebbf0b422032a81cba64c5
nickolanack <nickblackwell82@gmail.com>
2021-11-11 21:07:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Kml renderer.formatSpatialFileRendererScript.js
+++ app/Kml renderer.formatSpatialFileRendererScript.js
@@ -1,1 +1,5 @@
 Broadcast('kmlRender', 'format', $args);
+
+if(isset($args->options)&&isset($args->options->showAttributes)&&$args->options->showAttributes==true){
+    $transcoder->includeAttributes();
+}
