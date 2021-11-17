7a2cf8212a2c52d2ce2e5989067f3ac9ba25b97a
Nick Blackwell <nickblackwell82@gmail.com>
2021-11-17 11:06:16 -0800

    try to use CreateEditBtn instead

--- Plugins/ReferralManagement/js/ConfigItem.js
+++ Plugins/ReferralManagement/js/ConfigItem.js
@@ -54,54 +54,59 @@ var ConfigItem = (function() {
             return div;
         }
 
-        div.appendChild(new Element('button', {
+        // div.appendChild(new Element('button', {
 
-            html: "edit",
-            'class': 'inline-edit',
-            events: {
-                click: function() {
-
-
-                    var configValue = (new MockDataTypeItem({
-                        mutable: true,
-                        label: item.getEditLabel(),
-                        text: text,
-
-                    }));
-
-                    configValue.addEvent('save', function() {
-
-                        content.innerHTML = configValue.getText();
-                        text = configValue.getText();
-
-                        (new AjaxControlQuery(CoreAjaxUrlRoot, (options.userAuth ? 'user_' : '') + 'set_configuration_field', {
-                            "widget": item.getWidget(),
-                            "field": {
-                                "name": item.getParam(),
-                                "value": configValue.getText()
-                            }
-                        })).execute();
+        //     html: "edit",
+        //     'class': 'inline-edit',
+        //     events: {
+        //         click: function() {
 
 
-                    });
+        //             var configValue = (new MockDataTypeItem({
+        //                 mutable: true,
+        //                 label: item.getEditLabel(),
+        //                 text: text,
+
+        //             }));
+
+        //             configValue.addEvent('save', function() {
+
+        //                 content.innerHTML = configValue.getText();
+        //                 text = configValue.getText();
+
+        //                 (new AjaxControlQuery(CoreAjaxUrlRoot, (options.userAuth ? 'user_' : '') + 'set_configuration_field', {
+        //                     "widget": item.getWidget(),
+        //                     "field": {
+        //                         "name": item.getParam(),
+        //                         "value": configValue.getText()
+        //                     }
+        //                 })).execute();
+
+
+        //             });
 
 
 
-                    (new UIModalDialog(
-                        ReferralManagementDashboard.getApplication(),
-                        configValue, {
-                            "formName": item.getForm(),
-                            "formOptions": {
-                                template: "form"
-                            }
-                        }
-                    )).show();
+        //             (new UIModalDialog(
+        //                 ReferralManagementDashboard.getApplication(),
+        //                 configValue, {
+        //                     "formName": item.getForm(),
+        //                     "formOptions": {
+        //                         template: "form"
+        //                     }
+        //                 }
+        //             )).show();
 
-                }
-            }
+        //         }
+        //     }
 
-        }));
+        // }));
 
+
+        var btn=ConfigItem.CreateEditBtn(item, options);
+        if(btn){
+            div.appendChild(btn);
+        }
 
 
         return div;
