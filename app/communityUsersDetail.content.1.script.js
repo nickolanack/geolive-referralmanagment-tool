9ad52a1eb988098d46d7851546e9515d281ffae2
nickolanack <nickblackwell82@gmail.com>
2021-11-17 18:53:17 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/communityUsersDetail.content.1.script.js
+++ app/communityUsersDetail.content.1.script.js
@@ -1,10 +1,1 @@
-var p=new Element('p',{
-			"class":"hint",
-			"html":''
-		});
-
-		p.appendChild(HtmlContent.MakeInfoButtonModule({
-			description:'You can approve new site users.'
-		}));
-		
-		return p;
+return UserGroups.PendingCommunityInformation();
