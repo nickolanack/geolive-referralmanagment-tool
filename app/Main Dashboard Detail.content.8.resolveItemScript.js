61b526a167b132fa76afb63aef0c6663139e4749
nickolanack <nickblackwell82@gmail.com>
2021-11-23 05:37:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Main Dashboard Detail.content.8.resolveItemScript.js
+++ app/Main Dashboard Detail.content.8.resolveItemScript.js
@@ -1,17 +1,1 @@
-
-
-
-
-var recent= <?php 
-
- $posts=($plugin=GetPlugin('Discussions'))->getPostsForItem(145, 'widget', 'wabun');
- echo json_encode($posts, JSON_PRETTY_PRINT);
-
-
-?>;
-
-
-
-RecentItems.RecentUserActivity.setListData(recent.reverse(),'.user');
-
 return RecentItems.RecentUserActivity;
