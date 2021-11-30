cff569baafdb289bb7682b463ef1c49cfef6b304
nickolanack <nickblackwell82@gmail.com>
2021-11-30 18:35:57 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userProfileDetailView.content.2.resolveItemScript.js
+++ app/userProfileDetailView.content.2.resolveItemScript.js
@@ -1,3 +1,11 @@
 return new ProjectList({
-    "label":  (item.getId()==AppClient.getId()?"Your":item.getName()+"'s")+" Projects"
+    "label":  (item.getId()==AppClient.getId()?"Your":item.getName()+"'s")+" Projects",
+    projects:function(callback){
+
+		    	ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+     				callback(team.getProjects().filter(function(p){
+     					p.getProjectSubmitterId()==user.getId()
+     				}));
+     			});
+            }
 })
