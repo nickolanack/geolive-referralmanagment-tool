0573300c8a114e99c5fd957ce7b7ea3812456f2a
nickolanack <nickblackwell82@gmail.com>
2021-11-30 19:09:56 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userProfileDetailView.content.2.resolveItemScript.js
+++ app/userProfileDetailView.content.2.resolveItemScript.js
@@ -1,11 +1,1 @@
-return new ProjectList({
-    "label":  (item.getId()==AppClient.getId()?"Your":item.getName()+"'s")+" Projects",
-    projects:function(callback){
-
-		    	ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-     				callback(team.getProjects().filter(function(p){
-     					return p.getProjectSubmitterId()+""==item.getId()+""
-     				}));
-     			});
-            }
-})
+return ProjectList.UsersProjectList(item);
