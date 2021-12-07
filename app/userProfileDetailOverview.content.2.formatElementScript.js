07cad89edb17a35f80d6894d0dcbda0f19b20b63
nickolanack <nickblackwell82@gmail.com>
2021-12-07 22:38:05 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/userProfileDetailOverview.content.2.formatElementScript.js
+++ app/userProfileDetailOverview.content.2.formatElementScript.js
@@ -1,4 +1,11 @@
 labelEl.addClass('email-click');
+
+if(item.getId()+""==AppClient.getId()+""){
+    
+    labelEl.addClass("is-you");
+    return;
+}
+
 labelEl.addEvent('click', function(){
     
     new Element('a', {
