f4b12397f5e5af0e1f62dcb8462a025a185e074a
nickolanack <nickblackwell82@gmail.com>
2021-12-15 05:50:58 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.11.formatModuleScript.js
+++ app/singleProjectOverviewDetail.content.11.formatModuleScript.js
@@ -1,4 +1,7 @@
 setTimeout(function(){
-module.getElement().appendChild(new Element('button', {html:"Add notes"}));
+    module.getElement().addClass('show-content');
+module.getElement().appendChild(new Element('button', {html:"Add notes", events:{click:function(){
+    module.getElement().addClass('show-content');
+}}}));
 console.error('debug this')
 }, 500);
