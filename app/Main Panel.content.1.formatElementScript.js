316cabd3b2b1acb0abfe0cb6a275590cae73c2ee
nickolanack <nickblackwell82@gmail.com>
2022-01-20 22:45:45 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Main Panel.content.1.formatElementScript.js
+++ app/Main Panel.content.1.formatElementScript.js
@@ -1,7 +1,12 @@
 application.getNamedValue('navigationController',function(controller){
-    controller.addEvent('navigate', function(state) {
+    controller.addEvent('navigate', function(state, state, options, item) {
         console.log(state); 
         valueEl.innerHTML=state.view;
+    })
+    
+    controller.addEvent('childNavigation', function(menu, state, options, item) {
+        console.log(state); 
+        
     })
     
 })
