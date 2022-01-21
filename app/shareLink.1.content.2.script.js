e5c54c4244e9e6ee8589511939ccb05e92c951d8
nickolanack <nickblackwell82@gmail.com>
2022-01-21 18:07:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/shareLink.1.content.2.script.js
+++ app/shareLink.1.content.2.script.js
@@ -1,7 +1,17 @@
-return new ElementModule('button', {
+var button= new ElementModule('button', {
     "class":"form-btn primary-btn share",
     'html':"Create share link",
     events:{click:function(){
         
+        (new AjaxControlQuery(CoreAjaxUrlRoot, 'generate_share_link', {
+			'plugin': "ReferralManagement"
+		})).addEvent('success', function(resp){
+		    
+		    console.log(resp);
+		    
+		}).execute();
+    
     }}
-})
+});
+
+return button;
