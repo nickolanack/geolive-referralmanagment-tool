c83f92bae1bdcccab5bb418eb28787c0e6252edc
nickolanack <nickblackwell82@gmail.com>
2022-01-28 19:54:33 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects List Detail.content.6.script.js
+++ app/Projects List Detail.content.6.script.js
@@ -1,5 +1,5 @@
 if(item instanceof ProjectList&&item.getShowMinimize&&item.getShowMinimize()){
     return UIInteraction.createSectionToggle(function(v) {
         return v instanceof UIListViewModule 	 
-    }).
+    });
 }
