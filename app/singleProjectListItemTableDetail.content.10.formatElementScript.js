cfd8dd885f81f501cfdac2e6254e89dd444354af
nickolanack <nickblackwell82@gmail.com>
2022-02-01 20:05:44 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.10.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.10.formatElementScript.js
@@ -1,4 +1,4 @@
-el.addClass("inline");
+el.addClass("inline tag-width");
 el.setAttribute("data-col","community");
 
 
@@ -6,8 +6,9 @@ var communities=([item.getProjectCommunity()]).concat(item.getCommunitiesInvolve
 //return communities.slice(0,2).join(', ')+(communities.length>2?', '+communities.length+' other'+(communities.length==3?'':'s'):'')
 
 var types=communities;
-types.slice(1).forEach(function(type){
-  var tag=el.appendChild(new Element('span', {"class":"field-value alt-tag"}));
+types.slice(0,2).forEach(function(type){
+  var tag=el.appendChild(new Element('span', {"class":"field-value"}));
+  tag.innerHTML=type;
   //RecentItems.colorizeEl(tag, type);
     tag.addEvent('click', function(e){
         e.stop();//Propagation()
