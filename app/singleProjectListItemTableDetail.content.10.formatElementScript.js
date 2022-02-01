2577da9cb8e73a92b39b127d453a6ae32a1a1c96
nickolanack <nickblackwell82@gmail.com>
2022-02-01 19:14:18 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.10.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.10.formatElementScript.js
@@ -1,18 +1,2 @@
-el.addClass("inline check");
-el.setAttribute("data-col","");
-
-if(item instanceof MissingProject){
-    return null;
-}
-
-var input=new Element('input', {type:"checkbox", events:{click:function(event){
-    event.stopPropagation();
-    ProjectSelection.handleSelection(item, this);
-}}});
-
-if(ProjectSelection.hasProject(item)){
-    input.checked=true;
-}
-
-el.appendChild(input)
-
+el.addClass("inline");
+el.setAttribute("data-col","status");
