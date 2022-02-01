8dfbc1e03607a0b0487654f541d294e9fcae682c
nickolanack <nickblackwell82@gmail.com>
2022-02-01 20:04:26 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.10.fieldValueScript.js
+++
@@ -1,3 +0,0 @@
-
-var communities=([item.getProjectCommunity()]).concat(item.getCommunitiesInvolved())
-return communities.slice(0,2).join(', ')+(communities.length>2?', '+communities.length+' other'+(communities.length==3?'':'s'):'')
--- app/singleProjectListItemTableDetail.content.10.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.10.formatElementScript.js
@@ -1,2 +1,16 @@
 el.addClass("inline");
 el.setAttribute("data-col","community");
+
+
+var communities=([item.getProjectCommunity()]).concat(item.getCommunitiesInvolved())
+//return communities.slice(0,2).join(', ')+(communities.length>2?', '+communities.length+' other'+(communities.length==3?'':'s'):'')
+
+var types=communities;
+types.slice(1).forEach(function(type){
+  var tag=el.appendChild(new Element('span', {"class":"field-value alt-tag"}));
+  //RecentItems.colorizeEl(tag, type);
+    tag.addEvent('click', function(e){
+        e.stop();//Propagation()
+       // UIInteraction.navigateToNamedCategoryType(type);
+    });
+});
