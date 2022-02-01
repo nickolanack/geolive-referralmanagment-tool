91b6fb57931fcd75d4d0868f0ae4ad48142c8947
nickolanack <nickblackwell82@gmail.com>
2022-02-01 19:40:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.10.fieldValueScript.js
+++ app/singleProjectListItemTableDetail.content.10.fieldValueScript.js
@@ -1,1 +1,3 @@
-return item.getCommunitiesInvolved().join(', ');
+
+var communities=([item.getProjectCommunity()]).concat(item.getCommunitiesInvolved())
+return communities.slice(0,2).join(', ')+(communites.length>2?' and '+communites.length+' other'+(communites.length==3?'':'s'):'')
