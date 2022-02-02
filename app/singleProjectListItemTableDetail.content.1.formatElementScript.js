8b90676cfe18d847a3644ee64d3b7473f7448597
nickolanack <nickblackwell82@gmail.com>
2022-02-02 21:59:57 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.1.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.1.formatElementScript.js
@@ -2,8 +2,8 @@ el.addClass("inline check");
 el.setAttribute("data-col","id");
 
 
-if(item.getPermits){
-    var permits=item.getPermits();
+if(item.getPermitIds){
+    var permits=item.getPermitIds();
     if(permits.length){
         el.addClass('has-ids');
         el.addClass('has-'+(permits.length)+'id'+(permits.length==1?'':'s'))
