edd69581feae473c437e63129b026dd8475dffb2
nickolanack <nickblackwell82@gmail.com>
2022-02-10 20:55:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.14.formatModuleScript.js
+++ app/singleProjectOverviewDetail.content.14.formatModuleScript.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
     module.getElement().addClass('has-hidden-content hide');
-    var button=module.getElement().appendChild(new Element('button', {html:"Add notes", "class":"form-btn primary-btn edit", events:{click:function(){
+    var button=module.getElement().appendChild(new Element('button', {html:"Add notes", "class":"notes form-btn primary-btn edit", events:{click:function(){
         module.getElement().removeClass('hide');
         button.parentNode.removeChild(button);
     }}}));
