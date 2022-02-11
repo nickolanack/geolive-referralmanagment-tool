aa030d82c044baa1b82489953ee200fe849f7a4c
nickolanack <nickblackwell82@gmail.com>
2022-02-11 22:17:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.content.9.onRenderTextFieldScript.js
+++ app/widget.ProposalWizardBuilder.65.1.content.9.onRenderTextFieldScript.js
@@ -1,4 +1,5 @@
 if(window.GuestProposal&&item instanceof GuestProposal){
+    inputElement.setStyle('display','none');
     inputElement.disabled=true;
     textField.getElement().addClass('disabled');
 }
