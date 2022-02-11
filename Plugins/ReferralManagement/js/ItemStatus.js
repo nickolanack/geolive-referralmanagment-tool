41e384baf1edf279768e3dc673e8ca633b5885d9
Nick Blackwell <nickblackwell82@gmail.com>
2022-02-11 12:06:42 -0800

    use status with hard coded values

--- Plugins/ReferralManagement/js/ItemStatus.js
+++ Plugins/ReferralManagement/js/ItemStatus.js
@@ -4,7 +4,12 @@ var ItemStatus = (function() {
 	var ItemStatus = new Class({
 
 
+		getProjectStatus:function(){
 
+			return [
+				'Filing', 'Monitoring'
+			]
+		}
 	});
 
 
@@ -12,32 +17,14 @@ var ItemStatus = (function() {
 
 
 
-		el.addClass("inline tag-width");
-		el.setAttribute("data-col", "community");
 
-
-		var communities = ([item.getProjectCommunity()]).concat(item.getCommunitiesInvolved())
-		//return communities.slice(0,2).join(', ')+(communities.length>2?', '+communities.length+' other'+(communities.length==3?'':'s'):'')
-
-		var showRootCommunity = false;
-		if (!showRootCommunity) {
-			var i = communities.indexOf(UserGroups.GetCollective());
-			if (i >= 0) {
-				communities.splice(i, 1);
-			}
-
-		}
-
-		if (communities.length == 0) {
-			return;
-		}
-
-		var types = communities;
+		var types = item.getProjectStatus();
+		
 		var plainTag = new NamedCategory({
 			name: "",
 			shortName: "",
 			description: "",
-			type: "Community.tag",
+			type: "Status.tag",
 			id: -1,
 			color: "#f0f0f0",
 			category: "_"
@@ -49,7 +36,7 @@ var ItemStatus = (function() {
 		RecentItems.colorizeEl(valueEl, types[0], plainTag);
 		el.addEvent('click', function(e) {
 			e.stop(); //Propagation()
-			UIInteraction.navigateToNamedCommunityType(types[0]);
+			UIInteraction.navigateToNamedStatusType(types[0]);
 		});
 
 
@@ -61,21 +48,21 @@ var ItemStatus = (function() {
 
 			}));
 
-			var content = new Element('div');
+			//var content = new Element('div');
 			//var textContent='';
 
 			types.slice(1).forEach(function(type, i) {
 
 
-				var tag = content.appendChild(new Element('span', {
-					"class": "field-value tip-tag"
+				var tag = el.appendChild(new Element('span', {
+					"class": "field-value alt-tag"
 				}));
 
 				//tag.innerHTML=type;
 				RecentItems.colorizeEl(tag, type, plainTag);
 				tag.addEvent('click', function(e) {
 					e.stop(); //Propagation()
-					UIInteraction.navigateToNamedCommunityType(type);
+					UIInteraction.navigateToNamedStatusType(type);
 				});
 
 				tag.innerHTML = type;
@@ -83,11 +70,11 @@ var ItemStatus = (function() {
 
 			});
 
-			new UIPopover(othersEl, {
-				content: content,
-				anchor: UIPopover.AnchorAuto(),
-				clickable: true
-			});
+			// new UIPopover(othersEl, {
+			// 	content: content,
+			// 	anchor: UIPopover.AnchorAuto(),
+			// 	clickable: true
+			// });
 
 		}
 
