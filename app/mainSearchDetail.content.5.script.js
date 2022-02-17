05121a9b29c0482de89fd6190dad181da33dc4de
nickolanack <nickblackwell82@gmail.com>
2022-02-17 17:33:57 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/mainSearchDetail.content.5.script.js
+++ app/mainSearchDetail.content.5.script.js
@@ -1,13 +1,13 @@
 
-var fitSticky=function(){
+// var fitSticky=function(){
     
-   var bar = $$('.ui-view.search-panel')[0];
-   var frame=$$('.ui-view.main-panel')[0];
+//   var bar = $$('.ui-view.search-panel')[0];
+//   var frame=$$('.ui-view.main-panel')[0];
    
-   if(bar&&frame){
-       bar.setStyle('width', frame.getSize().x+'px');
-   }
+//   if(bar&&frame){
+//       bar.setStyle('width', frame.getSize().x+'px');
+//   }
     
-}
-window.addEvent('resize', fitSticky);
-fitSticky();
+// }
+// window.addEvent('resize', fitSticky);
+// fitSticky();
