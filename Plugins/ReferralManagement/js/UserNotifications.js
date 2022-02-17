5361c9ce97fca87a0f318ba47d0e00fbb66583be
Nick Blackwell <nickblackwell82@gmail.com>
2022-02-17 10:49:33 -0800

    add logout

--- Plugins/ReferralManagement/js/UserNotifications.js
+++ Plugins/ReferralManagement/js/UserNotifications.js
@@ -9,7 +9,7 @@ var UserNotifications=(function(){
 
 		var application =ReferralManagementDashboard.getApplication();
 
-		return [
+		var btns = [
 			new ElementModule('span',{
 			    "class":"notifications",
 			    events:{click:function(){
@@ -37,16 +37,32 @@ var UserNotifications=(function(){
 			    events:{click:function(){
 			        
 
-			        var el=$$('body')[0];
-			       if(el.hasClass('hide-admin')){
-			       		el.removeClass('hide-admin');
-			       		return;
-			       }
-			       el.addClass('hide-admin');
+			    	if(confirm('Are you sure you want to log out')){
+			    		AppClient.logout();
+			    	}
 			        
 			    }}
 			})
-		]
+		];
+
+		if(AppClient.getUserType()==='admin'){
+			btns.push(new ElementModule('span',{
+				    "class":"admin-toggle",
+				    events:{click:function(){
+				        
+
+				        var el=$$('body')[0];
+				       if(el.hasClass('hide-admin')){
+				       		el.removeClass('hide-admin');
+				       		return;
+				       }
+				       el.addClass('hide-admin');
+				        
+				    }}
+				}))
+		}
+
+		return btns;
 
 	}
 
