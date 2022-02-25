7a11261ce2e65ddb2b4517b55ded44e5aa360893
nickolanack <nickblackwell82@gmail.com>
2022-02-25 18:19:50 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.children.2.content.5.formatModuleScript.js
+++ app/widget.ProposalWizardBuilder.65.1.children.2.content.5.formatModuleScript.js
@@ -9,9 +9,9 @@ if(!module.hasWizard()){
     setInterval(function(){
             
         var wizards=module.getChildWizards();
-        console.log(wizards.map(function(wizard){
+        console.log(JSON.stringify(wizards.map(function(wizard){
             return wizard.getData();
-        }));
+        })));
         
     }, 3000);
     
