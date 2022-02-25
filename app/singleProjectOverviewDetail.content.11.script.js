2a7e3d82930ca85ed933e48ed580d0804c733829
nickolanack <nickblackwell82@gmail.com>
2022-02-25 20:55:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.11.script.js
+++ app/singleProjectOverviewDetail.content.11.script.js
@@ -1,3 +1,7 @@
- return UIInteraction.createSectionToggle(function(v) {
+ var b =  UIInteraction.createSectionToggle(function(v) {
     return (['status-assessment', 'status-processing']).indexOf(v.getIdentifier())>=0; 	 
  });
+ 
+ b.addClass('some-flow-toggle');
+ 
+ return b;
