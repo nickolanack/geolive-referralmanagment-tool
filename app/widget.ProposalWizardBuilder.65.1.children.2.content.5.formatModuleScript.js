3eabf9d6f54b5636e883d146ea08b278109c434d
nickolanack <nickblackwell82@gmail.com>
2022-03-02 05:28:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.children.2.content.5.formatModuleScript.js
+++ app/widget.ProposalWizardBuilder.65.1.children.2.content.5.formatModuleScript.js
@@ -1,1 +1,13 @@
-(new UIFormListBehavior(module));
+(new UIFormListBehavior(module)).setNewItemFn(function(){
+    
+    return new MockDataTypeItem({
+        id:-1,
+        type:item.getType(),
+        number:'',
+        issued:'',
+        expiry:''
+    });
+}
+    
+    
+})
