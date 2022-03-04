9a774d02186c78810a4ba7f44ae846ada317b196
nickolanack <nickblackwell82@gmail.com>
2022-03-04 20:35:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.content.7.onRenderTextFieldScript.js
+++
@@ -1,5 +0,0 @@
-if(window.GuestProposal&&item instanceof GuestProposal){
-    inputElement.disabled=true;
-    textField.getElement().addClass('disabled');
-    textField.setLabel('Submission sent')
-}
--- app/widget.ProposalWizardBuilder.65.1.content.8.onRenderTextFieldScript.js
+++ app/widget.ProposalWizardBuilder.65.1.content.8.onRenderTextFieldScript.js
@@ -1,5 +1,5 @@
 if(window.GuestProposal&&item instanceof GuestProposal){
-    //inputElement.disabled=true;
-    //textField.getElement().addClass('disabled');
-    textField.setLabel('Requested response date')
+    inputElement.disabled=true;
+    textField.getElement().addClass('disabled');
+    textField.setLabel('Submission sent')
 }
