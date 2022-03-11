efa2a41bf5891785970510b11e25a27d6eb98cb3
nickolanack <nickblackwell82@gmail.com>
2022-03-11 19:26:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.4.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.4.formatElementScript.js
@@ -1,2 +1,6 @@
 el.addClass("inline");
 el.setAttribute("data-col","created");
+
+
+el.setAttribute('data-day', item.getCreationDate().split(' ').shift());
+el.setAttribute('data-time', item.getCreationDate().split(' ').pop());
