1aef466b4faa164eadcbb8fd90858fbdb6552cc0
nickolanack <nickblackwell82@gmail.com>
2022-03-15 17:15:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/dashboardLoader.content.1.script.js
+++ app/dashboardLoader.content.1.script.js
@@ -1,7 +1,18 @@
-return function(){
-   
-        
-    DashboardLoader.loadUserDashboardView(application);
-    
-   
-}
+
+
+
+return `<div class="application-logo" style="    height: 200px;
+    background-size: auto 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin-bottom: 50px;
+    display: inline-block;
+    width: 100%;
+    line-height: 30;
+    position: relative;
+    top: 100px;
+    top: calc( 50% - 200px );
+    text-align: center;
+    position: fixed;
+
+">`+DashboardConfig.getValue('loadingLabel')+`</div>`;
---
+++ app/dashboardLoader.content.2.script.js
@@ -0,0 +1,7 @@
+return function(){
+   
+        
+    DashboardLoader.loadUserDashboardView(application);
+    
+   
+}
