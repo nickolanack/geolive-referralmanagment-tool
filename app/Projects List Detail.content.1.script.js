5db295523abdf3073f0a25ade62c7ac7b3e07da9
nickolanack <nickblackwell82@gmail.com>
2022-03-16 21:15:52 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Projects List Detail.content.1.script.js
+++ app/Projects List Detail.content.1.script.js
@@ -1,14 +1,22 @@
 
 
 
-if(item instanceof ProjectList&&item.getShowMinimize&&item.getShowMinimize()&&item.getProjectList().length>0){
+if(item instanceof ProjectList&&item.getShowMinimize&&item.getShowMinimize()){
     var options={};
     
     if(item.getStartMinimized){
         options.startHidden=item.getStartMinimized();
     }
     
-    return UIInteraction.createSectionToggle(function(v) {
+    
+    
+    var toggle= UIInteraction.createSectionToggle(function(v) {
         return v instanceof UIListViewModule 	 
     },options);
+    
+    item.getProjectList(function(list){
+        toggle.getElement().addClass('is-empty');
+    });
+    
+    return toggle;
 }
