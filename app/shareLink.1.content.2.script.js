53454e12cb56927df4f03ecb65bde86b0f64f0dc
nickolanack <nickblackwell82@gmail.com>
2022-03-16 05:32:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/shareLink.1.content.2.script.js
+++ app/shareLink.1.content.2.script.js
@@ -6,6 +6,6 @@
 
         var link=window.location.href.split('/').slice(0,3).join('/')+'/proposal/'+item.getId()+'/auto';
         return new ElementModule('p', {
-            html:'<a id="theShareLink" style="color:mediumseagreen;" target="_blank" href="'+link+'"></a>",
+            html:'<a id="theShareLink" style="color:mediumseagreen;" target="_blank" href="'+link+'"></a>',
             
         });
