942e343e578efbd4818f046ceac5d711fc93a039
nickolanack <nickblackwell82@gmail.com>
2022-03-16 20:16:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.1.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.1.formatElementScript.js
@@ -12,7 +12,7 @@ if(item.getPermitIds){
     var ids=el.appendChild(new Element('span',{"class":"id-items"}));
     
     new UIPopover(ids, {
-        description:permits.join("<br/>"),
+        description:'Permits &amp; IDs'+permits.join("<br/>"),
         anchor:UIPopover.AnchorAuto()
     });
 }
