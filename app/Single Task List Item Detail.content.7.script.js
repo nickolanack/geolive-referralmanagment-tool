e60fee489f8369560fcd27c16c60a6abe4244c28
nickolanack <nickblackwell82@gmail.com>
2022-03-25 17:36:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Single Task List Item Detail.content.7.script.js
+++ app/Single Task List Item Detail.content.7.script.js
@@ -5,7 +5,7 @@ return function(viewer, element, parentModule){
     element.addClass(item.isComplete()?"complete":"not-complete");
  
     
-    
+    var pop;
     var el=element.appendChild(new Element('div', {"class":"completion-indicator", 
     events:{click:function(e){
         e.stop();
@@ -17,10 +17,11 @@ return function(viewer, element, parentModule){
        if(item.isComplete()){
            element.removeClass('not-complete');
            element.addClass('complete');
+           pop.setDescription('click to mark task incomplete');
        }else{
            element.addClass('not-complete');
            element.removeClass('complete');
-          
+           pop.setDescription('click to mark task complete');
        }
        
  
@@ -29,6 +30,12 @@ return function(viewer, element, parentModule){
         
     }}}));
     
+    
+    pop=new UIPopover(el, {
+        description:item.isComplete()?'click to mark task incomplete':'click to mark task complete'),
+        anchor:UIPopover.AnchorAuto()
+    });
+}
     
     
 }
