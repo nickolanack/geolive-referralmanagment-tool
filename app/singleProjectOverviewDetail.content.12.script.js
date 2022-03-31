a2c1489693466a3f87cb98f102c2fd628eeb3f4d
nickolanack <nickblackwell82@gmail.com>
2022-03-31 02:32:40 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.12.script.js
+++ app/singleProjectOverviewDetail.content.12.script.js
@@ -1,10 +1,6 @@
  var b =  UIInteraction.createSectionToggle(function(v) {
     return (['status-assessment', 'status-processing']).indexOf(v.getIdentifier())>=0; 	 
  });
- console.log('add init');
- b.addEvent('init', function(){
-     console.log('set up special behavior')
- });
  
  b.getElement().addClass('some-flow-toggle');
  
--- app/singleProjectOverviewDetail.content.16.script.js
+++ app/singleProjectOverviewDetail.content.16.script.js
@@ -2,6 +2,11 @@
     return v instanceof DiscussionModule;	 
  });
  
+ console.log('add init');
+ b.addEvent('init', function(){
+     console.log('set up special behavior')
+ });
+ 
  b.getElement().addClass('some-discussion-toggle');
  
  return b;
