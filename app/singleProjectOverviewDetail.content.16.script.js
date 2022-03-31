b31ae4df1ff224a141d90ad5ea9fc526da154a1a
nickolanack <nickblackwell82@gmail.com>
2022-03-31 04:18:14 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.16.script.js
+++ app/singleProjectOverviewDetail.content.16.script.js
@@ -2,7 +2,7 @@
     return v instanceof DiscussionModule;	 
  });
  
- console.log('add init');
+
  b.addEvent('init', function(){
      b.getFirst().getDiscussion(function(discussion){
         discussion.onStarted(function(){
@@ -16,12 +16,13 @@
             var reply=b.findTargets(function(v){
                 return v.getIdentifier()==='discussion-reply';
             });
-            console.log(reply);
+           
             var replyView=reply.length>0?reply[0]:null;
             if(replyView){
                 replyView.getNamedValue('addNotesButton',function(replyBtn){
                     replyBtn.addEvent('click', function(){
                         b.getElement().addClass('with-reply-box');
+                        b.expand();
                     });
                     b.on('collapse',function(){
                         b.getElement().removeClass('with-reply-box')
