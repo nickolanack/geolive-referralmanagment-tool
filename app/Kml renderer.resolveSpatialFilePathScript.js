0a9749e0874692284b7c425146f249fdbb9df4a0
nickolanack <nickblackwell82@gmail.com>
2022-04-05 16:08:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Kml renderer.resolveSpatialFilePathScript.js
+++ app/Kml renderer.resolveSpatialFilePathScript.js
@@ -1,21 +1,44 @@
 
 if(strpos($file,'{datawarehouse}')===0){
-    //throw new \Exception(print_r($args, true));
-    
-    
-    $id=explode('{project:', $file);
-    $id=array_pop($id);
-    $id=explode('}',$id);
-    $id=array_shift($id);
-    
-    // if(intval($id)<=0){
-    //     $id=30;
-    // }
-    
-    
-    $data=GetPlugin('ReferralManagement')->getProjectData($id);
-    return $data['metadata']->file;
-    
-}
+	    //throw new \Exception(print_r($args, true));
+	    
+	    
+	    $id=explode('{project:', $file);
+	    $id=array_pop($id);
+	    $id=explode('}',$id);
+	    $id=array_shift($id);
+	    
+	    // if(intval($id)<=0){
+	    //     $id=30;
+	    // }
+	    
+	    
+	   
+	    $gather=GetPlugin('ReferralManagement');
+	    $data=$gather->getProjectData($id);
 
-return $file;
+
+	    
+	    if(isset($data['metadata']->file->file)){
+
+	    	$paths=$gather->getParameters('datawarehousePaths', array());
+
+	    	foreach($paths as $dir){
+	    		$dir=rtim(trim($dir),'/');
+	    		if((!empty($dir))&&is_dir($dir)){
+	    			$realpath=realpath($dir.'/'.$data['metadata']->file->file);
+			        if(file_exists($realpath)){
+			            return $realpath;
+			        }
+	    		}
+	    	}
+
+	        
+	        throw new \Exception('file does not exist: '+$realpath);
+	    }
+	    
+	    return $data['metadata']->file;
+	    
+	}
+
+	return $file;
