f0d4a928143932fd68f971062b633f075c194587
nickolanack <nickblackwell82@gmail.com>
2022-04-13 04:22:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/defaultNotificationDetail.content.1.getItemListScript.js
+++ app/defaultNotificationDetail.content.1.getItemListScript.js
@@ -2,7 +2,7 @@
 ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
     
     if(item.getMetadata().items){
-       callback(PostContent.resolveItems(item, item.getMetadata().items));
+       callback(NotificationContent.resolveItems(item, item.getMetadata().items));
        return;
     }
 
