e466aa894b29fdddbf4032be5662c60390645fbd
nickolanack <nickblackwell82@gmail.com>
2022-04-18 19:29:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/defaultPostDetail.content.4.namedViewScript.js
+++ app/defaultPostDetail.content.4.namedViewScript.js
@@ -1,12 +1,1 @@
-
-
-var type=(item.getType?item.getType():item.type).split('.').pop();
-var map={"proposal":"project"};
-if(map[type]){
-    type=map[type];
-}
-
-return 'single'+(type.capitalize())+'ListItemDetail';
-
-
-return namedView;
+return PostContent.viewForItem(item);
