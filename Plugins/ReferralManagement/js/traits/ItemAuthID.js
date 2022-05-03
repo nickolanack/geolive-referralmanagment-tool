8b9c7c45e5a64ea2c478383ffc8c6446bf17e6d5
Nick Blackwell <nickblackwell82@gmail.com>
2022-05-03 11:12:00 -0700

    commit class

---
+++ Plugins/ReferralManagement/js/traits/ItemAuthID.js
@@ -0,0 +1,35 @@
+var ItemAuthID=(function(){
+
+
+
+	var ItemAuthID=new Class({
+
+
+
+		getAuthID: function() {
+			var me = this;
+			var permits= me.data.attributes.permitNumber;
+			
+			if(typeof permits=='string'){
+				if(permits!=''&&permits[0]=='['){
+					permits=JSON.parse(permits)
+				}
+			}
+
+			if(typeof permits=='string'){
+				return permits;
+			}
+
+			if(isArray_(permits)){
+				return permits[0];
+			}
+		}
+	
+
+
+	});
+
+	return ItemAuthID;
+
+
+})():
