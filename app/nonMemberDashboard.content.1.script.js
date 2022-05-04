6de08f7111d110eb9b98822100ee77b85bc3af30
nickolanack <nickblackwell82@gmail.com>
2022-05-04 19:03:12 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/nonMemberDashboard.content.1.script.js
+++ app/nonMemberDashboard.content.1.script.js
@@ -6,11 +6,3 @@
 					}
 				})).show();
 				
-return new ModalFormButtonModule(application, AppClient, {
-			    label: "Edit Profile",
-			    formName: "userProfileForm",
-			    "class": "primary-btn edit",
-			    formOptions: {
-						template: "form"
-				},
-			});	
