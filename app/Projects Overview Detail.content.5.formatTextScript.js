8a1bcf5bc3ef3cf7aad5186fdf8897fc23c47e50
nickolanack <nickblackwell82@gmail.com>
2022-05-04 22:59:23 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Projects Overview Detail.content.5.formatTextScript.js
@@ -0,0 +1,12 @@
+var btn=ConfigItem.CreateEditBtn(new ConfigItem({
+    "editLabel":"Edit Empty Projects",
+    'widget':options.widget,
+    'form':'textFieldForm',
+    'stepOptions':{
+        width:600
+    }
+}));
+
+if(btn){
+    module.getElement().appendChild(btn);
+}
