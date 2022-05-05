f838677856d5d07b9474c9985c3d7732557dc176
Nick Blackwell <nickblackwell82@gmail.com>
2022-05-05 13:01:49 -0700

    remove deprecated method

--- Plugins/ReferralManagement/js/GatherDashboard.js
+++ Plugins/ReferralManagement/js/GatherDashboard.js
@@ -122,40 +122,6 @@ var GatherDashboard = (function() {
 
 		},
 
-		/**
-		 * @deprecated move js/menus/Counters.js
-		 */
-		addItemSpatialInfo: function(el, item, application) {
-
-			var fileCounter = null;
-
-			var addEl = function() {
-				fileCounter = el.appendChild(new Element('span'));
-				fileCounter.addClass('items');
-				el.addClass('withItemsIndicator');
-			}
-
-
-			var updateCounter = function() {
-
-				if (!fileCounter) {
-					addEl();
-				}
-
-				fileCounter.setAttribute('data-items', item.getSpatialDocuments().length);
-				if (item.getSpatialDocuments().length > 0) {
-					el.addClass("hasItems");
-					return;
-				}
-				el.removeClass("hasItems");
-			}
-
-
-			updateCounter();
-
-		},
-
-	
 
 
 		loadUserDashboardView: function(application) {
