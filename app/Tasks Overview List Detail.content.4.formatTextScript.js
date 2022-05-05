16a0c706bfb34af3df980142102fef20e2a179a8
nickolanack <nickblackwell82@gmail.com>
2022-05-05 02:38:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Tasks Overview List Detail.content.4.formatTextScript.js
+++ app/Tasks Overview List Detail.content.4.formatTextScript.js
@@ -10,3 +10,13 @@ var btn=ConfigItem.CreateEditBtn(new ConfigItem({
 if(btn){
     module.getElement().appendChild(btn);
 }
+
+ ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
+     
+    if(team.getProjects().filter(function(p){
+        return p.getTasks().length>0;
+    }).length>0){
+        module.getElement().addClass('hidden');
+    }
+     
+ });
