b9fc0a86570feb59e18302dd43e16b3e7befe0cf
nickolanack <nickblackwell82@gmail.com>
2022-05-06 19:32:24 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.content.1.onRenderTextFieldScript.js
+++ app/widget.ProposalWizardBuilder.65.1.content.1.onRenderTextFieldScript.js
@@ -1,5 +1,5 @@
 try{
     
-    module.setInfoTip('some tip')
+    module.setInfoTip('The Referral ID number should be listed first. Additional ID numbers can be added for reference.')
     
 }catch(e){console.error(e);}
