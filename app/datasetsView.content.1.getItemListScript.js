5c29fdd50e4235c82d17eac38c228a9b3d53ae97
nickolanack <nickblackwell82@gmail.com>
2022-05-06 05:49:02 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetsView.content.1.getItemListScript.js
+++ app/datasetsView.content.1.getItemListScript.js
@@ -7,50 +7,8 @@ if(item instanceof ProjectList){
 }
 
 
-return ProjectTagList.getProjectTagsData('_root');
+return ProjectTagList.getProjectTagsData('_root').concat(new MockDataTypeItem({
+    name:'Community Vault'
+}));
 
 
-/*
-var MockEventDataTypeItem=new Class({
-				Extends:MockDataTypeItem,
-				Implements:[Events]
-
-		});
-
-return [
-    
-            
-    
-                new MockEventDataTypeItem({
-					name:"Water",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-				
-				new MockEventDataTypeItem({
-					name:"Community",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-				
-				new MockEventDataTypeItem({
-					name:"Health",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-				
-				new MockEventDataTypeItem({
-					name:"Forestry",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-				
-				new MockEventDataTypeItem({
-					name:"Resources",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-				
-				new MockEventDataTypeItem({
-					name:"Satellite",
-					description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-				}),
-    
-    
-    ]
-    */
