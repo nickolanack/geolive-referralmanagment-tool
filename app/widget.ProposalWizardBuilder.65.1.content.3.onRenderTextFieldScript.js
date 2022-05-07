3ae90a1e6ff5c143867076e56ef6094703fa4721
nickolanack <nickblackwell82@gmail.com>
2022-05-07 15:54:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.content.3.onRenderTextFieldScript.js
+++ app/widget.ProposalWizardBuilder.65.1.content.3.onRenderTextFieldScript.js
@@ -1,5 +1,5 @@
-//if(window.GuestProposal&&item instanceof GuestProposal){
+if(window.GuestProposal&&item instanceof GuestProposal){
     module.setMandatory(true, function(object){
         return !!object.value;
     });
-//}   
+}   
