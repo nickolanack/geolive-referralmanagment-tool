c768ba1114bead25699c250c17aa295b876d8052
nickolanack <nickblackwell82@gmail.com>
2022-05-10 22:13:14 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.14.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.14.formatElementScript.js
@@ -2,9 +2,9 @@ el.addClass("inline security");
 el.setAttribute("data-col","security");
 
 
-var team=item.getUsers();
-if(team.length){
-    valueEl.appendChild(new Element('span', {'html':team.length, 'class':'team-members'}));
+var users=item.getUsers();
+if(users.length){
+    valueEl.appendChild(new Element('span', {'html':users.length, 'class':'team-members'}));
 }
 
 console.log('security list');
