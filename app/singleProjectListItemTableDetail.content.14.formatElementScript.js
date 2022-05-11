ccee8039022d7f621d3f7c2e4f6b930f07e8a540
nickolanack <nickblackwell82@gmail.com>
2022-05-11 02:16:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.14.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.14.formatElementScript.js
@@ -3,23 +3,41 @@ el.setAttribute("data-col","security");
 
 
 var users=item.getUsers();
-if(users.length){
+    
+var description=[];
+
+
+if(users.length>0){
     valueEl.appendChild(new Element('span', {'html':users.length, 'class':'team-members'}));
+    description.push(users.length==1?'There is 1 team member':'There are '+users.length+' team members');
 }
 
 console.log('security list');
 
-var managers=valueEl.appendChild(new Element('span', {'html':'', 'class':'managers'}));
-var team=ProjectTeam.CurrentTeam();
-    
-managers.innerHTML=team.getUsers().filter(function(u){
-    return u.isTeamManager()&&users.map(function(u){return u.getId()}).indexOf(u.getId())==-1;
-}).length;
 
+
+var team=ProjectTeam.CurrentTeam();
+var viewers=team.getUsers().filter(function(u){
+    return u.isTeamManager()&&users.map(function(u){return u.getId()}).indexOf(u.getId())==-1;
+});
+
+if(viewers.length>0)
+    valueEl.appendChild(new Element('span', {'html':viewers.length, 'class':'managers'}));
+    description.push(links.length==1?'There is 1 manager':'There are '+links.length+' managers');
+}
 
 var links=item.getShareLinks();
-if(links.length){
+if(links.length>0){
     valueEl.appendChild(new Element('span', {'html':links.length, 'class':'share-links'}));
+    description.push(links.length==1?'There is 1 share link':'There are '+links.length+' share links');
+}
+
+
+if(users.length>0||viewers.length>0||links.length>0){
+     new UIPopover(el, {
+        description:'<h2>Item Access:</h2>'+description.join('<br/>'),
+        anchor:UIPopover.AnchorAuto()
+    });
 }
 
     
