18e8e52af4b66875fc8d8eb3e81dda76241a7ad8
nickolanack <nickblackwell82@gmail.com>
2022-05-11 21:10:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Main Dashboard Detail.content.6.resolveItemScript.js
+++ app/Main Dashboard Detail.content.6.resolveItemScript.js
@@ -4,26 +4,17 @@ return new ProjectList({
 	                "tags":[
 	                new MockDataTypeItem({
                         name:"Community Vault",
-                        description:"These datasets and collections are only visible to your community members. Nobody else has access to them.
-
-
-",
+                        description:"These datasets and collections are only visible to your community members. Nobody else has access to them.",
                         icon:null
                     }),
                     new MockDataTypeItem({
                         name:"Shared",
-                        description:"These are datasets and collections that your community is sharing with other communities and GCT3. This information will be visible and downloadable to members of these other communities.
-
-",
+                        description:"These are datasets and collections that your community is sharing with other communities and GCT3. This information will be visible and downloadable to members of these other communities.",
                         icon:null
                     }),
                     new MockDataTypeItem({
                         name:"Sharing",
-                        description:"These are datasets and collections that have been shared by other communities and GCT3 with your community. You will be able to view and download these files.
-
-
- 13m 37s
-",
+                        description:"These are datasets and collections that have been shared by other communities and GCT3 with your community. You will be able to view and download these files.",
                         icon:null
                     })
 	                
