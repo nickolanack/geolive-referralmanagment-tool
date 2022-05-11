27f1f4f2fb89eb071d303805a24422c16fda37d9
nickolanack <nickblackwell82@gmail.com>
2022-05-11 00:02:12 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.13.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.13.formatElementScript.js
@@ -1,6 +1,10 @@
 el.addClass("inline");
 el.setAttribute("data-col","status");
 
-
+el.addEvent('click', function(e){
+   e.stop();
+   UIInteraction.navigateToProjectSection(item ,"Security");
+    
+});
 
 ItemStatus.AddTags(item, el, valueEl)
