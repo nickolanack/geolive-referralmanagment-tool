fd0d768e05a057c28c9ad39c84dfad9e8149dfed
Nick Blackwell <nickblackwell82@gmail.com>
2022-05-10 18:58:48 -0700

    typo, missing 't'

--- Plugins/ReferralManagement/js/traits/ItemShareLinks.js
+++ Plugins/ReferralManagement/js/traits/ItemShareLinks.js
@@ -3,7 +3,7 @@ var ItemShareLinks=(function(){
 	var ItemShareLinks=new Class({
 		getShareLinks: function() {
 			if (this.data.links&&isArray_(this.data.links)){
-				return his.data.links;
+				return this.data.links;
 			}
 			return [];
 		}
