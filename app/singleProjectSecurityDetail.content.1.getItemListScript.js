c30ef1b6ed5608a2d480d36db0084161f187a78a
nickolanack <nickblackwell82@gmail.com>
2022-05-11 04:11:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectSecurityDetail.content.1.getItemListScript.js
+++ app/singleProjectSecurityDetail.content.1.getItemListScript.js
@@ -22,7 +22,7 @@
     		       label:"Share Links"
     		   }));
     		   
-    		   list=list.concat(item.getShareLinks());
+    		   list=list.concat(item.getShareLinks().map(function(link){return new ShareLinkItem(link); }));
     		   
 		   }
 		   
