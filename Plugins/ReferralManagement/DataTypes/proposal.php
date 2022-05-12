328dde120880758ae4cca4724b4716485228322c
Nick Blackwell <nickblackwell82@gmail.com>
2022-05-11 22:42:40 -0700

    add auth write

--- Plugins/ReferralManagement/DataTypes/proposal.php
+++ Plugins/ReferralManagement/DataTypes/proposal.php
@@ -8,18 +8,57 @@ class ProposalDataType extends \core\extensions\plugin\PluginDataType {
     );
 
     protected $onlyAuthClient=false;
-
     protected static $filter;
+
+    private static $Auth=array();
 
     /**
      * @SuppressWarnings("unused")
      */
     public function authorize($task, $item, $userId=-1) {
 
+        if($userId==-1){
+            $userId=GetClient()->getUserId();
+        }
 
-        // if (GetClient()->isAdmin()){
-        //     return true;
-        // }
+
+        $item=$this->resolveItem($item);
+        $cacheKey=$task.'.'.$item->id.'.'.$userId;
+        if(isset(self::$Auth[$cacheKey)){
+            return self::$Auth[$cacheKey);
+        }
+       
+
+        $auth=$this->isVisible($item, $userId);
+        
+        if($task=='write'&&$auth){
+            $auth=GetClient()->isUserAdmin($userId)||intval($item->user)==$userId;
+            if(!$auth){
+               array_walk($item->teamMembers, function($teamMember)use(&$auth, $userId){
+
+                    if(intval($teamMember->id)==$userId){
+                        $auth=true;
+                    }
+
+               });
+            }
+        }        
+
+
+        self::$Auth[$cacheKey]=$auth;
+        return $auth;
+    }
+
+    protected function isVisible($item, $userId){
+
+         $filter=$this->getFilter();
+         return $filter($item, $userId);
+
+    }
+
+    protected function resolveItem($item){
+
+
         if(is_string($item)){
             $item=intval($item);
         }
@@ -30,12 +69,9 @@ class ProposalDataType extends \core\extensions\plugin\PluginDataType {
         if(!is_object($item)){
             throw new \Exception('Expected array item metadata: '.gettype($item));
         }
-
-        $filter=$this->getFilter();
-        return $filter($item, $userId);
+        return $item;
 
     }
-
 
 
     protected function getFilter(){
