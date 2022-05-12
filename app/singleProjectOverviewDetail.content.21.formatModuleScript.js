d4495cc1a990405de6b226460eaa40692a02acd7
nickolanack <nickblackwell82@gmail.com>
2022-05-12 23:21:53 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.21.formatModuleScript.js
+++ app/singleProjectOverviewDetail.content.21.formatModuleScript.js
@@ -1,15 +1,31 @@
-setTimeout(function(){
-    module.getElement().addClass('has-hidden-content hide');
-    var button=module.getElement().appendChild(new Element('button', {
-            html:"Add notes", 
-            "class":"notes form-btn primary-btn edit", 
-            events:{click:function(){
-                module.getElement().removeClass('hide');
-                //button.parentNode.removeChild(button);
-                button.addClass('hide');
-            }}
-        
-        }));
-    module.setNamedValue('addNotesButton', button);
-    console.error('debug this')
-}, 500);
+AppClient.authorize('write', {
+				id: item.getId(),
+				type: item.getType()
+			}, function(access) {
+				//check access, bool.
+				if (access) {
+					setTimeout(function(){
+                        module.getElement().addClass('has-hidden-content hide');
+                        var button=module.getElement().appendChild(new Element('button', {
+                                html:"Add notes", 
+                                "class":"notes form-btn primary-btn edit", 
+                                events:{click:function(){
+                                    module.getElement().removeClass('hide');
+                                    //button.parentNode.removeChild(button);
+                                    button.addClass('hide');
+                                }}
+                            
+                            }));
+                        module.setNamedValue('addNotesButton', button);
+                        console.error('debug this')
+                    }, 500);
+				} else {
+				    
+				}
+			});
+
+
+
+
+
+
