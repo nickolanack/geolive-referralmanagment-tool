511a6ac24778ba8d16a1be4e44c9933c400259c3
nickolanack <nickblackwell82@gmail.com>
2022-05-17 05:19:56 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/communityUsersDetail.content.2.formatListItemModuleScript.js
+++ app/communityUsersDetail.content.2.formatListItemModuleScript.js
@@ -1,4 +1,4 @@
 
-   ProjectTeam.AddListItemEvents(child, childView, function(c){return c.isCommunityMember()||c.isUnassigned();});
-   UIInteraction.addUserProfileClick(childView.getElement(), child);
+  ProjectTeam.AddListItemEvents(child, childView, function(c){return c.isCommunityMember()||c.isUnassigned();});
+  UIInteraction.addUserProfileClick(childView.getElement(), child);
 
