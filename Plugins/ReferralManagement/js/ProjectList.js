e256c18d162e36dd558ca3879adb231fca42c8f7
Nick Blackwell <nickblackwell82@gmail.com>
2022-05-17 20:46:09 -0700

    simplify for each list

--- Plugins/ReferralManagement/js/ProjectList.js
+++ Plugins/ReferralManagement/js/ProjectList.js
@@ -907,43 +907,55 @@ var ProjectList = (function() {
 
 	ProjectList.ResolveSharedLists=function(){
 
-		return [
-            new MockDataTypeItem({
-                name:"Community Vault",
-                description:"These datasets and collections are only visible to your community members. Nobody else has access to them.",
+
+		return ([{
+			name:"Community Vault",
+            description:"These datasets and collections are only visible to your community members. Nobody else has access to them.",
+            filter:function(p){
+            	return p.getProjectCommunity()==ProjectTeam.CurrentTeam().getUser(AppClient.getId()).getCommunity();
+            }
+		},{
+			name:"Shared",
+            description:"These are datasets and collections that your community is sharing with other communities and GCT3. This information will be visible and downloadable to members of these other communities.",
+            filter:function(p){
+            	return p.getProjectCommunity()==ProjectTeam.CurrentTeam().getUser(AppClient.getId()).getCommunity();
+            }
+
+		},{
+			name:"Sharing",
+            description:"These are datasets and collections that have been shared by other communities and GCT3 with your community. You will be able to view and download these files.",    
+            filter:function(p){
+            	return p.getProjectCommunity()!=ProjectTeam.CurrentTeam().getUser(AppClient.getId()).getCommunity();
+            }
+
+		}]).map(function(list){
+
+			return  new MockDataTypeItem({
+                name:list.name,
+                description:list.description,
                 icon:null,
                 navigationFn:function(){
                     
                     var controller=GatherDashboard.getApplication().getNamedValue('navigationController')
                     controller.navigateTo("Datasets", "Main", {
         				item: new ProjectList({
-                		    "label": "Community Vault",
-                		    "description":"These datasets and collections are only visible to your community members. Nobody else has access to them.",
+                		    "label": list.name,
+                		    "description":list.description,
                 		    projects:function(callback){
                 
                 		    	ProjectTeam.CurrentTeam().runOnceOnLoad(function(team){
-                     				callback(team.getProjects().filter(function(p){
-                     					return true;
-                     				}));
+                     				callback(team.getProjects().filter(list.filter));
                      			});
                             }
                 		})
         			});
                     
                 }
-            }),
-            new MockDataTypeItem({
-                name:"Shared",
-                description:"These are datasets and collections that your community is sharing with other communities and GCT3. This information will be visible and downloadable to members of these other communities.",
-                icon:null
-            }),
-            new MockDataTypeItem({
-                name:"Sharing",
-                description:"These are datasets and collections that have been shared by other communities and GCT3 with your community. You will be able to view and download these files.",
-                icon:null
             })
-            
-            ];
+
+		});
+
+
 
 
 	}
