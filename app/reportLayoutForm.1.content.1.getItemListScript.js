c1a4326393fb8e3a8f47dcbd7c6ba1893c910793
nickolanack <nickblackwell82@gmail.com>
2022-05-20 19:38:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/reportLayoutForm.1.content.1.getItemListScript.js
+++ app/reportLayoutForm.1.content.1.getItemListScript.js
@@ -1,5 +1,18 @@
-return [new MockDataTypeItem({
-    name:"Name",
-    description:"asdf asdf",
-    content:""
-})]
+
+
+
+
+
+(new AjaxControlQuery(CoreAjaxUrlRoot, "get_configuration_field", {
+		'widget': "reportTemplates",
+		'field': "templatesData"
+	})).addEvent('success',function(response){
+            callback(response.value.map(function(item){
+                return new MockDataTypeItem({
+                    name:item.name,
+                    description:item.description,
+                    content:""
+                });
+                
+            }))
+	}).execute();
