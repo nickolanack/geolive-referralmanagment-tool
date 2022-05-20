966b7a8c73992998f5a14a707987ce98eef6b395
nickolanack <nickblackwell82@gmail.com>
2022-05-20 18:34:23 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectEditButtonsDetail.content.3.script.js
+++ app/singleProjectEditButtonsDetail.content.3.script.js
@@ -1,3 +1,5 @@
+//report btn
+
 if(item.isArchived()){
     return null;
 }
@@ -6,13 +8,15 @@ if(!DashboardConfig.getValue("showProjecReports")){
     return null;
 }
 
-return new ElementModule('button',{"identifier":"button-report", "html":"Create report", "class":"primary-btn report", "events":{"click":function(){
+return [
+    new ElementModule('button',{"identifier":"button-report", "html":"Create report", "class":"primary-btn report", "events":{"click":function(){
     
-    var exportQuery=new AjaxControlQuery(CoreAjaxUrlRoot, 'generate_report', {
-		                "plugin": "ReferralManagement",
-		                "proposal":item.getId()
-		                });
-    				//exportQuery.execute(); //for testing.
-    				window.open(exportQuery.getUrl(true),'Download'); 
-
-}}})
+        var exportQuery=new AjaxControlQuery(CoreAjaxUrlRoot, 'generate_report', {
+    		                "plugin": "ReferralManagement",
+    		                "proposal":item.getId()
+    		                });
+        				//exportQuery.execute(); //for testing.
+        				window.open(exportQuery.getUrl(true),'Download'); 
+    
+    }}})
+];
