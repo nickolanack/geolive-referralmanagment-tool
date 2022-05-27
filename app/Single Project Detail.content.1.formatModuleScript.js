ca7f160ce9c65385c26897f98b4a6facedda8a57
nickolanack <nickblackwell82@gmail.com>
2022-05-27 06:54:51 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Single Project Detail.content.1.formatModuleScript.js
+++ app/Single Project Detail.content.1.formatModuleScript.js
@@ -2,8 +2,9 @@
 module.addWeakEvent(item, "change",function(){
     if(module.options.namedView==='singleProjectOverviewDetail'){
         module.redraw(); //automatically stops spinning
+    }else{
+        module.stopSpinner();
     }
-        
 })
 
 module.addWeakEvent(item, "saving",function(){
