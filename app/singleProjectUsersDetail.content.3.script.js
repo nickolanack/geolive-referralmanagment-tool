fb3ca2487178cde2e5c4fc2d6819b3e7a1ab4820
nickolanack <nickblackwell82@gmail.com>
2022-05-27 19:02:44 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectUsersDetail.content.3.script.js
+++ app/singleProjectUsersDetail.content.3.script.js
@@ -1,1 +1,9 @@
-return '<div class="section-title"><span>Communities:</span></div>';
+return (new ModalFormButtonModule(application, item,{
+        label:"Add Team Member",
+        formName:"userSelectionForm",
+        formOptions:{
+            template:"form"
+        },
+        //hideText:true,
+        "class":"primary-btn"
+    }))
