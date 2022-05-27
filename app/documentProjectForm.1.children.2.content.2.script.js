0b1b28976efd4b2ee66602a5d12633734e26eb7c
nickolanack <nickblackwell82@gmail.com>
2022-05-27 05:34:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/documentProjectForm.1.children.2.content.2.script.js
+++ app/documentProjectForm.1.children.2.content.2.script.js
@@ -3,7 +3,7 @@ return new Element('button',{html:"Add dataset(s)", "class":"primary-btn", event
     
     
     
-    (new UIModalDialog(application, item,{
+    (new UIModalDialog(application, new SelectionProxy(item), {
     
         formName:"datasetSelectForm",
         formOptions:{
