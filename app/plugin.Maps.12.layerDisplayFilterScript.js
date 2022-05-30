213f72ed59a4e82f8889582a11e3f1a1513573bf
nickolanack <nickblackwell82@gmail.com>
2022-05-30 05:03:07 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.12.layerDisplayFilterScript.js
+++ app/plugin.Maps.12.layerDisplayFilterScript.js
@@ -1,5 +1,14 @@
 $parseSettings=$layer->getParseSettings();
 if(key_exists('filter', $parseSettings)&&$parseSettings->filter=='user'){
     // error_log(json_encode($feature));
-    return $feature["uid"]==GetClient()->getUserId();
+   if($feature["uid"]==GetClient()->getUserId()){
+       return true;
+   }
+   
+   if(isset($options->map)&&strpos($feature['name'], $options->map)===0){
+      
+       return isset($feature['info']->shared)&&$feature['info']->shared;
+   }
+   
+   return false;
 }
