37fb2a91d28c81833ba10a44f81d8b9dab33c98e
nickolanack <nickblackwell82@gmail.com>
2022-05-30 20:21:00 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Dashboard.onRenderApplictionScript.js
+++ app/Dashboard.onRenderApplictionScript.js
@@ -122,6 +122,7 @@ IncludeJSBlock('
     
     GetWidget('siteSpecific')->display($targetInstance);   
     
+    
 GetWidget('mainNotificationsDetail')->display($targetInstance); //proposal wizard from map
     
 GetWidget('defaultPostDetail')->display($targetInstance);
