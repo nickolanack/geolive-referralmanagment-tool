6612490b493e5c742f699e8022ae8ae310926523
nickolanack <nickblackwell82@gmail.com>
2022-06-03 03:24:41 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Single Task List Item Detail.content.4.script.js
+++ app/Single Task List Item Detail.content.4.script.js
@@ -1,2 +1,7 @@
 //tags
-return ItemNavigationTagLinks.CreateNavigationTagListModule(item);
+var mod= ItemNavigationTagLinks.CreateNavigationTagListModule(item);
+
+mod.setIdentifier('col-tags');
+mod.getElement().setAttribute('data-col','tags');
+
+return mod;
