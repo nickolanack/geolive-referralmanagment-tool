52482a80a750d1953091ac7da5bb7470b6b4cbab
nickolanack <nickblackwell82@gmail.com>
2022-06-03 03:13:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Single Task List Item Detail.content.1.script.js
+++ app/Single Task List Item Detail.content.1.script.js
@@ -1,5 +1,48 @@
 //complete
 
+
+
+
+var element;
+var mod=new ElementModule('div',{
+    identifier:"col-complete"
+    "class":item.isComplete()?"complete":"not-complete",
+   
+})
+var el=element.appendChild(new Element('div', {"class":"completion-indicator", 
+    events:{click:function(e){
+        e.stop();
+        
+   
+       item.setComplete(!item.isComplete());
+       item.save();
+       
+       if(item.isComplete()){
+           element.removeClass('not-complete');
+           element.addClass('complete');
+           pop.setDescription('click to mark incomplete');
+       }else{
+           element.addClass('not-complete');
+           element.removeClass('complete');
+           pop.setDescription('click to mark complete');
+       }
+       
+ 
+        
+        
+        
+    }}}));
+element=mod.getElement();
+
+pop=new UIPopover(el, {
+        description:item.isComplete()?'click to mark incomplete':'click to mark complete',
+        anchor:UIPopover.AnchorAuto()
+    });
+
+return mod;
+
+/*
+
 return function(viewer, element, parentModule){
     
     
@@ -41,3 +84,5 @@ return function(viewer, element, parentModule){
     
     
 }
+
+*/
