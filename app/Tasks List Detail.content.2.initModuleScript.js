f339b47b6b71fd56a0351fc3fae8bd972a58c542
nickolanack <nickblackwell82@gmail.com>
2022-06-03 02:37:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Tasks List Detail.content.2.initModuleScript.js
+++ app/Tasks List Detail.content.2.initModuleScript.js
@@ -1,1 +1,16 @@
-(new TableHeader('tasktTableLayout', {})).render(listModule);
+(new TableHeader('tasktTableLayout', {
+        "assigned": {
+            "width": "30px"
+        },
+        "name": {
+            "width": "auto",
+            "minWidth": "250px",
+            "label": "Name"
+        },
+        "comments": {
+            "width": "45px",
+            "tip": "",
+            "label": ""
+        }
+    
+})).render(listModule);
