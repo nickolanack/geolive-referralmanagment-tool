5a9b2c1917977d534cb971043bd632d237976a2c
Nick Blackwell <nickblackwell82@gmail.com>
2022-06-07 15:51:54 -0700

    add icon and wording

--- Plugins/ReferralManagement/js/ProjectList.js
+++ Plugins/ReferralManagement/js/ProjectList.js
@@ -1006,12 +1006,10 @@ var ProjectList = (function() {
 
 	}
 
-
-	ProjectList.ResolveSharedLists = function() {
-
-
-		return ([{
+	ProjectList.SharedListFilters = function() {
+		return [{
 			name: "Community Vault",
+			icon:"https://dyl2vw577xcfk.cloudfront.net/gct3.gather.geoforms.ca/1/Uploads/%5BG%5D_yqF_6QX_TVS_%5BImAgE%5D-%3Ethumb%3A%3E200x%3E150.png",
 			description: "These datasets and collections are only visible to your community members. Nobody else has access to them.",
 			filter: function(p) {
 				return p.getProjectCommunity() == ProjectTeam.CurrentTeam().getUser(AppClient.getId()).getCommunity() && p.getCommunitiesInvolved().filter(function(c) {
@@ -1019,7 +1017,8 @@ var ProjectList = (function() {
 				}).length == 0;
 			}
 		}, {
-			name: "Shared",
+			name: "You have Shared",
+			icon:"https://dyl2vw577xcfk.cloudfront.net/gct3.gather.geoforms.ca/1/Uploads/UJ8_%5BG%5D_Je9_%5BImAgE%5D_Ngx-%3Ethumb%3A%3E200x%3E150.png",
 			description: "These are datasets and collections that your community is sharing with other communities and GCT3. This information will be visible and downloadable to members of these other communities.",
 			filter: function(p) {
 
@@ -1029,18 +1028,24 @@ var ProjectList = (function() {
 			}
 
 		}, {
-			name: "Sharing",
+			name: "Others are Sharing",
+			icon:"https://dyl2vw577xcfk.cloudfront.net/gct3.gather.geoforms.ca/1/Uploads/%5BImAgE%5D_mib_%5BG%5D_zR_yF4-%3Ethumb%3A%3E200x%3E150.png",
 			description: "These are datasets and collections that have been shared by other communities and GCT3 with your community. You will be able to view and download these files.",
 			filter: function(p) {
 				return p.getProjectCommunity() != ProjectTeam.CurrentTeam().getUser(AppClient.getId()).getCommunity();
 			}
 
-		}]).map(function(list) {
+		}]
+	}
+	ProjectList.ResolveSharedLists = function() {
+
+
+		return (ProjectList.SharedListFilters()).map(function(list) {
 
 			return new MockDataTypeItem({
 				name: list.name,
 				description: list.description,
-				icon: null,
+				icon: list.icon||null,
 				navigationFn: function() {
 
 					var controller = GatherDashboard.getApplication().getNamedValue('navigationController')
