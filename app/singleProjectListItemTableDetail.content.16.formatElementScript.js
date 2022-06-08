8c635499428c0ef8283448416d3f38840b220857
nickolanack <nickblackwell82@gmail.com>
2022-06-08 14:53:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.16.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.16.formatElementScript.js
@@ -1,11 +1,17 @@
 el.addClass("inline sharing");
 el.setAttribute("data-col","sharing");
 
-
+var sharedLabel='';
 
 ProjectList.SharedListFilters().forEach(function(filter){
     if(filter.filterFn(item)){
         el.addClass(filter.name);
         valueEl.setStyle('background-image', 'url('+filter.icon+')');
+        sharedLabel=filter.tip||filter.label;
     }
 })
+
+ new UIPopover(valueEl, {
+        description:sharedLabel,
+        anchor:UIPopover.AnchorAuto()
+    });
