a7e53c0891723442ccc2d57aba955c2c04544097
nickolanack <nickblackwell82@gmail.com>
2022-06-08 18:00:05 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.16.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.16.formatElementScript.js
@@ -14,7 +14,7 @@ ProjectList.SharedListFilters().forEach(function(filter, i){
 })
 
     if(sharedIndex==2&&item.getProjectSubmitterId()+""==AppClient.getId()+""){
-        sharedLabel="You created this in another community. Managers of "+item.getProjectCommunity()+" can edit.";
+        sharedLabel="You created this in another community. Managers of <b>"+item.getProjectCommunity()+"</b> can edit.";
     }
 
 
