eedd945aa8550b7b412b0bf9bc54e301cc7fda66
Nick Blackwell <nickblackwell82@gmail.com>
2022-06-09 21:29:26 -0700

    print data token

--- Plugins/ReferralManagement/views/map.php
+++ Plugins/ReferralManagement/views/map.php
@@ -5,7 +5,14 @@ $segments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 	
 print_r($segments);
 
-// array_shift($segments);
-// array_shift($segments);
+array_shift($segments);
+array_shift($segments);
+
+
+$project=array_shift($segments);
+$token=array_shift($segments);
+
+
+print_r(GetPlugin('Links')->peekDataToken($token));
 
 
