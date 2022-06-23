56c1abc50d66c8b6c4efc95018a737e9c4c4e269
nickolanack <nickblackwell82@gmail.com>
2022-06-23 20:07:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/reportListItem.content.1.script.js
+++ app/reportListItem.content.1.script.js
@@ -10,5 +10,23 @@ var reportBtn=new ElementModule('button',{"identifier":"button-report", "html":"
     
     }}})
     
+if(AppClient.getUserType()=="admin"){    
+var adminButton=new ElementModule('button',{"identifier":"button-report", "html":"Preview", "class":"primary-btn report", "events":{"click":function(){
+    
+        var exportQuery=new AjaxControlQuery(CoreAjaxUrlRoot, 'generate_report', {
+    		                "plugin": "ReferralManagement",
+    		                "project":item.getProject(),
+    		                "template":item.getName(),
+    		                'format'=>'html'
+    		            });
+        				//exportQuery.execute(); //for testing.
+        				window.open(exportQuery.getUrl(true),'Download'); 
+    
+    }}});
+    
+    return [reportBtn, adminButon];
+    
+}
+    
     
 return reportBtn
