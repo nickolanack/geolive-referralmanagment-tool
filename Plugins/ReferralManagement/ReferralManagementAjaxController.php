da9015f57a451bba99edcd44ad40b527f9e1d067
Nick Blackwell <nickblackwell82@gmail.com>
2022-07-13 11:47:54 -0700

    add path

--- Plugins/ReferralManagement/ReferralManagementAjaxController.php
+++ Plugins/ReferralManagement/ReferralManagementAjaxController.php
@@ -24,7 +24,7 @@ class ReferralManagementAjaxController extends \core\AjaxController implements \
 	protected function listCalEvents(){
 
 
-		return array('data'=>json_decode(file_get_contents(__DIR__)));
+		return array('data'=>json_decode(file_get_contents(__DIR__.'/holidays.json')));
 
 
 	}
