4ac8f69d3feafeaa9f2aff6b2fca11f7608e619f
Nick Blackwell <nickblackwell82@gmail.com>
2022-07-28 15:18:51 -0700

    move to ICal class

--- Plugins/ReferralManagement/ReferralManagementAjaxController.php
+++ Plugins/ReferralManagement/ReferralManagementAjaxController.php
@@ -20,23 +20,8 @@ class ReferralManagementAjaxController extends \core\AjaxController implements \
 
 	protected function getICal(){
 
-		$vCalendar = new \Eluceo\iCal\Component\Calendar('www.example.com');
-		$vEvent = new \Eluceo\iCal\Component\Event();
-		$vEvent
-		    ->setDtStart(new \DateTime('2012-12-24'))
-		    ->setDtEnd(new \DateTime('2012-12-24'))
-		    ->setNoTime(true)
-		    ->setSummary('Christmas');
-
-		$vCalendar->addComponent($vEvent);
-
-		header('Content-Type: text/calendar; charset=utf-8');
-		header('Content-Disposition: attachment; filename="cal.ics"');
-
-		echo $vCalendar->render();
-
+		echo (new \ReferralManagement\Ical())->getCalendarForUser();
 		exit();
-
 
 	}
 
