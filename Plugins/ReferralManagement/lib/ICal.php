3aaf1ab1a2a88b8f4d939a32acdc14dd9013d476
Nick Blackwell <nickblackwell82@gmail.com>
2022-07-28 15:56:18 -0700

    get user items list

--- Plugins/ReferralManagement/lib/ICal.php
+++ Plugins/ReferralManagement/lib/ICal.php
@@ -6,6 +6,9 @@ namespace ReferralManagement;
 class ICal{
 
 
+	protected function getPlugin(){
+		return GetPlugin('ReferralManagement');
+	}
 
 	public function getCalendarForUser(){
 
@@ -14,6 +17,11 @@ class ICal{
 
 		$vCalendar = new \Eluceo\iCal\Component\Calendar('www.example.com');
 		$vEvent = new \Eluceo\iCal\Component\Event();
+
+
+		$list=$this->getPlugin()->getActiveProjectList(1);
+		error_log($list);
+
 		$vEvent
 		    ->setDtStart(new \DateTime('2012-12-24'))
 		    ->setDtEnd(new \DateTime('2012-12-24'))
