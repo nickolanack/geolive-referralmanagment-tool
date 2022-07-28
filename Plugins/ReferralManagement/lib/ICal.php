de6131c147248c8b630c22b0b5c50c2d599f3800
Nick Blackwell <nickblackwell82@gmail.com>
2022-07-28 16:00:23 -0700

    add event for each item

--- Plugins/ReferralManagement/lib/ICal.php
+++ Plugins/ReferralManagement/lib/ICal.php
@@ -16,19 +16,27 @@ class ICal{
 
 
 		$vCalendar = new \Eluceo\iCal\Component\Calendar('www.example.com');
-		$vEvent = new \Eluceo\iCal\Component\Event();
-
+		
 
 		$list=$this->getPlugin()->getActiveProjectList(1);
-		error_log(print_r($list, true));
 
-		$vEvent
-		    ->setDtStart(new \DateTime('2012-12-24'))
-		    ->setDtEnd(new \DateTime('2012-12-24'))
-		    ->setNoTime(true)
-		    ->setSummary('Christmas');
+		foreach ($list as $projectData) {
 
-		$vCalendar->addComponent($vEvent);
+			$vEvent = new \Eluceo\iCal\Component\Event();
+
+		
+			$vEvent
+			    ->setDtStart(new \DateTime('2012-12-24'))
+			    ->setDtEnd(new \DateTime('2012-12-24'))
+			    ->setNoTime(true)
+			    ->setSummary($projectData->attributes->title);
+
+			$vCalendar->addComponent($vEvent);
+
+		}
+
+
+		
 
 		header('Content-Type: text/calendar; charset=utf-8');
 		header('Content-Disposition: attachment; filename="cal.ics"');
