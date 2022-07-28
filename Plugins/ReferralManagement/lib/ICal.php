6c641e21583574735cddb5b88b1a02f8aea192e2
Nick Blackwell <nickblackwell82@gmail.com>
2022-07-28 15:20:52 -0700

    add file

---
+++ Plugins/ReferralManagement/lib/ICal.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+namespace ReferralManagement;
+
+class ICal{
+
+
+
+	public function getCalendarForUser(){
+
+
+
+
+		$vCalendar = new \Eluceo\iCal\Component\Calendar('www.example.com');
+		$vEvent = new \Eluceo\iCal\Component\Event();
+		$vEvent
+		    ->setDtStart(new \DateTime('2012-12-24'))
+		    ->setDtEnd(new \DateTime('2012-12-24'))
+		    ->setNoTime(true)
+		    ->setSummary('Christmas');
+
+		$vCalendar->addComponent($vEvent);
+
+		header('Content-Type: text/calendar; charset=utf-8');
+		header('Content-Disposition: attachment; filename="cal.ics"');
+
+		return $vCalendar->render();
+	}
+
+
+}
