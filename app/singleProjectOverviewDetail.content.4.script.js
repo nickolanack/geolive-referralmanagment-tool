a22b26f6d0bfe3e5602e67afc6375a3adc19aff4
nickolanack <nickblackwell82@gmail.com>
2022-07-29 19:10:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectOverviewDetail.content.4.script.js
+++ app/singleProjectOverviewDetail.content.4.script.js
@@ -1,8 +1,22 @@
-return new ElementModule('div', {
+var mod=new ElementModule('div', {
     "class":"toggle-status",
     events:{
         click:function(){
-            
+            var el=mod.getElement();
+            var p=el.parentNode;
+            if(p.hasClass('show-progress')){
+                p.removeClass('show-progress');
+                return;
+            }
+             p.addClass('show-progress');
         }
     }
+});
+
+mod.runOnceOnLoad(function(){
+    
+    //...init
+    
 })
+
+return mod;
