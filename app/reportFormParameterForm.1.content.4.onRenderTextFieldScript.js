bf0bdc1e49519e236fb771f8b5039ea2daa98e47
nickolanack <nickblackwell82@gmail.com>
2022-08-10 18:24:15 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/reportFormParameterForm.1.content.4.onRenderTextFieldScript.js
+++ app/reportFormParameterForm.1.content.4.onRenderTextFieldScript.js
@@ -2,7 +2,7 @@ var el=module.getElement();
 var p=el.parentNode;
 p.addClass('minimize');
 
-var toggle=el.appendChild(new Element('button', {"class":"section-toggle", events:{click:function(){
+var toggle=el.parentNode.appendChild(new Element('button', {"class":"section-toggle", events:{click:function(){
     if(p.hasClass('minimize')){
         p.removeClass('minimize');
         toggle.addClass('active');
