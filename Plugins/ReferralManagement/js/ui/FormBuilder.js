824f13b2ec445eb1b9aa582b6c41fcaa4a7581a7
Nick Blackwell <nickblackwell82@gmail.com>
2022-08-11 22:18:28 -0700

    move form field to class

--- Plugins/ReferralManagement/js/ui/FormBuilder.js
+++ Plugins/ReferralManagement/js/ui/FormBuilder.js
@@ -32,7 +32,77 @@ var FormBuilder = (function(){
 
 
 
+		getFormParameter:function(item){
 
+
+			var label = item.getName().replace(/([A-Z])/g, " $1");
+			label = label[0].toUpperCase() + label.slice(1);
+
+
+			if(item.getFieldType()=='heading'){
+			    
+			    return '<h2>'+item.getDefaultValue()+'</h2>';
+
+			}
+
+
+			if(item.getFieldType()=='boolean'){
+			    
+			    return new SimpleBooleanModule({
+			        label:label,
+			        type:"checkbox",
+			        value: (['true', true]).indexOf(item.getDefaultValue())>=0,
+			    }).addDataSubmitter(function(object, wizardDataSet){
+			        wizardDataSet[item.getName()]=object.value;
+			    });
+
+			    
+			}
+
+
+			if(item.getFieldType()=='options'){
+			    
+			    return new SelectableTagCloudModule({
+			        label:label,
+			        tags:item.getOptions().values,
+			        maxSelected:item.getOptions().maxSelected||-1
+			    }).addDataSubmitter(function(object, wizardDataSet){
+			        wizardDataSet[item.getName()]=object.value;
+			    });
+			    
+			    
+			}
+
+
+			if(item.getFieldType()=='script'){
+			    return eval("(function(){\n"+item.getDefaultValue()+"\n})();");
+			}
+
+
+
+			if(item.getFieldType()=='parameters'){
+			   
+			    wizard.appendData(JSON.parse(item.getDefaultValue()));
+			    return null;
+			}
+
+
+			if(item.getFieldType()=='text'){
+			    
+			    return new TextFieldModule({
+			       label:label,
+			       wrapElementClassName:"no-media",
+			       value:item.getDefaultValue()||"",
+			    }).addDataSubmitter(function(object, wizardDataSet){
+			        wizardDataSet[item.getName()]=object.value;
+			    });
+
+			    
+			}
+
+
+			return 'field placeholder'
+		}
 
 
 	});
