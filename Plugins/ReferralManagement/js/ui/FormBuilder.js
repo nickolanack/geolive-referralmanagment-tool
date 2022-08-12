834f84be2afe968136b4f86d645a6485ff00920f
Nick Blackwell <nickblackwell82@gmail.com>
2022-08-11 22:26:51 -0700

    add methods

--- Plugins/ReferralManagement/js/ui/FormBuilder.js
+++ Plugins/ReferralManagement/js/ui/FormBuilder.js
@@ -51,6 +51,22 @@ var FormBuilder = (function(){
 		},
 
 
+		formatOptionsField:function(textField, inputElement, module){
+			var el=module.getElement();
+			var p=el.parentNode;
+			p.addClass('minimize');
+
+			var toggle=el.parentNode.appendChild(new Element('button', {"class":"section-toggle", events:{click:function(){
+			    if(p.hasClass('minimize')){
+			        p.removeClass('minimize');
+			        toggle.addClass('active');
+			        return;
+			    }
+			    p.addClass('minimize');
+			    toggle.removeClass('active');
+			}}}));
+		},
+
 
 		getFormParameter:function(item){
 
