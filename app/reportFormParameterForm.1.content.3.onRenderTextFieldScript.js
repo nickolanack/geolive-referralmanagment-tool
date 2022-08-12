8f39cb38d2e829757361a96e58fa3ed23c6f9517
nickolanack <nickblackwell82@gmail.com>
2022-08-12 05:26:02 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/reportFormParameterForm.1.content.3.onRenderTextFieldScript.js
+++ app/reportFormParameterForm.1.content.3.onRenderTextFieldScript.js
@@ -1,18 +1,2 @@
-var updateField=function(){
-  var type=wizard.getData().fieldType;
-  if(type=='heading'){
-      textField.setLabel('Heading Text')
-  }
-  
-  if(type=='script'){
-      textField.setLabel('Script')
-  }
-  
-  if((['heading', 'script']).indexOf(type)>=0){
-      module.getElement().removeClass('width-2')
-  }
-  
-};
 
-wizard.on('valueChange', updateField);
-setTimeout(updateField, 250);
+FormBuilder.formatValueField(textField, inputElement, module);
