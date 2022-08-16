8e2572c40318b2e3a7b65ab6a93baa78aa0c9d63
nickolanack <nickblackwell82@gmail.com>
2022-08-16 18:37:23 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.18.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.18.formatElementScript.js
@@ -6,11 +6,20 @@ el.appendChild(new Element('div', {
     events:{
         click:function(event){
             event.stop();
-            if(el.hasClass('active')){
-                el.removeClass('active');
-                return;
+            
+            if(item.authorize('write')){
+               if(el.hasClass('active')){
+                    el.removeClass('active');
+                    return;
+                }
+                el.addClass('active'); 
             }
-            el.addClass('active');
         }
     }
 }));
+
+
+if(item.isPublic()){
+    el.addClass('active');
+}
+
