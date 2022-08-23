0b24ad77f534b8d1f3727dbfcbcc014ca058a854
Nick Blackwell <nickblackwell82@gmail.com>
2022-08-23 13:18:02 -0700

    use passed data

--- Plugins/ReferralManagement/js/spatial/SpatialProjectWorker.js
+++ Plugins/ReferralManagement/js/spatial/SpatialProjectWorker.js
@@ -4,7 +4,7 @@ onmessage = function(e) {
 
 
     if (!userFunction) {
-        userFunction = (new Function('return function(feature, type, options){ ' + "\n" + metadata.script + "\n" + '}')).call(null);
+        userFunction = (new Function('return function(feature, type, options){ ' + "\n" + e.data + "\n" + '}')).call(null);
         return;
     }
 
