90663ea0ba4bd7c03561ac0ac05012657f1948d2
nickolanack <nickblackwell82@gmail.com>
2022-09-01 22:41:16 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/install.onRenderApplictionScript.js
+++ app/install.onRenderApplictionScript.js
@@ -24,3 +24,25 @@ GetWidget('adminStyles')->display($targetInstance);
 GetWidget('darkTheme')->display($targetInstance);   
 
 
+IncludeCSSBlock('
+
+
+    .ui-view.dashboard-main.dark:before {
+        background-image: url("https://gather.geoforms.ca/assets/images/tim-johnson-bmljdpdeigw-unsplash.jpg");
+        content:
+        "";
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-size: cover;
+        opacity: 0.2;
+        z-index: 0;
+    }
+    
+    .ui-view.dashboard-main.dark {
+        z-index: 1;
+    }
+
+
+
+');
