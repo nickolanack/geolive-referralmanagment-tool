01e5afde2486e1238829650d0a3225572bc31bd9
nickolanack <nickblackwell82@gmail.com>
2022-09-01 20:21:06 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/install.onRenderApplictionScript.js
+++ app/install.onRenderApplictionScript.js
@@ -3,3 +3,5 @@ GetWidget('sharedGeneratedStyles')->display($targetInstance);
 
 GetWidget('createDashboardForm')->display($targetInstance);
 
+GetWidget('adminStyles')->display($targetInstance);   
+GetWidget('darkTheme')->display($targetInstance);   
