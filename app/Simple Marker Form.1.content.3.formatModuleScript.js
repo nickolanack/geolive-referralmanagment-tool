bc06f6deeee3349f18af9af7aba37bb213dbb14f
nickolanack <nickblackwell82@gmail.com>
2022-09-08 18:57:29 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Simple Marker Form.1.content.3.formatModuleScript.js
+++ app/Simple Marker Form.1.content.3.formatModuleScript.js
@@ -1,18 +1,2 @@
-module.getChildWizard(function(wizard) {
-	wizard.addEvent('valueChange', function() {
-	    wizard.update();
-	    var d= wizard.getData();
-	    var parentWizard=module.getWizard();
-	    var p=parentWizard.getData();
-	    console.log(d);
-	    
-	    var images = JSTextUtilities.ParseImages(d.description).map(function(o) {
-			return o.url;
-		});
-		
-		if(images.length>0){
-		    parentWizard.setDataValue('icon', images[0]);
-		}
-	    
-	});
-});
+
+ProjectMap.dropMarkerSubformHelper(module);
