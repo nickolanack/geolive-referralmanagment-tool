ac03f378f1fb0f187adba39edfc97dcd94caa672
nickolanack <nickblackwell82@gmail.com>
2022-09-09 06:14:41 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleUserListItemDetail.content.2.fieldValueScript.js
+++ app/singleUserListItemDetail.content.2.fieldValueScript.js
@@ -1,4 +1,14 @@
 if(!item.getEmail){
+    
+    try{
+        var user=ProjectTeam.CurrentTeam().getUser(item.getId());
+        if(user){
+            return user.getEmail();
+        }
+    }catch(e){
+        
+    }
+    
     return '{missing getEmail()}';
 }
 return item.getEmail()
