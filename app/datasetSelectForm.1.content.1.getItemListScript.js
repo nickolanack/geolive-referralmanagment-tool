17d7fe7768bbbf58eb7e482503a9928e3d686096
nickolanack <nickblackwell82@gmail.com>
2022-09-13 22:38:53 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/datasetSelectForm.1.content.1.getItemListScript.js
@@ -0,0 +1,1 @@
+ItemProjectsCollection.GetSelectableProjectsList(item, callback)
---
+++ app/datasetSelectForm.1.content.1.initModuleScript.js
@@ -0,0 +1,6 @@
+ProjectList.AddTableHeader(module)
+module.setPaginationOptions({
+    position:"auto",
+    showPages:true
+});
+new TableAutoHeightBehavior(module, {});
