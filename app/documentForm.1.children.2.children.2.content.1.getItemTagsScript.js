9ec83b57ae0e243ace05dd876cf629662c9cf870
nickolanack <nickblackwell82@gmail.com>
2022-09-13 20:44:03 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/documentForm.1.children.2.children.2.content.1.getItemTagsScript.js
+++ app/documentForm.1.children.2.children.2.content.1.getItemTagsScript.js
@@ -2,4 +2,6 @@ var cats= Proposal.ListTerritories();
 if(cats.length==0){
     cats.push("placeholder");
 }
-return cats.sort(function(a, b){ return a.localeCompare(b); })
+ cats.sort(function(a, b){ return a.localeCompare(b); })
+ 
+return cats;
