7662a124e219b846847906d58c2005305a3dc37d
nickolanack <nickblackwell82@gmail.com>
2022-09-16 05:17:53 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Dashboard.formatModulesScript.js
+++ app/Dashboard.formatModulesScript.js
@@ -1,6 +1,11 @@
 
 console.log('quick dark mode');
 
+DashboardConfig.getValues(['darkMode', 'pageClassNames'],function(values){
+    console.log(value)
+});
+
+
 list.content[0].options.className+="<?php 
 
 $dashConfig=GetWidget('dashboardConfig');
