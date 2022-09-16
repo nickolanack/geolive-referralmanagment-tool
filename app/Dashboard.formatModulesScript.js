fa959bf7572fa5bc456d38fc6a3246f1b3e8263e
nickolanack <nickblackwell82@gmail.com>
2022-09-16 05:24:19 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Dashboard.formatModulesScript.js
+++ app/Dashboard.formatModulesScript.js
@@ -4,7 +4,7 @@ console.log('quick dark mode');
 if(window.DashboardConfig){
     DashboardConfig.getValues(['darkMode', 'pageClassNames'],function(values){
         if(window.DisplayTheme){
-            DisplayTheme.SetDefaults(values, list.content[0]);
+            DisplayTheme.setDefaults(values, list.content[0]);
         }
     });
 }
