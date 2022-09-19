641b520b99be82fce514ddfcf496a20cc62ef1f3
Nick Blackwell <nickblackwell82@gmail.com>
2022-09-19 15:34:40 -0700

    hide/show communities

--- Plugins/ReferralManagement/js/traits/ItemAccess.js
+++ Plugins/ReferralManagement/js/traits/ItemAccess.js
@@ -2,7 +2,7 @@ var ItemAccess = (function() {
 
 
 	var ItemAccess = new Class_({
-		getProjectCommunity:function(){
+		getProjectCommunity: function() {
 			return this.data.community;
 		},
 
@@ -11,23 +11,23 @@ var ItemAccess = (function() {
 			var me = this;
 
 			if (me.data && me.data.attributes.firstNationsInvolved) {
-				var communities= me.data.attributes.firstNationsInvolved;
+				var communities = me.data.attributes.firstNationsInvolved;
 
-				if(typeof communities=='string'){
+				if (typeof communities == 'string') {
 
-					if(communities.length>0&&communities[0]=='['){
-						communities=JSON.parse(communities);
+					if (communities.length > 0 && communities[0] == '[') {
+						communities = JSON.parse(communities);
 					}
 
 				}
 
 
-				var allCommunities=ProjectTeam.GetAllCommunities().map(function(a){
+				var allCommunities = ProjectTeam.GetAllCommunities().map(function(a) {
 					return a.toLowerCase();
 				});
 
-				return communities.filter(function(a){
-					return allCommunities.indexOf(a.toLowerCase())>-0;
+				return communities.filter(function(a) {
+					return allCommunities.indexOf(a.toLowerCase()) > -0;
 				});
 
 			}
@@ -38,13 +38,12 @@ var ItemAccess = (function() {
 
 		getCommunitiesSelectedString: function() {
 
-			var allCommunities=ProjectTeam.GetAllCommunities();
+			var allCommunities = ProjectTeam.GetAllCommunities();
 
-			if(allCommunities.length==1){
+			if (allCommunities.length == 1) {
 				return '';
 			}
 
-			
 
 
 			var communities = this.getCommunitiesInvolved();
@@ -71,7 +70,7 @@ var ItemAccess = (function() {
 
 		ProjectList.SharedListFilters().forEach(function(filter, i) {
 			if (filter.filterFn(item)) {
-				className=filter.name.split(' ').join('-');
+				className = filter.name.split(' ').join('-');
 				sharedIcon = filter.icon;
 				sharedLabel = filter.tip || filter.label;
 				sharedIndex = i;
@@ -86,7 +85,7 @@ var ItemAccess = (function() {
 		sharedLabel = sharedLabel.replace('{itemCommunity}', item.getProjectCommunity());
 
 		return {
-			className:className,
+			className: className,
 			label: sharedLabel,
 			icon: sharedIcon
 		};
@@ -98,92 +97,123 @@ var ItemAccess = (function() {
 		// el.addClass("inline sharing");
 		// el.setAttribute("data-col","sharing");
 		// 
-	
-		var lastClass=null;
-		var popover=null;
+
+		var lastClass = null;
+		var popover = null;
 
 
-		var updateEls=updateEls=function(){
+		var updateEls = updateEls = function() {
 
 			var sharedInfo = ItemAccess.GetSharingInfo(item);
 
-			if(lastClass){
+			if (lastClass) {
 				el.addClass(lastClass);
 			}
 
 			el.addClass(sharedInfo.className);
-			lastClass=sharedInfo.className;
+			lastClass = sharedInfo.className;
 
 			valueEl.setAttribute('data-sharedLabel', sharedInfo.label);
 			valueEl.setStyle('background-image', 'url(' + sharedInfo.icon + ')');
 
-			if(!popover){
-				popover=new UIPopover(valueEl, {
+			if (!popover) {
+				popover = new UIPopover(valueEl, {
 					description: sharedInfo.label,
 					anchor: UIPopover.AnchorAuto()
 				});
-			}else{
+			} else {
 				popover.setDescription(sharedInfo.label);
 			}
 
 		};
 
 		if (item instanceof TagCloudModule) {
-			var tagCloud = item;
-
-			var getCommunitiesModule=function(){
-				var viewer=tagCloud.getViewer();
-				if(!viewer){
-					return null;
-				}
-				var modules=tagCloud.getViewer().findChildViews(function(t){return t instanceof TagCloudModule});
-				if(modules.length==2){
-					return modules.pop()
-				}
-				return null;
-			}
-
-			
-
-			item = {
-				isPublic: function() {
-					if(tagCloud.getValues().indexOf('Public')>=0){
-						return true;
-					}
-					return false;
-				},
-				getProjectCommunity: function() {
-					return 'gct3';
-				},
-				getCommunitiesInvolved: function() {
-
-					var mod=getCommunitiesModule();
-					if(mod){
-						return mod.getValues();
-					}
-
-					return [];
-				},
-			}
-
-			tagCloud.on('change', updateEls);
-			tagCloud.runOnceOnLoad(function(){
-				var mod=getCommunitiesModule();
-				if(mod){
-					mod.on('change', updateEls);
-				}
-
-				updateEls();
-			});
+			item = ItemAccess.AddTagCloudBehavior(item, updateEls);
 		}
 
-		
+
 
 		updateEls();
 
 
 
 	};
+
+
+	ItemAccess.AddTagCloudBehavior = function(item, updateEls) {
+
+
+		
+		var tagCloud = item;
+
+		var getCommunitiesModule = function() {
+			var viewer = tagCloud.getViewer();
+			if (!viewer) {
+				return null;
+			}
+			var modules = tagCloud.getViewer().findChildViews(function(t) {
+				return t instanceof TagCloudModule
+			});
+			if (modules.length == 2) {
+				return modules.pop()
+			}
+			return null;
+		}
+
+
+
+
+
+
+
+		item = {
+			isPublic: function() {
+				if (tagCloud.getValues().indexOf('Public') >= 0) {
+					return true;
+				}
+				return false;
+			},
+			getProjectCommunity: function() {
+				return 'gct3';
+			},
+			getCommunitiesInvolved: function() {
+
+				var mod = getCommunitiesModule();
+				if (mod) {
+					return mod.getValues();
+				}
+
+				return [];
+			},
+		}
+
+
+		var updateFn = function() {
+			updateEls();	
+			
+			var mod = getCommunitiesModule();
+			if (mod) {
+				mod.getElement().setStyle('display', item.isPublic()?'none':null);
+			}
+			
+		}
+
+
+		tagCloud.on('change', updateFn);
+		tagCloud.runOnceOnLoad(function() {
+			var mod = getCommunitiesModule();
+			if (mod) {
+				mod.on('change', updateFn);
+			}
+
+			updateFn();
+		});
+
+		return item;
+
+
+	}
+
 
 
 	return ItemAccess;
