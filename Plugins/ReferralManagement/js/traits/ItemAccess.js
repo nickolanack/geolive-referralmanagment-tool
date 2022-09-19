d7a7f00f697eb1724dadc364ad57cb73fd08d740
Nick Blackwell <nickblackwell82@gmail.com>
2022-09-19 13:50:53 -0700

    extract method for reuse

--- Plugins/ReferralManagement/js/traits/ItemAccess.js
+++ Plugins/ReferralManagement/js/traits/ItemAccess.js
@@ -1,54 +1,76 @@
-var ItemAccess=(function(){
+var ItemAccess = (function() {
 
 
-	var ItemAccess=new Class_({});
+	var ItemAccess = new Class_({
 
+
+	});
+
+
+	ItemAccess.GetSharingInfo = function(item) {
+
+		var sharedLabel = '';
+		var sharedIndex = -1;
+		var sharedImage = null;
+		var className = '';
+
+		ProjectList.SharedListFilters().forEach(function(filter, i) {
+			if (filter.filterFn(item)) {
+				className=filter.name.split(' ').join('-');
+				sharedImage = filter.icon;
+				sharedLabel = filter.tip || filter.label;
+				sharedIndex = i;
+			}
+		})
+
+		if (sharedIndex == 3 && item.getProjectSubmitterId() + "" == AppClient.getId() + "") {
+			sharedLabel = "You created this in another community. Managers of <b>" + item.getProjectCommunity() + "</b> can edit.";
+		}
+
+
+		sharedLabel = sharedLabel.replace('{itemCommunity}', item.getProjectCommunity());
+
+		return {
+			className:className,
+			label: sharedLabel,
+			icon: sharedIcon
+		};
+
+	};
 
 	ItemAccess.AddInlineAccessIndicator = function(item, el, valueEl) {
 
 		// el.addClass("inline sharing");
 		// el.setAttribute("data-col","sharing");
-		
-		if(item instanceof TagCloudModule){
-			var tagClount=item;
-			item={
-				isPublic:function(){
+
+		if (item instanceof TagCloudModule) {
+			var tagClount = item;
+			item = {
+				isPublic: function() {
 					return false;
 				},
-				getProjectCommunity:function(){
+				getProjectCommunity: function() {
 					return 'gct3';
 				},
-				getCommunitiesInvolved:function(){
+				getCommunitiesInvolved: function() {
 					return [];
 				},
 			}
 		}
 
+		var sharedInfo = ItemAccess.GetSharingInfo(item);
 
-		var sharedLabel='';
-		var sharedIndex=-1;
+		el.addClass(sharedInfo.className);
+		valueEl.setAttribute('data-sharedLabel', sharedInfo.label);
+		valueEl.setStyle('background-image', 'url(' + sharedInfo.icon + ')');
 
-		ProjectList.SharedListFilters().forEach(function(filter, i){
-		    if(filter.filterFn(item)){
-		        el.addClass(filter.name.split(' ').join('-'));
-		        valueEl.setStyle('background-image', 'url('+filter.icon+')');
-		        sharedLabel=filter.tip||filter.label;
-		        sharedIndex=i;
-		    }
-		})
-
-		    if(sharedIndex==3&&item.getProjectSubmitterId()+""==AppClient.getId()+""){
-		        sharedLabel="You created this in another community. Managers of <b>"+item.getProjectCommunity()+"</b> can edit.";
-		    }
-		    
-		    
-		    sharedLabel=sharedLabel.replace('{itemCommunity}', item.getProjectCommunity());
+		
 
 
-		 new UIPopover(valueEl, {
-		        description:sharedLabel,
-		        anchor:UIPopover.AnchorAuto()
-		    });
+		new UIPopover(valueEl, {
+			description: sharedInfo.label,
+			anchor: UIPopover.AnchorAuto()
+		});
 
 
 	};
