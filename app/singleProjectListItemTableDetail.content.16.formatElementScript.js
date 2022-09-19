ea9a6f3c2cb77be4f5eba4749fae102c7695292c
nickolanack <nickblackwell82@gmail.com>
2022-09-19 19:55:25 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleProjectListItemTableDetail.content.16.formatElementScript.js
+++ app/singleProjectListItemTableDetail.content.16.formatElementScript.js
@@ -1,27 +1,4 @@
 el.addClass("inline sharing");
 el.setAttribute("data-col","sharing");
 
-var sharedLabel='';
-var sharedIndex=-1;
-
-ProjectList.SharedListFilters().forEach(function(filter, i){
-    if(filter.filterFn(item)){
-        el.addClass(filter.name.split(' ').join('-'));
-        valueEl.setStyle('background-image', 'url('+filter.icon+')');
-        sharedLabel=filter.tip||filter.label;
-        sharedIndex=i;
-    }
-})
-
-    if(sharedIndex==3&&item.getProjectSubmitterId()+""==AppClient.getId()+""){
-        sharedLabel="You created this in another community. Managers of <b>"+item.getProjectCommunity()+"</b> can edit.";
-    }
-    
-    
-    sharedLabel=sharedLabel.replace('{itemCommunity}', item.getProjectCommunity());
-
-
- new UIPopover(valueEl, {
-        description:sharedLabel,
-        anchor:UIPopover.AnchorAuto()
-    });
+ItemAccess.AddInlineAccessIndicator(item, el, valueEl);
