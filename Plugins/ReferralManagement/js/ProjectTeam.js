0f4efbc77ea0354f0d4b48223eea157e432730d8
nickolanack <anna@NickMacPro.local>
2022-09-21 22:40:06 -0500

    small btn

--- Plugins/ReferralManagement/js/ProjectTeam.js
+++ Plugins/ReferralManagement/js/ProjectTeam.js
@@ -1171,7 +1171,7 @@ var ProjectTeam = (function() {
 		if(ProjectTeam.GetAllCommunities().length>3){
 			var first=module.getCloud().getElement();
 			var el=first.parentNode;
-			el.insertBefore(new Element('button', {"class":'tag-el primary-btn', html:"All", events:{
+			el.insertBefore(new Element('button', {"class":'tag-el primary-btn small', html:"Select all", events:{
 				click:function(){
 
 					module.getCloud().getWords().forEach(function(t){
@@ -1185,7 +1185,7 @@ var ProjectTeam = (function() {
 					});
 				}}
 			}),first);
-			el.insertBefore(new Element('button', {"class":'tag-el primary-btn', html:"None", events:{
+			el.insertBefore(new Element('button', {"class":'tag-el primary-btn small', html:"Select none", events:{
 				click:function(){
 
 					var words=module.getCloud().getWords();
