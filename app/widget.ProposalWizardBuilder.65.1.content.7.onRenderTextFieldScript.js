e87b4f0dab9657417171b3b8317e29110d9282a0
nickolanack <nickblackwell82@gmail.com>
2022-09-28 19:45:09 +0000

    -- Auto Commit - Changes made through Frontend Interface

--- app/widget.ProposalWizardBuilder.65.1.content.7.onRenderTextFieldScript.js
+++ app/widget.ProposalWizardBuilder.65.1.content.7.onRenderTextFieldScript.js
@@ -5,5 +5,8 @@ if(window.GuestProposal&&item instanceof GuestProposal){
     module.setMandatory(true, function(object){
         return !!object.value;
     });
+    
+    //add 21 days
+    module.setValue(new Date(new Date().getTime()+(1000*3600*24*21)).toISOString().split('T').shift());
 
 }
