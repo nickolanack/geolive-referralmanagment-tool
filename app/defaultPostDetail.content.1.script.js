e511b6d3a6a0bbe87d4768ee4fb39a919d9a3af2
nickolanack <nickblackwell82@gmail.com>
2022-09-28 21:09:46 +0000

    -- Auto Commit - Changes made through Frontend Interface

--- app/defaultPostDetail.content.1.script.js
+++ app/defaultPostDetail.content.1.script.js
@@ -1,3 +1,5 @@
 if (ProjectTeam.CurrentTeam().hasUser(item.getUserId())) {
     return new ElementModule('span',{'class':"post-user", 'html':ProjectTeam.CurrentTeam().getUser(item.getUserId()).getName()});
+}else{
+    console.log('guest submitter?')
 }
