587075057d7653a7a003f10afa40a99af978d901
nickolanack <nickblackwell82@gmail.com>
2022-10-13 04:39:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Displays the form from parameters before rendering the report.1.content.2.formatModuleScript.js
+++ app/Displays the form from parameters before rendering the report.1.content.2.formatModuleScript.js
@@ -1,8 +1,1 @@
-// (new UIFormListBehavior(module)).disableNewItems().disableRemoveEmpty().addDataFormatter(function(data, item){
-    
-//     return ObjectAppend_({}, item.toObject(), data);
-    
-// }).setUpdateField('parameters');
-
-
 FormBuilder.formFieldItemListUpdater(module, item);
--- app/Displays the form from parameters before rendering the report.1.content.2.getItemListScript.js
+++ app/Displays the form from parameters before rendering the report.1.content.2.getItemListScript.js
@@ -1,13 +1,1 @@
-
-    FormBuilder.formFieldItemList(item, callback);
-
-            // callback((item.getParameters?item.getParameters():[]).map(function(item){
-            //     return new MockDataTypeItem({
-            //         name:item.name,
-            //         description:item.description,
-            //         fieldType:item.fieldType,
-            //         defaultValue:item.defaultValue,
-            //         options:item.options
-            //     });
-                
-            // }));
+FormBuilder.formFieldItemList(item, callback);
