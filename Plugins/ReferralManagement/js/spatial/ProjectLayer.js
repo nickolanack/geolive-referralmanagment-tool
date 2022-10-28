25755304dc67cf96b033561bf34bcce3485aced9
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-28 11:56:27 -0700

    reorganize class to try to use implements and override init

--- Plugins/ReferralManagement/js/spatial/ProjectLayer.js
+++ Plugins/ReferralManagement/js/spatial/ProjectLayer.js
@@ -1,10 +1,10 @@
 var ProjectLayer = (function() {
 
 
-	var current=document.currentScript.src;
-	var userFunctionWorker=current.replace('ProjectLayer.js', 'SpatialProjectWorker.js');
+	var current = document.currentScript.src;
+	var userFunctionWorker = current.replace('ProjectLayer.js', 'SpatialProjectWorker.js');
 
-	var getLayerOptions=function(options, map) {
+	var getLayerOptions = function(options, map) {
 
 
 
@@ -12,7 +12,7 @@ var ProjectLayer = (function() {
 			icon: 'https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0',
 			showLabels: false,
 			clickable: false,
-			icons:[]
+			icons: []
 		};
 		var lineOptions = {
 
@@ -27,29 +27,29 @@ var ProjectLayer = (function() {
 
 
 				(new HTMLTagParser()).imagesUrls(metadata.description).forEach(function(url) {
-					
-						options.icon = url;
-						markerOptions.icon = {
-							url: url,
-							scaledSize: new google.maps.Size(40, 40)
-						}
 
-						markerOptions.icons.push({
-							url: url,
-							scaledSize: new google.maps.Size(40, 40)
-						});
+					options.icon = url;
+					markerOptions.icon = {
+						url: url,
+						scaledSize: new google.maps.Size(40, 40)
+					}
+
+					markerOptions.icons.push({
+						url: url,
+						scaledSize: new google.maps.Size(40, 40)
+					});
 
 
-					
+
 				});
 			}
 
-			var initialVisibility="now";
+			var initialVisibility = "now";
 			if (typeof metadata.initialVisibility == "boolean") {
-				initialVisibility = metadata.initialVisibility?"now":"later";
+				initialVisibility = metadata.initialVisibility ? "now" : "later";
 			}
 
-			if(options.group=="project"||options.group=="selection"){
+			if (options.group == "project" || options.group == "selection") {
 				initialVisibility = "now";
 			}
 
@@ -99,39 +99,39 @@ var ProjectLayer = (function() {
 
 			}
 
-			if(metadata.script){
+			if (metadata.script) {
 
-				var worker =new Worker(userFunctionWorker);
+				var worker = new Worker(userFunctionWorker);
 				worker.postMessage(metadata.script);
-				var queue=[];
+				var queue = [];
 
 
-				worker.onmessage=function(e){
-					var args=queue.shift();
+				worker.onmessage = function(e) {
+					var args = queue.shift();
 					args[3].call(null, e.data);
 
-					if(queue.length>0){
-						var next=queue[0];
+					if (queue.length > 0) {
+						var next = queue[0];
 						worker.postMessage([next[0], next[1], next[2]]);
 					}
 
 				}
 
 
-				map.once('remove',function(){
+				map.once('remove', function() {
 					worker.terminate();
 					delete worker.onmessage;
 					delete options.script;
-					worker=null;
+					worker = null;
 				});
 
 				//=(new Function('return function(feature, type, index, callback){ '+"\n"+metadata.script+"\n"+'}')).call(null);
 
-				options.script=function(/*feature, type, index, callback*/){
+				options.script = function( /*feature, type, index, callback*/ ) {
 
-					
+
 					queue.push(arguments);
-					if(queue.length==1){
+					if (queue.length == 1) {
 						worker.postMessage([arguments[0], arguments[1], arguments[2]])
 					}
 
@@ -145,12 +145,140 @@ var ProjectLayer = (function() {
 			markerOptions: markerOptions,
 			polygonOptions: polygonOptions,
 			lineOptions: lineOptions,
-			initialVisibility:initialVisibility
+			initialVisibility: initialVisibility
 		});
 
 		return layerOptions;
 
 	};
+
+
+
+	var ProjectLayerFeatures = new Class_({
+
+		initialize: function(map, options) {
+
+			var layerOptions = getLayerOptions(options, map);
+
+			GeoliveLayer.prototype.initialize.call(this, map, layerOptions);
+
+
+			if ((options.id + "").indexOf("project-") === 0) {
+				var pid = options.id.split('-')[1];
+				var layerIndex = options.id.split('-').pop()
+				var project = ProjectTeam.CurrentTeam().getProject(pid);
+
+				var layer = this;
+
+				var attributeEventHandler = function(data) {
+
+					layer.options.projectAttributes = project.getDatasetAttributes(layerIndex);
+					layer.reload();
+				}
+
+				project.addEvent('updateDatasetAttributes', attributeEventHandler);
+				map.once('remove', function() {
+					project.removeEvent('updateDatasetAttributes', attributeEventHandler);
+				});
+
+			}
+
+		},
+
+		_formatFeature: function(data, type, index, callback) {
+
+			if (typeof this.options.script == 'function') {
+
+				this.options.script(data, type, index, function(result) {
+
+					if (result) {
+						callback(result);
+						return;
+					}
+
+					callback(data);
+
+				});
+
+				return;
+			}
+
+			callback(data);
+
+
+		},
+
+		_initMarker: function(data, markerDataArray, i) {
+			var me = this;
+			this._formatFeature(Object.append(data, this.options.markerOptions), 'marker', i, function(data) {
+				GeoliveLayer.prototype._initMarker.call(me, data, markerDataArray, i);
+			});
+
+
+		},
+		_initPolygon: function(data, lineDataArray, i) {
+			var me = this;
+			this._formatFeature(Object.append(data, this.options.polygonOptions), 'polygon', i, function(data) {
+				GeoliveLayer.prototype._initPolygon.call(me, data, lineDataArray, i);
+			});
+
+
+		},
+		_initLine: function(data, lineDataArray, i) {
+			var me = this;
+			this._formatFeature(Object.append(data, this.options.lineOptions), 'line', i, function(data) {
+				GeoliveLayer.prototype._initLine.call(me, data, lineDataArray, i);
+			});
+
+
+		},
+
+		_initNetworkLink: function(data, linkDataArray, i) {
+			var me = this;
+			this._formatFeature(Object.append(data, {}), 'link', i, function(data) {
+				GeoliveLayer.prototype._initNetworkLink.call(me, data, linkDataArray, i);
+			});
+
+
+		},
+
+		_getKmlQuery: function() {
+			var me = this;
+
+
+			var KMLDocumentQuery = new Class({
+				Extends: StringControlQuery,
+				initialize: function(url) {
+					var me = this;
+					StringControlQuery.prototype.initialize.call(this, CoreAjaxUrlRoot, 'get_kml_for_document', {
+						"document": url,
+						"options": (options.projectAttributes && me.options.projectAttributes.metadata) ? me.options.projectAttributes.metadata : {},
+						'widget': "kmlDocumentRenderer"
+					});
+
+					me._cacheable = true;
+
+				}
+			});
+
+			console.log(me.options.name + " " + (new KMLDocumentQuery(me.options.url)).getUrl(true));
+
+			return new KMLDocumentQuery(me.options.url);
+		},
+		_getTileUrl: function(tile, zoom) {
+			var me = this;
+			return CoreAjaxUrlRoot + '&task=get_tile_for_document&json=' + JSON.stringify({
+				"widget": "kmlDocumentTileRenderer",
+				"document": me.options.url,
+				"z": zoom,
+				"x": tile.x,
+				"y": tile.y
+			});
+		},
+
+
+	})
+
 
 	var ProjectLayer = new Class({
 
@@ -158,9 +286,9 @@ var ProjectLayer = (function() {
 
 
 			/**
-			 * Can define and return a totally new class here. 
-			 * why? because it has dependencies that may be unavailable when this file is loaded
-			 */
+			* Can define and return a totally new class here. 
+			* why? because it has dependencies that may be unavailable when this file is loaded
+			*/
 
 
 
@@ -171,142 +299,11 @@ var ProjectLayer = (function() {
 				return null;
 			}
 
-			var KMLDocumentQuery = new Class({
-				Extends: StringControlQuery,
-				initialize: function(url) {
-					var me = this;
-					StringControlQuery.prototype.initialize.call(this, CoreAjaxUrlRoot, 'get_kml_for_document', {
-						"document": url,
-						"options":(options.projectAttributes&&options.projectAttributes.metadata)?options.projectAttributes.metadata:{},
-						'widget': "kmlDocumentRenderer"
-					});
-
-					me._cacheable = true;
-
-				}
-			});
+			
 
 			var _baseClass = new Class({
 				Extends: GeoliveLayer,
-				initialize: function(map, options) {
-
-					var layerOptions=getLayerOptions(options, map);
-
-					GeoliveLayer.prototype.initialize.call(this, map, layerOptions);
-
-					// this.addParserFilter('point', function(data, i) {
-						
-					// 		//force icon scale
-
-					// 		data.icon = {
-					// 			url: data.icon,
-					// 			scaledSize: new google.maps.Size(40, 40)
-					// 		}
-
-					// 		return true;
-						
-					// });
-
-					if ((options.id + "").indexOf("project-") === 0) {
-						var pid = options.id.split('-')[1];
-						var layerIndex = options.id.split('-').pop()
-						var project = ProjectTeam.CurrentTeam().getProject(pid);
-
-						var layer=this;
-
-						var attributeEventHandler=function(data) {
-							
-							layer.options.projectAttributes = project.getDatasetAttributes(layerIndex);
-							layer.reload();
-						}
-
-						project.addEvent('updateDatasetAttributes', attributeEventHandler);
-						map.once('remove', function(){
-							project.removeEvent('updateDatasetAttributes', attributeEventHandler);
-						});
-
-					}
-
-
-
-				},
-
-				_formatFeature:function(data, type, index, callback){
-
-					if(typeof this.options.script=='function'){
-
-						this.options.script(data, type, index, function(result){
-
-							if(result){
-								callback(result);
-								return;
-							}
-
-							callback(data);
-
-						});
-
-						return;
-					}
-
-					callback(data);
-
-
-				},
-
-				_initMarker: function(data, markerDataArray, i) {
-					var me=this;
-					this._formatFeature(Object.append(data, this.options.markerOptions), 'marker', i, function(data){
-						GeoliveLayer.prototype._initMarker.call(me, data, markerDataArray, i);
-					});
-
-					
-				},
-				_initPolygon: function(data, lineDataArray, i) {
-					var me=this;
-					this._formatFeature(Object.append(data, this.options.polygonOptions), 'polygon', i, function(data){
-						GeoliveLayer.prototype._initPolygon.call(me, data, lineDataArray, i);
-					});
-
-					
-				},
-				_initLine: function(data, lineDataArray, i) {
-					var me=this;
-					this._formatFeature(Object.append(data, this.options.lineOptions), 'line', i, function(data){
-						GeoliveLayer.prototype._initLine.call(me, data, lineDataArray, i);
-					});
-
-					
-				},
-
-				_initNetworkLink: function(data, linkDataArray, i) {
-					var me=this;
-					this._formatFeature(Object.append(data, {}), 'link', i, function(data){
-						GeoliveLayer.prototype._initNetworkLink.call(me, data, linkDataArray, i);
-					});
-
-					
-				},
-
-				_getKmlQuery: function() {
-					var me = this;
-
-					console.log(me.options.name+" "+(new KMLDocumentQuery(me.options.url)).getUrl(true));
-
-					return new KMLDocumentQuery(me.options.url);
-				},
-				_getTileUrl: function(tile, zoom) {
-		            var me = this;
-		            return CoreAjaxUrlRoot + '&task=get_tile_for_document&json='+JSON.stringify({
-		            		"widget":"kmlDocumentTileRenderer",
-		            		"document":me.options.url,
-		            		"z":zoom,
-		            		"x": tile.x,
-		            		"y": tile.y
-		            	});
-		        },
-
-
+				Implements:[ProjectLayerFeatures],
 			});
 
 			//redefine for future instantiations;
