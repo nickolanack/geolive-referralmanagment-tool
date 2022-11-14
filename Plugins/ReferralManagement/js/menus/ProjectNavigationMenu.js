d60002fabc0b4de815121d72c0547d34fcd49853
Nick Blackwell <nickblackwell82@gmail.com>
2022-11-14 08:33:35 -0800

    add android link

--- Plugins/ReferralManagement/js/menus/ProjectNavigationMenu.js
+++ Plugins/ReferralManagement/js/menus/ProjectNavigationMenu.js
@@ -195,8 +195,8 @@ var ProjectNavigationMenu = (function() {
 
 
 									(new UIModalDialog(application, new MockDataTypeItem({
-										name:`<h2>Scan this code with the field assessment app</h2>IOS: <a href='https://testflight.apple.com/join/rPVA9ODg'>Become an open beta tester for IOS<a>
-										Android: <a href=''>Become an open beta tester for Android<a>`,
+										name:`<h2>Scan this code with the field assessment app</h2><span>IOS:</span> <a href='https://testflight.apple.com/join/rPVA9ODg'>Become an open beta tester for IOS<a><br/>
+										<span>Android:</span> <a href='https://play.google.com/apps/testing/org.nickolanack.gatherapp'>Become an open beta tester for Android<a>`,
 										image:resp.qrcode
 									}), {
 										"formName": "dialogForm",
