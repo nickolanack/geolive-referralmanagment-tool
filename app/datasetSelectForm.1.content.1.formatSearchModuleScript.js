1585af9eeff40bf1c095de111d0d901bf1933507
nickolanack <nickblackwell82@gmail.com>
2022-11-14 22:02:16 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetSelectForm.1.content.1.formatSearchModuleScript.js
+++ app/datasetSelectForm.1.content.1.formatSearchModuleScript.js
@@ -1,8 +1,32 @@
 searchModule.getElement().addClass('inline-filter-search');
 searchModule.getSearch(function(search){
+    
+    
+    var list=viewer.findChildViews(function(v) {
+        return v instanceof UIListViewModule;  				
+    }).pop();
+    
+    var _timeout=null;
+    
    search.on('keyPress',function(key, value){
         
-        console.log(value);
+        if(_timeout){
+            clearTimeout(_timeout);
+        }
+        
+        
+        _timeout=setTimeout(function(){
+            
+            _timeout=null;
+        
+            list.filter(function(item){
+                console.log(item)
+                return true;
+                
+            })
+        
+        
+        },250);
       
    });
 });
