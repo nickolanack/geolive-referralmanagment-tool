c8ec8238ace1af853b59b1a649b4db767c4a918c
nickolanack <nickblackwell82@gmail.com>
2022-11-14 21:46:57 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/datasetSelectForm.1.content.1.formatSearchModuleScript.js
+++ app/datasetSelectForm.1.content.1.formatSearchModuleScript.js
@@ -1,1 +1,8 @@
-searchModule.getElement().addClass('inline-filter-search')
+searchModule.getElement().addClass('inline-filter-search');
+searchModule.getSearch(function(search){
+   search.on('keyPress',function(text){
+        
+        console.log(text);
+      
+   });
+});
