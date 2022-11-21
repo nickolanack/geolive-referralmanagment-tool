cf6ce97f5c02180c808c36484b76439adc4a386e
nickolanack <nickblackwell82@gmail.com>
2022-11-21 19:28:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Displays the form from parameters before rendering the report.1.content.2.formatModuleScript.js
+++ app/Displays the form from parameters before rendering the report.1.content.2.formatModuleScript.js
@@ -1,1 +1,15 @@
-FormBuilder.formFieldItemListUpdater(module, item);
+FormBuilder.addPreprocessor('TemplateValue', function(item, textField){
+    
+    (new AjaxControlQuery(CoreAjaxUrlRoot, 'generate_report_field', {
+				"plugin": "ReferralManagement",
+				"project": listModule.getListItem().getProject(),
+				"template": item.getDefaultValue()
+			})).on('success',function(resp){
+
+				textField.setValue(resp.value);
+
+			}).execute();
+    
+    
+    
+}).formFieldItemListUpdater(module, item);
