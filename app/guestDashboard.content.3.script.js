5e9d180d2a8a68297a3229e62ac0ef4da6dd4f36
nickolanack <nickblackwell82@gmail.com>
2022-12-02 17:01:50 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/guestDashboard.content.3.script.js
+++ app/guestDashboard.content.3.script.js
@@ -1,18 +1,18 @@
 return ModuleArray([
-        new Element('a',{
+        new ElementModule('a',{
             href:"/install",
             html:"Install Gather"
         }),
-        new Element('a',{
+        new ElementModule('a',{
             href:"/welcome",
             html:"Geoforms"
         }),
-        new Element('a',{
+        new ElementModule('a',{
             href:"https://play.google.com/store/apps/details?id=org.nickolanack.gatherapp",
             html:"Gather App"
         }),
-        new Element('a',{
+        new ElementModule('a',{
             href:"https://play.google.com/store/apps/details?id=org.wabun.com",
             html:"Gather Community App"
         })
-    ],{"className":"credit-links"})
+    ],{"className":"credit-links"});
