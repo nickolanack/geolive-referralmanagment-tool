3019d03bcdc9d3fc90e547f7264ed28648fcccec
nickolanack <nickblackwell82@gmail.com>
2022-12-02 16:49:19 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/guestDashboard.content.3.script.js
@@ -0,0 +1,14 @@
+return [
+    new Element('a',{
+        href:"/install"
+    }),
+    new Element('a',{
+        href:"/welcome"
+    }),
+    new Element('a',{
+        href:"https://play.google.com/store/apps/details?id=org.nickolanack.gatherapp"
+    }),
+    new Element('a',{
+        href:"https://play.google.com/store/apps/details?id=org.wabun.com"
+    })
+    ]
