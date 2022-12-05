bc28cf3f0f2e86906a3ddf7895da0692423b76a5
nickolanack <nickblackwell82@gmail.com>
2022-12-05 17:32:21 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/singleUserListItemDetail.content.1.formatElementScript.js
+++ app/singleUserListItemDetail.content.1.formatElementScript.js
@@ -1,32 +1,3 @@
 
 
-if(AppClient.getUserType()=="admin"&&item.getUserId){
-    
-    var editEl=new Element('span',{"class":"field-value"});
-    el.appendChild(editEl)
-    new UIModalFormButton(editEl, application, item, {
-        "stopPropagation":true,
-        "formName":"userProfileForm", "formOptions":{template:"form"}});
-    editEl.addClass('editable');
-    
-    DashboardConfig.getValue("enableUserProfiles",function(enabled){
-        
-        if((!enabled)&&el.previousSibling){
-            new UIModalFormButton(el.previousSibling, application, item, {
-                "stopPropagation":true,
-                "formName":"userProfileForm", "formOptions":{template:"form"}});
-        }
-        
-       
-        
-        
-    })
-    
-    
-}
-
-
-UIInteraction.addUserProfileClick(el, item);
-if(el.previousSibling){
-    UIInteraction.addUserProfileClick(el.previousSibling, item);
-}
+ProjectTeam.AddListItemBehavior(item, application, el);
