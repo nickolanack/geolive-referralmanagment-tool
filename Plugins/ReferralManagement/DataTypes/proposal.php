b511bf853dd1f3ca8759278db17961b718e69835
Nick Blackwell <nickblackwell82@gmail.com>
2022-12-05 11:30:59 -0800

    add error log

--- Plugins/ReferralManagement/DataTypes/proposal.php
+++ Plugins/ReferralManagement/DataTypes/proposal.php
@@ -45,14 +45,21 @@ class ProposalDataType extends \core\extensions\plugin\PluginDataType {
 
             $auth=$auth||intval($item->user)==$userId;
             if(!$auth){
-               $teamMembers=$item->attributes->teamMembers;
-               array_walk($teamMembers, function($teamMember)use(&$auth, $userId){
 
-                    if(intval($teamMember->id)==$userId){
-                        $auth=true;
-                    }
+               if(isset($item->attributes->teamMembers)){
+                
+                   $teamMembers=$item->attributes->teamMembers;
+                   array_walk($teamMembers, function($teamMember)use(&$auth, $userId){
 
-               });
+                        if(intval($teamMember->id)==$userId){
+                            $auth=true;
+                        }
+
+                   });
+               }else{
+                error_log('teamMembers is not set');
+                error_log(print_r($item, true));
+               }
             }
 
 
