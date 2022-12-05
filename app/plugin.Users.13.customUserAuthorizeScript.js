0af7f2c597afa9ae710020709c182dd380118c9f
nickolanack <nickblackwell82@gmail.com>
2022-12-05 20:16:58 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Users.13.customUserAuthorizeScript.js
+++ app/plugin.Users.13.customUserAuthorizeScript.js
@@ -4,8 +4,8 @@
         error_log(print_r(array($itemtype, $task, $userId), true));
         GetPlugin('ReferralManagement');
         
-        if($userId!==-1&&GetClient()->getUserId()!=$userId&&(!GetClient()->isGuest())){
-            (new \ReferralManagement\User())->canEditUsersRole($userId);
+        if($userId===-1&&GetClient()->getUserId()!=intval($item)&&(!GetClient()->isGuest())){
+            (new \ReferralManagement\User())->canEditUsersRole($item);
         }
         
     }
