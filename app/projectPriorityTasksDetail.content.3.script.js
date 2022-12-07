3205a850f6da867ee3f091d7b93f88d53c592d01
nickolanack <nickblackwell82@gmail.com>
2022-12-07 17:25:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/projectPriorityTasksDetail.content.3.script.js
+++ app/projectPriorityTasksDetail.content.3.script.js
@@ -1,4 +1,12 @@
 
-return UIInteraction.createSectionToggle(function(v) {
+var toggle= UIInteraction.createSectionToggle(function(v) {
     	return v instanceof UIListViewModule
   	 });
+  	 
+toggle.on('collapse', function(){
+    toggle.getElement().parentNode.addClass('hide');
+}).on('expand', function(){
+    toggle.getElement().parentNode.removeClass('hide');
+});
+  	 
+return toggle;
