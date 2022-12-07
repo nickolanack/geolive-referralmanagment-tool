20246240c7975a08fadab0931cf22452c2935916
nickolanack <nickblackwell82@gmail.com>
2022-12-07 17:07:10 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Tasks List Detail.content.1.script.js
+++ app/Tasks List Detail.content.1.script.js
@@ -1,1 +1,4 @@
-return TaskItem.MainTaskListSortMenu();
+
+    var mod = new ElementModule('div',{"class":"section-title"});
+    mod.getElement().appendChild(new Element("span",{"html":"Tasks"}));
+    return mod;
