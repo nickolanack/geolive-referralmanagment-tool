60323515e66115e286bbacabfaefb673b080629c
nickolanack <nickblackwell82@gmail.com>
2022-12-09 22:57:43 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/communities.onRenderApplictionScript.js
+++ app/communities.onRenderApplictionScript.js
@@ -14,6 +14,31 @@ IncludeJSBlock('
 
 ');
 
+
+IncludeJSBlock('
+
+    a.ui-view[data-view="communityItem"] {
+        display: inline-block;
+        width: 100%;
+        min-height: 250px;
+        margin: 30px;
+        box-sizing: border-box;
+        backdrop-filter: blur(10px);
+        background-color: rgba(0,0,0,0.3);
+        position: relative;
+        padding: 30px;
+        border-radius: 9px;
+        filter: drop-shadow(0 0 6px #00000080);
+    }
+    
+    span.array-module.ui-list-view {
+        margin: auto;
+        max-width: 1000px;
+    }
+
+
+');
+
 GetWidget('sharedDashboardTheme')->display($targetInstance);
 GetWidget('sharedGeneratedStyles')->display($targetInstance);
 
