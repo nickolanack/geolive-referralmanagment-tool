702f1a9027318fbbe10bb95e0031270163625e2d
nickolanack <nickblackwell82@gmail.com>
2022-12-09 23:03:06 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/communitiesContent.content.1.getItemListScript.js
+++ app/communitiesContent.content.1.getItemListScript.js
@@ -2,7 +2,7 @@ callback((
     [{
         name:"Gather Demo Site",
         url:"https://rmt.geoforms.ca",
-        image:""
+        image:"https://dyl2vw577xcfk.cloudfront.net/rmt.geoforms.ca/1/Uploads/%5BG%5D_%5BImAgE%5D_XZu_kJ_SPB.png"
     }]
     ).map(function(data){
         return new MockDataTypeItem(data);
