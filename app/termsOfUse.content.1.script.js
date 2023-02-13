178d93455378f95849b462d3ad6cefa00bdc4216
nickolanack <nickblackwell82@gmail.com>
2023-02-13 21:11:25 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/termsOfUse.content.1.script.js
+++ app/termsOfUse.content.1.script.js
@@ -1,5 +1,6 @@
 return new Element('button',{
     html:"I Accept",
+    "class":"primary-btn nav-new-btn btn-index-1",
     events:{
         click:function(){
             localStorage.setItem("acceptedTerms", JSON.stringify({
