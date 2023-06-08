477e0077fe0db964a0d6c6aa4def5739eb939a39
Nick Blackwell <nickblackwell82@gmail.com>
2023-06-08 10:45:35 -0700

    move outside of callback - currentScript is garbage collected

--- Plugins/ReferralManagement/js/spatial/ProjectLayerWorkerLoader.js
+++ Plugins/ReferralManagement/js/spatial/ProjectLayerWorkerLoader.js
@@ -3,14 +3,14 @@ var ProjectLayerWorkerLoader=(function(){
 	/**
 	 * this class must not be compiled into another file because it uses its own path
 	 */
-
+	var current = document.currentScript.src;
 
 	var ProjectLayerWorkerLoader=new Class_({
 
 
 			getWorker:function(){
 
-				var current = document.currentScript.src;
+				
 				var userFunctionWorker = current.replace('ProjectLayerWorkerLoader.js', 'SpatialProjectWorker.js');
 
 				return new Worker(userFunctionWorker);
