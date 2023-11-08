f9bdab2335671c65033741eda0b3364daf272737
Nick Blackwell <nickblackwell82@gmail.com>
2023-11-07 22:18:43 -0800

    move block out of if

--- Plugins/ReferralManagement/js/NamedCategory.js
+++ Plugins/ReferralManagement/js/NamedCategory.js
@@ -458,29 +458,30 @@ var NamedCategory = (function() {
 		if(url){
 		    labelEl.setStyle('background-image', 'url('+url+')');
 		}
-		    el.setStyle('background-color', item.getColor());
-		    var c=item.getColor();
+		
+	    el.setStyle('background-color', item.getColor());
+	    var c=item.getColor();
 
-		    if(!c){
-		    	c="#fefefe";
-		    }
+	    if(!c){
+	    	c="#fefefe";
+	    }
 
-			if(c[0]=="#"){
-				var c = c.substring(1);      // strip #
-				var rgb = parseInt(c, 16);   // convert rrggbb to decimal
-				var r = (rgb >> 16) & 0xff;  // extract red
-				var g = (rgb >>  8) & 0xff;  // extract green
-				var b = (rgb >>  0) & 0xff;  // extract blue
+		if(c[0]=="#"){
+			var c = c.substring(1);      // strip #
+			var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+			var r = (rgb >> 16) & 0xff;  // extract red
+			var g = (rgb >>  8) & 0xff;  // extract green
+			var b = (rgb >>  0) & 0xff;  // extract blue
 
-				var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+			var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
 
-				if (luma < 40) {
-				    el.addClass('is-dark');
-				}else{
-					el.addClass('is-light');
-				}
+			if (luma < 40) {
+			    el.addClass('is-dark');
+			}else{
+				el.addClass('is-light');
 			}
 		}
+		
 
 
 	}
