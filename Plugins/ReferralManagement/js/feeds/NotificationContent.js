22f8b81af198a4bd9267286cfa230c3211f0ec07
nickolanack <nickblackwell82@gmail.com>
2025-01-29 23:59:01 -0800

    add a few methods for calc: ... event templates

--- Plugins/ReferralManagement/js/feeds/NotificationContent.js
+++ Plugins/ReferralManagement/js/feeds/NotificationContent.js
@@ -7,8 +7,48 @@ var NotificationContent = (function() {
 			return PostContent.viewForItem(item);
 		},
 
+		countUpdated:function(){
+			var list=ProjectTeam.CurrentTeam().getProjects()
+			.sort(function(a, b){ return b.getModificationDate().localeCompare(a.getModificationDate())})
+			.filter(function(a){
+				return  (new Date()).getTime()/1000 - a.data.modifiedDateTimestamp < 86400*100;
+			});
+
+			var l=list.length;
+			return l;
+		},
+
+		lastUpdated:function(){
+			var list=ProjectTeam.CurrentTeam().getProjects()
+			.sort(function(a, b){ return b.getModificationDate().localeCompare(a.getModificationDate())});
+			
+			return list[0].getModificationDate();
+		},
+
+		lastCreated:function(){
+			var list=ProjectTeam.CurrentTeam().getProjects()
+			.sort(function(a, b){ return b.getCreationDate().localeCompare(a.getCreationDate())});
+			
+			return list[0].getCreationDate();
+		},
 
 		formatEventText: function(text, data) {
+
+			var l=this.countUpdated();
+
+			if (text == 'calc: items updated') {
+
+				
+				text = 'There '+(l==1?'is':'are')+' '+(l)+' updated item'+(l==1?'':'s');
+				data.text = text;
+				return text;
+			}
+
+			if (text == 'calc: items created') {
+				text = 'There is 1 new dataset'
+				data.text = text;
+				return text;
+			}
 
 
 			if (text == 'event: update.proposal.team.add') {
