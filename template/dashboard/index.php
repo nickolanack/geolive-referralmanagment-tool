a180e1811e5a0ab9d138b718e06f67b000979896
nickolanack <nickblackwell82@gmail.com>
2025-03-26 22:14:06 -0700

    update meta

--- template/dashboard/index.php
+++ template/dashboard/index.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
 		<meta name="HandheldFriendly" content="true" />
-		<meta name="apple-mobile-web-app-capable" content="YES" />
+		<meta name="mobile-web-app-capable" content="YES" />
 		<jdoc:include type="head" />
 	</head>
 	<body>
