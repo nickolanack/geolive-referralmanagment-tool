b438785fa2430191b745af92ad755fb8ac226c8f
nickolanack <nickblackwell82@gmail.com>
2026-03-11 17:26:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/loginForm.1.children.2.content.2.script.js
@@ -0,0 +1,12 @@
+return new Element('button', {
+				html: 'Submit Registration',
+				style: "background-color:mediumseagreen;",
+				"class": "primary-btn",
+				"events"{
+				   "click":function(){
+				     console.log('hello world')
+				   
+				   }
+				}
+
+			})
