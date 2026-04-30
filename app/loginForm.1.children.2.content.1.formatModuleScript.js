0facda64f239235316739d99843a5accdc7abb13
nickolanack <nickblackwell82@gmail.com>
2026-04-30 18:04:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/loginForm.1.children.2.content.1.formatModuleScript.js
+++ app/loginForm.1.children.2.content.1.formatModuleScript.js
@@ -6,7 +6,9 @@ uiview.getChildWizard(function(wizard) {
 					wizard.update();
 					var d = wizard.getData();
 					var parentWizard = uiview.getWizard();
+					parentWizard.setDataValue("profileData", d["Attribute_userAttributes_Object"])
 					var p = parentWizard.getData();
 					console.log('wizard_data')
+					
 					})
 				})
